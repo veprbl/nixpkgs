34ac62f8f8c156e688144d3ac6dce818c0a06f0d
Vladimír Čunát <vcunat@gmail.com>
2014-04-24 14:41:30 +0200

    fontconfig: finalize a sane build
    Config from infinality etc. should be separate build.

--- pkgs/development/libraries/fontconfig/default.nix
+++ pkgs/development/libraries/fontconfig/default.nix
@@ -4,24 +4,17 @@ stdenv.mkDerivation rec {
   name = "fontconfig-2.11.1";
 
   src = fetchurl {
-    url = "http://fontconfig.org/release/${name}.tar.bz2";
+    url = "${meta.homepage}/release/${name}.tar.bz2";
     sha256 = "16baa4g5lswkyjlyf1h5lwc0zjap7c4d8grw79349a5w6dsl8qnw";
   };
-/*
-  infinality_patch = with freetype.infinality; if useInfinality
-    then let subvers = "1";
-      in fetchurl {
-        url = http://www.infinality.net/fedora/linux/zips/fontconfig-infinality-1-20130104_1.tar.bz2;
-        sha256 = "1fm5xx0mx2243jrq5rxk4v0ajw2nawpj23399h710bx6hd1rviq7";
-      }
-    else null;
-*/
+
+  patches = [ ./cache-regen.patch ];
+
   propagatedBuildInputs = [ freetype ];
   buildInputs = [ pkgconfig expat ];
 
   configureFlags = [
     "--with-cache-dir=/var/cache/fontconfig"
-    "--disable-docs"
     "--with-default-fonts="
   ];
 
@@ -40,11 +33,7 @@ stdenv.mkDerivation rec {
 
   # Don't try to write to /var/cache/fontconfig at install time.
   installFlags = "fc_cachedir=$(TMPDIR)/dummy";
-/*
-  postInstall = stdenv.lib.optionalString freetype.infinality.useInfinality ''
-    cd "$out/etc/fonts" && tar xvf ${infinality_patch}
-  '';
-*/
+
   meta = with stdenv.lib; {
     description = "A library for font customization and configuration";
     homepage = http://fontconfig.org/;
