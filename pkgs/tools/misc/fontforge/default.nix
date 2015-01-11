27a5365648e611d47f4d334ec9a175afb590d615
Vladimír Čunát <vcunat@gmail.com>
2015-01-11 23:07:06 +0100

    fontforge: explicitly add libpng to RPATH
    Also clean whitespace in the expression.

--- pkgs/tools/misc/fontforge/default.nix
+++ pkgs/tools/misc/fontforge/default.nix
@@ -1,29 +1,29 @@
 { stdenv, fetchurl, gettext, freetype, zlib
-, libungif, libpng, libjpeg, libtiff, libxml2
+, libungif, libpng, libjpeg, libtiff, libxml2, patchelf
 , withX11 ? false
 , libX11 ? null, lib, xproto ? null, libXt ? null
 }:
 
-let 
+let
   version = "20110222";
   name = "fontforge-${version}";
 in
 
 stdenv.mkDerivation {
   inherit name;
-  
+
   src = fetchurl {
     url = "mirror://sourceforge/fontforge/fontforge_full-${version}.tar.bz2";
     sha256 = "0gj342iyd2qmza523r84m65fm7bymcfd4lbllywbfjzq4s0838lg";
   };
-    
+
   configureFlags = lib.optionalString withX11 "--with-gui=gdraw";
-  
+
   preConfigure = ''
     unpackFile ${freetype.src}
     freetypeSrcPath=$(echo `pwd`/freetype-*)
     configureFlags="$configureFlags --with-freetype-src=$freetypeSrcPath"
-    
+
     substituteInPlace configure \
       --replace /usr/include /no-such-path \
       --replace /usr/lib /no-such-path \
@@ -34,6 +34,12 @@ stdenv.mkDerivation {
   '';
 
   buildInputs =
-    [ gettext freetype zlib libungif libpng libjpeg libtiff libxml2 ]
+    [ gettext freetype zlib libungif libpng libjpeg libtiff libxml2 patchelf ]
     ++ lib.optionals withX11 [ libX11 xproto libXt ];
+
+  # libpng is loaded on-demand
+  postFixup = ''
+    patchelf --set-rpath `patchelf --print-rpath "$out/bin/fontforge"`:"${libpng}/lib" "$out/bin/fontforge"
+  '';
 }
+
