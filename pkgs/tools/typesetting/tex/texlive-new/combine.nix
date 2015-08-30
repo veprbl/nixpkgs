29bf06b024c74784f097c4db4a9d461f03fb8d47
Vladimír Čunát <vcunat@gmail.com>
2015-08-30 23:11:52 +0200

    WIP: major refactor and minor fixes

---
+++ pkgs/tools/typesetting/tex/texlive-new/combine.nix
@@ -0,0 +1,131 @@
+params: with params;
+# combine =
+args@{ pkgFilter ? (pkg: pkg.tlType == "run" || pkg.tlType == "bin" || pkg.name == "core"), ... }:
+let
+  pkgSet = removeAttrs args ["pkgFilter"] // {
+    # include a fake "core" package
+    core.pkgs = [
+      (bin.core.doc // { tlType = "doc"; })
+      (bin.core.out // { tlType = "bin"; })
+    ];
+  };
+  pkgList = let
+    all = lib.filter pkgFilter (combinePkgs pkgSet);
+    splitBin = lib.partition (p: p.tlType == "bin") all;
+  in rec {
+    bin = mkUniquePkgs splitBin.right;
+    nonbin = mkUniquePkgs splitBin.wrong;
+  };
+
+  mkUniquePkgs = pkgs: fastUnique (a: b: a < b) (map builtins.toPath pkgs);
+in buildEnv {
+  name = "texlive-combined-${bin.year}";
+
+  extraPrefix = "/share/texmf";
+
+  ignoreCollisions = false;
+  paths = pkgList.nonbin;
+
+  buildInputs = [ makeWrapper ];
+
+  postBuild = ''
+    cd "$out"
+    mkdir -p ./bin
+  '' +
+    lib.concatMapStrings
+      (path: ''
+        for f in '${path}'/bin/*; do
+          ln -s "$f" ./bin/
+        done
+      '')
+      pkgList.bin
+    +
+  ''
+    export PATH="$out/bin:$out/share/texmf/scripts/texlive:${perl}/bin:$PATH"
+    export TEXMFCNF="$out/share/texmf/web2c"
+    export TEXMFDIST="$out/share/texmf"
+    export TEXMFSYSCONFIG="$out/share/texmf-config"
+    export TEXMFSYSVAR="$out/share/texmf-var"
+    export PERL5LIB="$out/share/texmf/scripts/texlive"
+  '' +
+    # patch texmf-dist -> texmf to be sure
+    # TODO: cleanup the search paths incl. SELFAUTOLOC, and perhaps do lua actions?
+    # tried inspiration from install-tl, sub do_texmf_cnf
+  ''
+    local cnfPath=./share/texmf/web2c/texmf.cnf
+    local cnfOrig="$(realpath $cnfPath)"
+    rm $cnfPath
+    cat "$cnfOrig" | sed 's/texmf-dist/texmf/g' > $cnfPath
+  '' +
+  # wrap created executables with required env vars
+  ''
+    wrapBin() {
+    for link in ./bin/*; do
+      [ -L "$link" -a -x "$link" ] || continue # if not link, assume OK
+      local target=$(readlink "$link")
+      case "$target" in
+        /*)
+          echo -n "Wrapping '$link'"
+          rm "$link"
+          makeWrapper "$target" "$link" \
+            --prefix PATH : "$out/bin:${perl}/bin" \
+            --set TEXMFCNF "$out/share/texmf/web2c" \
+            --set TEXMFDIST "$out/share/texmf" \
+            --set TEXMFSYSCONFIG "$out/share/texmf-config" \
+            --set TEXMFSYSVAR "$out/share/texmf-var" \
+            --prefix PERL5LIB : "$out/share/texmf/scripts/texlive"
+
+          # avoid using non-nix shebang in $target by calling interpreter
+          if [[ "$(head -c 2 $target)" = "#!" ]]; then
+            local interp="$(head -n 1 $target | sed 's/^\#\! *//;s/ *$//')"
+            local newInterp=""
+            case "$interp" in
+              /bin/sh)
+                newInterp="$(echo -n ${stdenv.shell} | sed 's/bash$/sh/' )";;
+              /usr/bin/env\ perl|/usr/bin/perl)
+                newInterp='${perl}/bin/perl';;
+              /nix/store/*)
+                echo
+                continue;;
+              *)
+                echo "Unknown shebang '$interp' in '$target'"
+                false
+            esac
+            echo " and patching shebang '$interp'"
+            sed "s|^exec |exec $newInterp |" -i "$link"
+          else
+            echo
+          fi
+          ;;
+      esac
+    done
+    }
+  '' +
+  # texlive post-install actions
+  ''
+    mkdir -p "$out/share/texmf/scripts/texlive/"
+    ln -s '${bin.core.out}/share/texmf-dist/scripts/texlive/TeXLive' "$out/share/texmf/scripts/texlive/"
+
+    for tool in updmap fmtutil; do
+      ln -sf "$out/share/texmf/scripts/texlive/$tool."* "$out/bin/$tool"
+    done
+    ln -sf fmtutil "$out/bin/mktexfmt"
+
+    perl `type -P mktexlsr.pl` ./share/texmf
+    texlinks.sh "$out/bin" && wrapBin
+    perl `type -P fmtutil.pl` --sys --all | grep '^fmtutil' # too verbose
+    #texlinks.sh "$out/bin" && wrapBin # may we need to run again?
+    yes | perl `type -P updmap.pl` --sys --syncwithtrees || true
+    yes | perl `type -P updmap.pl` --sys --syncwithtrees || true
+  '' +
+  # TODO: a context trigger https://www.preining.info/blog/2015/06/debian-tex-live-2015-the-new-layout/
+    # http://wiki.contextgarden.net/ConTeXt_Standalone#Unix-like_platforms_.28Linux.2FMacOS_X.2FFreeBSD.2FSolaris.29
+  ''
+    ln -s texmf/doc/{man,info} "$out/share/"
+  ''
+    + bin.cleanBrokenLinks
+  ;
+}
+# TODO: make TeX fonts visible by fontconfig: it should be enough to install an appropriate file
+#       similarly, deal with xe(la)tex font visibility?
+
--- pkgs/tools/typesetting/tex/texlive-new/default.nix
+++ pkgs/tools/typesetting/tex/texlive-new/default.nix
@@ -6,11 +6,26 @@ let
   # TODOs:
   #   - maybe fixup scripts in individual packages
 
-  /* curl ftp://tug.ctan.org/pub/tex/historic/systems/texlive/2015/tlnet-final/tlpkg/texlive.tlpdb.xz \
-    | xzcat | sed -rn -f ./tl2nix.sed > ./pkgs.nix */
-  tl-clean =
-    let orig = removeAttrs (import ./pkgs.nix tl-flatDeps) [ "trash" ];
-    in orig // {
+  # various binaries (compiled)
+  bin = callPackage ./bin.nix {
+    poppler = poppler_nox; # otherwise depend on various X stuff
+    ghostscript = ghostscriptX;
+    harfbuzz = harfbuzz.override {
+      withIcu = true; withGraphite2 = true;
+    };
+  };
+
+  # function for creating a working environment from a set of TL packages
+  combine = import ./combine.nix {
+    inherit bin combinePkgs buildEnv fastUnique lib makeWrapper perl stdenv;
+  };
+
+  # the set of TeX Live packages, collections, and schemes; using upstream naming
+  tl = let
+    /* curl ftp://tug.ctan.org/pub/tex/historic/systems/texlive/2015/tlnet-final/tlpkg/texlive.tlpdb.xz \
+      | xzcat | sed -rn -f ./tl2nix.sed > ./pkgs.nix */
+    orig = removeAttrs (import ./pkgs.nix tl) [ "trash" ];
+    clean = orig // {
       # overrides of texlive.tlpdb
 
       tetex = orig.tetex // { # 2015.08.27 as we need version with mktexlsr.pl
@@ -19,8 +34,9 @@ let
         md5.doc = "983f5e5b5f4e407760b4ec176cf6a58f";
         version = "3.0"; # it's the same
         postUnpack = "cd $out && patch -p2 < ${./texlinks.patch}";
+        # TODO: postUnpack per tlType
       };
-      dvidvi.md5 = { # only contains docs that's in bin.doc already
+      dvidvi.md5 = { # only contains docs that's in bin.core.doc already
       };
       hyphen-base = orig.hyphen-base // {
         # hacky fixup for: I can't find file `dehypht-x-2014-05-21.tex'
@@ -31,52 +47,40 @@ let
         hasRunfiles = false; # only *.po for tlmgr
       };
 
-      # luatex and metafont are heavy, so remove it from the basic stuff
+      # remove dependency-heavy packages from the basic collections
       collection-basic = orig.collection-basic // {
-        deps = removeAttrs orig.collection-basic.deps [ "luatex" "metafont" ];
+        deps = removeAttrs orig.collection-basic.deps [ "luatex" "metafont" "xdvi" ];
       };
       latex = orig.latex // {
         deps = removeAttrs orig.latex.deps [ "luatex" ];
       };
     };
-
-  tl-flatDeps = lib.mapAttrs flatDeps tl-clean;
+    # tl =
+    in lib.mapAttrs flatDeps clean;
+    # TODO: texlive.infra for web2c config?
 
   flatDeps = pname: attrs:
-        let
-            mkPkgV = pname: md5: let pkg = attrs // { inherit pname md5; };
-              in mkPkgs {
-                inherit pname; pkgList = [ pkg ];
-                version = attrs.version or bin.year;
-              };
-
-            mkPkgVx = type: {
-              ${type} = lib.optional (attrs.md5 ? type)
-                (mkPkgs (attrs.version or bin.year) "${pname}.${type}" attrs.md5.${type});
-            };
-            combDeps = (combinePkgs (attrs.deps or {}));
-        in {
-          # TL pkg contains three lists of packages: runtime files, docs, and sources
-          pkgs = if false then # TODO: fix and finish the refactoring
-            combDeps //
-            mkPkgVx "run" //
-            mkPkgVx "doc" //
-            mkPkgVx "source"
-          else {
-            # tarball of a collection/scheme itself only contains a tlobj file
-            run = lib.optional (attrs.hasRunfiles or false)
-                (mkPkgV pname attrs.md5.run)
-              ++ combDeps.run;
-            doc = lib.optional (attrs.md5 ? "doc")
-                (mkPkgV "${pname}.doc" attrs.md5.doc)
-              ++ combDeps.doc;
-            source = lib.optional (attrs.md5 ? "source")
-                (mkPkgV "${pname}.source" attrs.md5.source)
-              ++ combDeps.source;
-            bin = lib.optional (bin-all ? ${pname}) bin-all.${pname}
-              ++ combDeps.bin;
-          };
+    let
+      mkPkgV = tlType: let
+        pkg = { version = bin.year; } // attrs // {
+          md5 = attrs.md5.${tlType};
+          inherit tlType;
+          pname = pname + lib.optionalString (tlType != "run") ".${tlType}";
         };
+        in mkPkgs {
+          inherit (pkg) pname tlType version;
+          pkgList = [ pkg ];
+        };
+    in {
+      # TL pkg contains lists of packages: runtime files, docs, sources, binaries
+      pkgs =
+        # tarball of a collection/scheme itself only contains a tlobj file
+        lib.optional (attrs.hasRunfiles or false) (mkPkgV "run")
+        ++ lib.optional (attrs.md5 ? "doc") (mkPkgV "doc")
+        ++ lib.optional (attrs.md5 ? "source") (mkPkgV "source")
+        ++ lib.optional (bin ? ${pname}) (bin.${pname} // { tlType = "bin"; })
+        ++ combinePkgs (attrs.deps or {});
+    };
 
   unpackPkg =
     { url ? "ftp://tug.ctan.org/pub/tex/historic/systems/texlive/${bin.year}/tlnet-final/archive/${pname}.tar.xz"
@@ -90,21 +94,18 @@ let
             -C "$out" --anchored --exclude=tlpkg --keep-old-files
         '' + postUnpack;
 
-  mkPkgs = { pname, version, pkgList }:
+  mkPkgs = { pname, tlType, version, pkgList }:
       /* TODOs:
-          - how to patch shebangs?
-            even ${coreutils}/bin/env would make a hash-dependency on stdenv;
-            posted a question
-            -> make fixed-output *after* unpacking
-              (to have same derivation even when stdenv/platform changes)
-              for that we would need to download all and generate hashes on our own
+          - make fixed-output *after* unpacking
+            (to have same derivation even when stdenv/platform changes)
+            for that we would need to download all and generate hashes on our own
           - "historic" isn't mirrored; posted a question at #287
           - maybe cache (some) collections? (they don't overlap)
       */
       runCommand "texlive-${pname}-${version}"
         { # lots of derivations, not meant to be cached
           preferLocalBuild = true; allowSubstitutes = false;
-          passthru = { inherit pname version; };
+          passthru = { inherit pname tlType version; };
         }
         ( ''
           mkdir "$out"
@@ -112,34 +113,8 @@ let
         );
 
   # combine a set of TL packages into a single TL meta-package
-  combinePkgs = pkgSet:
-    let
-      #getAttrOr = default: attr: set:
-      #  if lib.hasAttr attr set then lib.getAttr attr set else default;
-      makeFlat = attrName: lib.concatLists # uniqueness is handled in `combine`
-        (map (dep: lib.getAttr attrName dep.pkgs)
-          (lib.mapAttrsToList (_n: a: a) pkgSet));
-    in {
-      run = makeFlat "run";
-      doc = makeFlat "doc";
-      source = makeFlat "source";
-      bin = makeFlat "bin";
-    };
-
-  /*
-  unpackPkgs = lib.concatMapStrings (src: ''
-    echo "Unpacking '${src}'"
-  '');
-  */
-
-  bin-all = callPackage ./bin.nix {
-    poppler = poppler_nox; # otherwise depend on various X stuff
-    ghostscript = ghostscriptX;
-    harfbuzz = harfbuzz.override {
-      withIcu = true; withGraphite2 = true;
-    };
-  };
-  bin = bin-all.core;
+  combinePkgs = pkgSet: lib.concatLists # uniqueness is handled in `combine`
+    (lib.mapAttrsToList (_n: a: a.pkgs) pkgSet);
 
   # TODO: replace by buitin once it exists
   fastUnique = comparator: list: with lib;
@@ -148,144 +123,19 @@ let
     in un_adj (lib.sort comparator list);
 
 in
-   tl-flatDeps // rec {
-    inherit bin bin-all;
-
+  tl // rec {
     # TODO: remove
-    mine = combine { pkgSet = {
-      inherit (tl-flatDeps)
-        scheme-basic scheme-tetex
-        units algorithms
-        ;
-    }; };
+    mine = combine {
+      inherit (tl) scheme-small units algorithms cm-super;
+    };
 
     combined = lib.mapAttrs
       (pname: attrs: combine { ${pname} = attrs; })
-      { inherit (tl-flatDeps)
+      { inherit (tl)
           scheme-full scheme-medium scheme-small scheme-basic scheme-minimal
           scheme-context scheme-gust scheme-tetex scheme-xml;
       };
 
-    combine = args@{ pkgFilter ? (type: path: type == "run" || type == "bin" || path == bin.doc), ... }:
-      let
-        pkgSet = removeAttrs args ["pkgFilter"] // {
-          # include a fake "bin" package with docs for binaries
-          bin.pkgs = { run = []; doc = [ bin.doc ]; source = []; bin = [ bin.out ]; };
-        };
-        metaPkg = combinePkgs pkgSet;
-
-        mkUniquePkgs = pkgs: fastUnique (a: b: a < b) (map builtins.toPath pkgs);
-      in buildEnv {
-        name = "texlive-combined-${bin.year}";
-
-        extraPrefix = "/share/texmf";
-
-        ignoreCollisions = false;
-        paths = mkUniquePkgs (
-          [ ]
-          ++ lib.filter (pkgFilter "run"   ) metaPkg.run
-          ++ lib.filter (pkgFilter "doc"   ) metaPkg.doc
-          ++ lib.filter (pkgFilter "source") metaPkg.source
-        );
-
-        buildInputs = [ makeWrapper ];
-
-        postBuild = ''
-          cd "$out"
-          mkdir -p ./bin
-        '' +
-          lib.concatMapStrings
-            (path: ''
-              for f in '${path}'/bin/*; do
-                ln -s "$f" ./bin/
-              done
-            '')
-            (mkUniquePkgs (lib.filter (pkgFilter "bin") metaPkg.bin))
-          +
-        ''
-          export PATH="$out/bin:$out/share/texmf/scripts/texlive:${perl}/bin:$PATH"
-          export TEXMFDIST="$out/share/texmf"
-          export TEXMFSYSCONFIG="$out/share/texmf-config"
-          export TEXMFSYSVAR="$out/share/texmf-var"
-          export PERL5LIB="$out/share/texmf/scripts/texlive"
-        '' +
-          # patch texmf-dist -> texmf to be sure
-          # TODO: cleanup the search paths incl. SELFAUTOLOC, and perhaps do lua actions?
-          # tried inspiration from install-tl, sub do_texmf_cnf
-        ''
-          local cnfPath=./share/texmf/web2c/texmf.cnf
-          local cnfOrig="$(realpath $cnfPath)"
-          rm $cnfPath
-          cat "$cnfOrig" | sed 's/texmf-dist/texmf/g' > $cnfPath
-        '' +
-        # wrap created executables with required env vars
-        ''
-          wrapBin() {
-          for link in ./bin/*; do
-            [ -L "$link" -a -x "$link" ] || continue # if not link, assume OK
-            local target=$(readlink "$link")
-            case "$target" in
-              /*)
-                echo -n "Wrapping '$link'"
-                rm "$link"
-                makeWrapper "$target" "$link" \
-                  --prefix PATH : "$out/bin:${perl}/bin" \
-                  --set TEXMFDIST "$out/share/texmf" \
-                  --set TEXMFSYSCONFIG "$out/share/texmf-config" \
-                  --set TEXMFSYSVAR "$out/share/texmf-var" \
-                  --prefix PERL5LIB : "$out/share/texmf/scripts/texlive"
-
-                # avoid using non-nix shebang in $target by calling interpreter
-                if [[ "$(head -c 2 $target)" = "#!" ]]; then
-                  local interp="$(head -n 1 $target | sed 's/^\#\! *//;s/ *$//')"
-                  local newInterp=""
-                  case "$interp" in
-                    /bin/sh)
-                      newInterp="$(echo -n ${stdenv.shell} | sed 's/bash$/sh/' )";;
-                    /usr/bin/env\ perl|/usr/bin/perl)
-                      newInterp='${perl}/bin/perl';;
-                    /nix/store/*)
-                      echo
-                      continue;;
-                    *)
-                      echo "Unknown shebang '$interp' in '$target'"
-                      false
-                  esac
-                  echo " and patching shebang '$interp'"
-                  sed "s|^exec |exec $newInterp |" -i "$link"
-                else
-                  echo
-                fi
-                ;;
-            esac
-          done
-          }
-        '' +
-        # texlive post-install actions
-        ''
-          mkdir -p "$out/share/texmf/scripts/texlive/"
-          ln -s '${bin}/share/texmf-dist/scripts/texlive/TeXLive' "$out/share/texmf/scripts/texlive/"
-
-          for tool in updmap fmtutil; do
-            ln -sf "$out/share/texmf/scripts/texlive/$tool."* "$out/bin/$tool"
-          done
-          ln -sf fmtutil "$out/bin/mktexfmt"
-
-          perl `type -P mktexlsr.pl` ./share/texmf
-          texlinks.sh "$out/bin" && wrapBin
-          perl `type -P fmtutil.pl` --sys --all | grep '^fmtutil' # too verbose
-          #texlinks.sh "$out/bin" && wrapBin # may we need to run again?
-          yes | perl `type -P updmap.pl` --sys --syncwithtrees || true
-          yes | perl `type -P updmap.pl` --sys --syncwithtrees || true
-        '' +
-        # TODO: a context trigger https://www.preining.info/blog/2015/06/debian-tex-live-2015-the-new-layout/
-          # http://wiki.contextgarden.net/ConTeXt_Standalone#Unix-like_platforms_.28Linux.2FMacOS_X.2FFreeBSD.2FSolaris.29
-        ''
-          ln -s texmf/doc/{man,info} "$out/share/"
-        ''
-          + bin-all.cleanBrokenLinks
-        ;
-      };
-      # TODO: make TeX fonts visible by fontconfig: it should be enough to install an appropriate file
+    inherit combine;
   }
 
