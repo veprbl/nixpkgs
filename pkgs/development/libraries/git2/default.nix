f59f13d68ac8fcd3878eff95e6c9cde84e2f7d7c
Damien Cassou <damien@cassou.me>
2016-04-01 14:40:08 +0200

    libgit2: 0.23.2 -> 0.24.0

--- pkgs/development/libraries/git2/default.nix
+++ pkgs/development/libraries/git2/default.nix
@@ -1,13 +1,13 @@
 {stdenv, fetchurl, pkgconfig, cmake, zlib, python, libssh2, openssl, http-parser, libiconv}:
 
 stdenv.mkDerivation (rec {
-  version = "0.23.2";
+  version = "0.24.0";
   name = "libgit2-${version}";
 
   src = fetchurl {
     name = "${name}.tar.gz";
     url = "https://github.com/libgit2/libgit2/tarball/v${version}";
-    sha256 = "1d3901bmgvdnmzrx21afi1d0llsqmca3ckj942p0i2wpdpr1kbcp";
+    sha256 = "0yq0gadcq28v81zsvkynbdrcx3s9n2gvmj3ci4zvh9p7cc99rkgm";
   };
 
   cmakeFlags = "-DTHREADSAFE=ON";
