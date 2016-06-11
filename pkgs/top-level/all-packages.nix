f337447af1d189b9010000cdcbb218bdb4b46573
Dmitry Kalinkin <dkalinkin@bnl.gov>
2016-06-11 16:48:18 -0400

    rivet: init at 2.5.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16954,6 +16954,8 @@ in
 
   hepmc = callPackage ../development/libraries/physics/hepmc { };
 
+  rivet = callPackage ../development/libraries/physics/rivet { };
+
   yoda = callPackage ../development/libraries/physics/yoda { };
 
   ### MISC
