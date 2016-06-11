33e8aecfb01ce9c3daea82e5131a724de87dbbd0
Dmitry Kalinkin <dkalinkin@bnl.gov>
2016-06-11 17:12:00 -0400

    lhapdf: init at 6.1.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17001,6 +17001,8 @@ in
 
   hepmc = callPackage ../development/libraries/physics/hepmc { };
 
+  lhapdf = callPackage ../development/libraries/physics/lhapdf { };
+
   rivet = callPackage ../development/libraries/physics/rivet { };
 
   yoda = callPackage ../development/libraries/physics/yoda { };
