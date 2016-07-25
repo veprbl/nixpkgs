5d612942a4a6986689b60f1e8883f309f1abc6ac
Dmitry Kalinkin <dkalinkin@bnl.gov>
2016-07-25 02:00:51 -0400

    pythia: init at 8.219

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16958,6 +16958,8 @@ in
 
   lhapdf = callPackage ../development/libraries/physics/lhapdf { };
 
+  pythia = callPackage ../development/libraries/physics/pythia { };
+
   rivet = callPackage ../development/libraries/physics/rivet { };
 
   thepeg = callPackage ../development/libraries/physics/thepeg { };
