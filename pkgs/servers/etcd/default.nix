a3e4b22b5f57f03b5245bfbc3f0482e61ebc55d5
zimbatm <zimbatm@zimbatm.com>
2017-01-09 22:55:41 +0000

    etcd: 3.0.6 -> 3.0.15

--- pkgs/servers/etcd/default.nix
+++ pkgs/servers/etcd/default.nix
@@ -4,16 +4,15 @@ with lib;
 
 buildGoPackage rec {
   name = "etcd-${version}";
-  version = "3.0.6"; # After updating check that nixos tests pass
-  rev = "v${version}";
+  version = "3.0.15"; # After updating check that nixos tests pass
 
   goPackagePath = "github.com/coreos/etcd";
 
   src = fetchFromGitHub {
-    inherit rev;
     owner = "coreos";
     repo = "etcd";
-    sha256 = "163qji360y21nr1wnl16nbvvgdgqgbny4c3v3igp87q9p78sdf75";
+    rev = "v${version}";
+    sha256 = "0kd1yxy57m4w86s6z97iy324kxxbk9caymd973xzz8hy0m36rw9f";
   };
 
   goDeps = ./deps.nix;
