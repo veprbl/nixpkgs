81b1ab888be46fd1e9951d43a9490717ce321987
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2017-05-23 19:24:45 +0300

    yoda: build from hg

--- pkgs/development/libraries/physics/yoda/default.nix
+++ pkgs/development/libraries/physics/yoda/default.nix
@@ -1,28 +1,19 @@
-{ stdenv, fetchurl, fetchpatch, python2Packages, root, makeWrapper, withRootSupport ? false }:
+{ stdenv, fetchhg, autoreconfHook, python2Packages, root, makeWrapper, withRootSupport ? false }:
 
 stdenv.mkDerivation rec {
   name = "yoda-${version}";
-  version = "1.6.6";
+  version = "791a95f4453e";
 
-  src = fetchurl {
-    url = "http://www.hepforge.org/archive/yoda/YODA-${version}.tar.bz2";
-    sha256 = "088xx4q6b03bnj6xg5189m8wsznhal8aj3jk40sbj24idm4jl5yg";
+  src = fetchhg {
+    url = "https://yoda.hepforge.org/hg/yoda";
+    rev = version;
+    sha256 = "1adwl973i4k1nyji8rllvwbdd9vkizm7rwsgg8jqp45lpw0qwy3j";
   };
 
   pythonPath = []; # python wrapper support
 
-  patches = [
-    (fetchpatch {
-      url = "https://yoda.hepforge.org/hg/yoda/rev/3dbc8927e715?style=raw";
-      sha256 = "02rm34z9lbab66p7gpij12qwdph5fddpksg80qz0m537wjwy2ddy";
-    })
-    (fetchpatch {
-      url = "https://yoda.hepforge.org/hg/yoda/rev/669c2be582ef?style=raw";
-      sha256 = "0s705cl3bazpvpvy46vv1k223knwxq2yy5na1c6lv217sq9w86wj";
-    })
-  ];
-
-  buildInputs = with python2Packages; [ python numpy matplotlib makeWrapper ]
+  nativeBuildInputs = [ autoreconfHook ];
+  buildInputs = with python2Packages; [ cython python numpy matplotlib makeWrapper ]
     ++ stdenv.lib.optional withRootSupport root;
 
   enableParallelBuilding = true;
