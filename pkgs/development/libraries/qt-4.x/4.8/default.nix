9e768fc50bbdabffeb7c4f54463bb58ddf022004
Will Dietz <w@wdtz.org>
2017-06-01 08:31:55 -0500

    qt4: Add patch for socklen_t on musl, from alpine

--- pkgs/development/libraries/qt-4.x/4.8/default.nix
+++ pkgs/development/libraries/qt-4.x/4.8/default.nix
@@ -106,7 +106,8 @@ stdenv.mkDerivation rec {
     ++ stdenv.lib.optional stdenv.isAarch64 (fetchpatch {
         url = "https://src.fedoraproject.org/rpms/qt/raw/ecf530486e0fb7fe31bad26805cde61115562b2b/f/qt-aarch64.patch";
         sha256 = "1fbjh78nmafqmj7yk67qwjbhl3f6ylkp6x33b1dqxfw9gld8b3gl";
-      });
+      })
+    ++ stdenv.lib.optional stdenv.isMusl ./qt-musl.patch;
 
   preConfigure = ''
     export LD_LIBRARY_PATH="`pwd`/lib:$LD_LIBRARY_PATH"
