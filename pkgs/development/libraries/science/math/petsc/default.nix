32adcb71f6b03ee322b6bce9db9ac3dcb4c49c3b
Josef Kemetmueller <josef.kemetmueller@gmail.com>
2017-09-08 00:02:13 +0200

    petsc: Enable additional features
    This adds: openblas, openmpi, suitesparse, hwloc, scotch fftw, netcdf,
    hdf5, superlu and metis support

--- pkgs/development/libraries/science/math/petsc/default.nix
+++ pkgs/development/libraries/science/math/petsc/default.nix
@@ -1,9 +1,6 @@
-{ stdenv
-, fetchurl
-, blas
-, gfortran
-, liblapack
-, python }:
+{ stdenv, fetchurl
+, openblasCompat, openmpi, suitesparse, hwloc, scotch, fftw, netcdf, hdf5, superlu, metis
+, python2 }:
 
 stdenv.mkDerivation rec {
   name = "petsc-${version}";
@@ -14,20 +11,48 @@ stdenv.mkDerivation rec {
     sha256 = "0jfl35lrhzvv982z6h1v5rcp39g0x16ca43rm9dx91wm6i8y13iw";
   };
 
-  nativeBuildInputs = [ blas gfortran.cc.lib liblapack python ];
+  nativeBuildInputs = [ python2 ];
+
+  buildInputs = [
+    openblasCompat
+    openmpi
+    suitesparse
+    hwloc
+    scotch
+    fftw
+    netcdf
+    hdf5
+    superlu
+    metis
+  ];
 
   preConfigure = ''
-    patchShebangs .
-    configureFlagsArray=(
-      $configureFlagsArray
-      "--CC=$CC"
-      "--with-cxx=0"
-      "--with-fc=0"
-      "--with-mpi=0"
-      "--with-blas-lib=[${blas}/lib/libblas.a,${gfortran.cc.lib}/lib/libgfortran.a]"
-      "--with-lapack-lib=[${liblapack}/lib/liblapack.a,${gfortran.cc.lib}/lib/libgfortran.a]"
-    )
+    substituteInPlace config/install.py --replace "/usr/bin/install_name_tool" "install_name_tool"
   '';
+
+  configureFlags = let
+    sharedLibraryExtension = if stdenv.isDarwin then ".dylib" else ".so";
+  in [
+    "--with-fc=0"
+    "--with-debugging=0"
+    "--with-mpi=1"
+    # PETSc is not threadsafe, disable pthread/openmp (see http://www.mcs.anl.gov/petsc/miscellaneous/petscthreads.html)
+    "--with-pthread=0"
+    "--with-openmp=0"
+    "--with-ssl=0"
+    "--with-x=0"
+    "--with-blas-lapack-lib=${openblasCompat}/lib/libopenblas${sharedLibraryExtension}"
+    "--with-suitesparse-dir=${suitesparse}"
+    "--with-hwloc-dir=${hwloc}"
+    "--with-ptscotch-dir=${scotch}"
+    "--with-fftw-dir=${fftw}"
+    "--with-netcdf-dir=${netcdf}"
+    "--with-hdf5-dir=${hdf5}"
+    "--with-superlu-dir=${superlu}"
+    "--with-metis-dir=${metis}"
+  ];
+
+  doCheck = true;
 
   postInstall = ''
     rm $out/bin/petscmpiexec
