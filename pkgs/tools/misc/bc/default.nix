d285e52a19d82df44d0bdd4157c9f048e718c382
Ben Gamari <ben@smart-cactus.org>
2017-10-17 09:49:54 -0400

    bc: Mark appropriate buildInputs as nativeBuildInputs

--- pkgs/tools/misc/bc/default.nix
+++ pkgs/tools/misc/bc/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, flex, readline, ed, texinfo}:
+{stdenv, autoreconfHook, buildPackages, fetchurl, flex, readline, ed, texinfo}:
 
 stdenv.mkDerivation rec {
   name = "bc-1.07.1";
@@ -7,11 +7,22 @@ stdenv.mkDerivation rec {
     sha256 = "62adfca89b0a1c0164c2cdca59ca210c1d44c3ffc46daf9931cf4942664cb02a";
   };
 
-  configureFlags = [ "--with-readline" ];
+  configureFlags = [
+    "--with-readline"
+    "CC_FOR_BUILD=${buildPackages.stdenv.cc.targetPrefix}gcc"
+  ];
 
-  buildInputs = [flex readline ed texinfo];
+  # As of 1.07 cross-compilation is quite complicated as the build system wants
+  # to build a code generator, bc/fbc, on the build machine.
+  patches = [ ./cross-bc.patch ];
+  nativeBuildInputs = [autoreconfHook flex ed.out texinfo] ++
+    stdenv.lib.optionals (stdenv.hostPlatform != stdenv.buildPlatform)
+    [buildPackages.stdenv.cc buildPackages.readline.out buildPackages.ncurses.out];
+  buildInputs = [readline];
 
-  doCheck = true;
+  makeFlags = ''HOST_READLINE=${buildPackages.readline.out.outPath}/lib HOST_NCURSES=${buildPackages.ncurses.out.outPath}/lib'';
+
+  doCheck = stdenv.hostPlatform == stdenv.buildPlatform;
 
   meta = {
     description = "GNU software calculator";
