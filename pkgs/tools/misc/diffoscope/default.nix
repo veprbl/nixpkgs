ac162e7acb018c245a7b4ba742f3e3b58ebbe325
Will Dietz <w@wdtz.org>
2018-01-06 15:38:23 -0600

    diffoscope: 87 -> 90
    * Tests: enable, minor fix
      (squashfs test mentioned previously either
       no longer exists or is not a problem)
    * No need to generate manpage ourselves these days
    * rpm-python is no longer listed in setup.py

--- pkgs/tools/misc/diffoscope/default.nix
+++ pkgs/tools/misc/diffoscope/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchgit, python3Packages, docutils
+{ lib, stdenv, fetchgit, python3Packages
 , acl, apktool, libbfd, bzip2, cbfstool, cdrkit, colord, colordiff, coreutils, cpio, diffutils, dtc
 , e2fsprogs, file, findutils, fontforge-fonttools, fpc, gettext, ghc, ghostscriptX, giflib, gnupg1, gnutar
 , gzip, imagemagick, jdk, libarchive, libcaca, llvm, mono, openssh, pdftk, pgpdump, poppler_utils, sng, sqlite
@@ -8,24 +8,20 @@
 
 python3Packages.buildPythonApplication rec {
   name = "diffoscope-${version}";
-  version = "87";
+  version = "90";
 
   src = fetchgit {
     url    = "git://anonscm.debian.org/reproducible/diffoscope.git";
     rev    = "refs/tags/${version}";
-    sha256 = "0j3pljwmggrpaghbamvr24x4cg5yj7hl2ll27405p7970scnpngv";
+    sha256 = "1w16667j6ag2iim1xcy8y9v9965mq50k64wnf693mivddll62704";
   };
 
   patches = [
     ./ignore_links.patch
+    ./remove-sbin-test.patch
   ];
 
-  postPatch = ''
-    # Upstream doesn't provide a PKG-INFO file
-    sed -i setup.py -e "/'rpm-python',/d"
-  '';
-
-  # Still missing these tools: docx2txt enjarify js-beautify oggDump Rscript
+  # Still missing these tools: abootimg docx2txt enjarify js-beautify oggDump Rscript
   # Also these libraries: python3-guestfs
   pythonPath = with python3Packages; [ debian libarchive-c python_magic tlsh rpm ] ++ [
       acl libbfd bzip2 cdrkit colordiff coreutils cpio diffutils dtc e2fsprogs file findutils
@@ -34,14 +30,9 @@ python3Packages.buildPythonApplication rec {
     ] ++ lib.optionals enableBloat [
       apktool cbfstool colord fpc ghc ghostscriptX giflib gnupg1 imagemagick
       llvm jdk mono openssh pdftk poppler_utils tcpdump unoconv
-    ];
+    ] ++ lib.optional doCheck pytest;
 
-  doCheck = false; # Calls 'mknod' in squashfs tests, which needs root
-
-  postInstall = ''
-    mkdir -p $out/share/man/man1
-    ${docutils}/bin/rst2man.py debian/diffoscope.1.rst $out/share/man/man1/diffoscope.1
-  '';
+  doCheck = true; # Calls 'mknod' in squashfs tests, which needs root
 
   meta = with stdenv.lib; {
     description = "Perform in-depth comparison of files, archives, and directories";
