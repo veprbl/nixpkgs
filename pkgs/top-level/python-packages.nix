259d24545f3eaa00abc91bbb0284bdbaf9385321
Will Dietz <w@wdtz.org>
2018-01-11 11:50:07 -0600

    pysqlite: quickfix for cross

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14696,6 +14696,7 @@ in {
       substituteInPlace "setup.cfg"                                     \
               --replace "/usr/local/include" "${pkgs.sqlite.dev}/include"   \
               --replace "/usr/local/lib" "${pkgs.sqlite.out}/lib"
+      export LDSHARED="$CC -pthread -shared"
     '';
 
     meta = {
