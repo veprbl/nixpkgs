59115ffc19991c161e8cbb73af6cf464533d040c
Will Dietz <w@wdtz.org>
2018-01-11 16:07:01 -0600

    musl64: grab from gravity, requires netrc magic

--- pkgs/stdenv/linux/bootstrap-files/musl64.nix
+++ pkgs/stdenv/linux/bootstrap-files/musl64.nix
@@ -2,5 +2,9 @@
 (import ./i686.nix) //
 
 {
-  bootstrapTools = /nix/store/z54w017jsdyh54qk4g1mkql68lqwp90l-bootstrap-tools.tar.xz;
+  # bootstrapTools = /nix/store/z54w017jsdyh54qk4g1mkql68lqwp90l-bootstrap-tools.tar.xz;
+  bootstrapTools = import <nix/fetchurl.nix> {
+    url = https://gravity.cs.illinois.edu/build/2177106/download/1/bootstrap-tools.tar.xz;
+    sha256 = "eab723b47e516671c52fef9b11ac909d55a7745ed04dcc5ae294a1d974b144e0";
+  };
 }
