d07819f583f0c37f94b6945766d182aee832e166
Will Dietz <w@wdtz.org>
2018-01-13 14:21:53 -0600

    musl.nix: disable building unfree for now, so I don't waste time

--- musl.nix
+++ musl.nix
@@ -1,1 +1,1 @@
-import (fetchGit ./.) { localSystem = (import ./lib).systems.examples.musl64; }
+import (fetchGit ./.) { localSystem = (import ./lib).systems.examples.musl64; config = { allowUnfree = false; }; }
