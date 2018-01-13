afcc7b1119e52352aca448f71093cca1b01e3a5e
Will Dietz <w@wdtz.org>
2018-01-13 14:57:17 -0600

    systemd: patch to build on musl
    * many patches from OE, rebased from 237 to 238
      with a few minor touchups.
    * fix use of strerror_r
    * (accepted upstream) tweak script extracting AF_*
    * (upstream issue, WIP) don't hardcode signal number

--- pkgs/os-specific/linux/systemd/default.nix
+++ pkgs/os-specific/linux/systemd/default.nix
@@ -92,7 +92,30 @@ in stdenv.mkDerivation rec {
     "-Dsulogin-path=${utillinux}/bin/sulogin"
     "-Dmount-path=${utillinux}/bin/mount"
     "-Dumount-path=${utillinux}/bin/umount"
+  ] ++ stdenv.lib.optionals stdenv.hostPlatform.isMusl [
+    "-Dnss-systemd=false"
+    "-Dlocaled=false"
+    "-Dresolve=false"
+    "-Dutmp=false"
+    "-Dmyhostname=false"
   ];
+
+  patches = [
+    ./fix-for-utillinux-2.32.patch
+    ./dont-hardcode-sigrt-relative.patch
+  ] ++ stdenv.lib.optionals hostPlatform.isMusl (
+    let systemd_rev = "c58ab03f64890e7db88745a843bd4520e307099b"; # v238-stable
+  in [
+    (fetchpatch {
+      url = "https://github.com/dtzWill/systemd/compare/${systemd_rev}...238-musl-2.patch";
+      sha256 = "13v7yzyifilb41jjjmwh3vlfaw6la5ilhrxbmipif3p69pbm355f";
+    })
+  ]);
+
+  postPatch = ''
+    substituteInPlace ./src/basic/generate-af-list.sh \
+      --replace 'PF_' '[AP]F_'
+  '';
 
   preConfigure = ''
     mesonFlagsArray+=(-Dntp-servers="0.nixos.pool.ntp.org 1.nixos.pool.ntp.org 2.nixos.pool.ntp.org 3.nixos.pool.ntp.org")
