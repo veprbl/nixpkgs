e455812ee4b9ba4c761e30596f4fbb5f2e406ad6
Will Dietz <w@wdtz.org>
2018-01-14 15:33:54 -0600

    smartmontools: prefer patch from upstream project

--- pkgs/tools/system/smartmontools/default.nix
+++ pkgs/tools/system/smartmontools/default.nix
@@ -21,10 +21,12 @@ in stdenv.mkDerivation rec {
   };
 
   patches = [ ./smartmontools.patch ]
+    # https://www.smartmontools.org/changeset/4603
     ++ stdenv.lib.optional stdenv.isMusl (fetchpatch {
       name = "musl-canonicalize_file_name.patch";
-      url = "https://git.alpinelinux.org/cgit/aports/plain/main/smartmontools/musl-canonicalize_file_name.patch?id=ad1dc3afc60b2a8efb7b9c1f59a02c4fd2c66c6f";
-      sha256 = "101yqmfpfry9fzm4ycj1zirwiwbifqs54q9xsd3xkji14dj7zdai";
+      url = "https://www.smartmontools.org/changeset/4603?format=diff&new=4603";
+      sha256 = "06s9pcd95snjkrbfrsjby2lln3lnwjd21bgabmvr4p7fx19b75zp";
+      stripLen = 2;
     });
   postPatch = "cp -v ${driverdb} drivedb.h";
 
