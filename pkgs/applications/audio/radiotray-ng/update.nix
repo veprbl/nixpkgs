77a05e1d44c232c9dd7c9e7151f22a8c62c1e98c
Will Dietz <w@wdtz.org>
2018-01-13 20:21:06 -0600

    latestGitHubRelease: sort tags, filter out preleases

--- pkgs/applications/audio/radiotray-ng/update.nix
+++ pkgs/applications/audio/radiotray-ng/update.nix
@@ -3,13 +3,12 @@
 , common-updater-scripts
 }:
 attrPath: { owner, repo, rev, ... }:
-  # TODO:
-  # * don't assume first entry is newer
-  # * semver compare for newest, probably use something in lib.
   let
-    releases = builtins.fetchurl "https://api.github.com/repos/${owner}/${repo}/releases";
-    releaseJSON = builtins.fromJSON (builtins.readFile releases);
-    tag = (builtins.head releaseJSON).tag_name;
+    json = builtins.fetchurl "https://api.github.com/repos/${owner}/${repo}/releases";
+    all_releases = builtins.fromJSON (builtins.readFile json);
+    releases_unsorted = builtins.filter (r: !r.prerelease) all_releases;
+    releases = lib.sort (a: b: lib.versionOlder b.tag_name a.tag_name) releases_unsorted;
+    tag = (builtins.head releases).tag_name;
     version = lib.removePrefix "v" tag;
   in
   writeScript "update-${attrPath}" ''
