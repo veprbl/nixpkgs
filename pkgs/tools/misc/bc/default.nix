d266dab003425550569796afdfe4f41f90420902
Will Dietz <w@wdtz.org>
2018-01-13 22:37:09 -0600

    bc: cleanup and simplify thanks to reviewer feedback \o/

--- pkgs/tools/misc/bc/default.nix
+++ pkgs/tools/misc/bc/default.nix
@@ -12,14 +12,15 @@ stdenv.mkDerivation rec {
   # As of 1.07 cross-compilation is quite complicated as the build system wants
   # to build a code generator, bc/fbc, on the build machine.
   patches = [ ./cross-bc.patch ];
-  nativeBuildInputs = [autoreconfHook flex ed.out texinfo] ++
-    stdenv.lib.optionals (stdenv.hostPlatform != stdenv.buildPlatform)
-    [buildPackages.stdenv.cc buildPackages.readline.out buildPackages.ncurses.out];
+  depsBuildBuild = [ buildPackages.stdenv.cc ];
+  nativeBuildInputs = [autoreconfHook flex ed.out texinfo]
+    ++ # needed for cross, see makeFlags below
+    [buildPackages.readline.out buildPackages.ncurses.out];
   buildInputs = [readline];
 
   makeFlags = ''HOST_READLINE=${buildPackages.readline.out.outPath}/lib HOST_NCURSES=${buildPackages.ncurses.out.outPath}/lib'';
 
-  doCheck = stdenv.hostPlatform == stdenv.buildPlatform;
+  doCheck = true;
 
   meta = {
     description = "GNU software calculator";
