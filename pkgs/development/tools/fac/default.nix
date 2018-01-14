1a282aac350d326649940cf52dcc0174ae0d5b6c
Will Dietz <w@wdtz.org>
2018-01-13 20:38:36 -0600

    fac: latestGitHubRelease support

--- pkgs/development/tools/fac/default.nix
+++ pkgs/development/tools/fac/default.nix
@@ -1,24 +1,29 @@
-{ stdenv, buildGoPackage, fetchFromGitHub, makeWrapper, git }:
+{ stdenv, buildGoPackage, fetchFromGitHub, makeWrapper, git, latestGitHubRelease }:
 
-buildGoPackage rec {
-  name = "fac-${version}";
+let
   version = "1.0.1";
-
-  goPackagePath = "github.com/mkchoi212/fac";
-
-  src = fetchFromGitHub {
+  srcinfo = {
     owner = "mkchoi212";
     repo = "fac";
     rev = "v${version}";
     sha256 = "1j5kip3l3p9qlly03pih905sdz3ncvpj7135jpnfhckbk1s5x9dc";
   };
 
+in buildGoPackage rec {
+  name = "fac-${version}";
+  inherit version;
+
+  goPackagePath = "github.com/mkchoi212/fac";
+
+  src = fetchFromGitHub srcinfo;
   nativeBuildInputs = [ makeWrapper ];
 
   postInstall = ''
     wrapProgram $bin/bin/fac \
       --prefix PATH : ${git}/bin
   '';
+
+  passthru.updateScript = latestGitHubRelease "fac" srcinfo;
 
   meta = with stdenv.lib; {
     description = "CUI for fixing git conflicts";
