7067c59e8d285be2985bbfbc61b9d4cc7581992a
Will Dietz <w@wdtz.org>
2018-01-13 20:22:20 -0600

    micro: use latestGitHubRelease too

--- pkgs/applications/editors/micro/default.nix
+++ pkgs/applications/editors/micro/default.nix
@@ -1,22 +1,27 @@
-{ stdenv, buildGoPackage, fetchFromGitHub }:
+{ stdenv, buildGoPackage, fetchFromGitHub, latestGitHubRelease }:
 
-buildGoPackage  rec {
-  name = "micro-${version}";
+let
   version = "1.3.4";
-
-  goPackagePath = "github.com/zyedidia/micro";
-
-  src = fetchFromGitHub {
+  srcinfo = {
     owner = "zyedidia";
     repo = "micro";
     rev = "v${version}";
     sha256 = "1giyp2xk2rb6vdyfnj5wa7qb9fwbcmmwm16wdlnmq7xnp7qamdkw";
     fetchSubmodules = true;
   };
+in buildGoPackage  rec {
+  name = "micro-${version}";
+  inherit version;
+
+  goPackagePath = "github.com/zyedidia/micro";
+
+  src = fetchFromGitHub srcinfo;
 
   subPackages = [ "cmd/micro" ];
 
   buildFlagsArray = [ "-ldflags=" "-X main.Version=${version}" ];
+
+  passthru.updateScript = latestGitHubRelease "micro" srcinfo;
 
   meta = with stdenv.lib; {
     homepage = https://micro-editor.github.io;
