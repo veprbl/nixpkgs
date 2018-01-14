773c7bd89697d87c4d0fe82ce1a0615b622798b6
Will Dietz <w@wdtz.org>
2018-01-14 15:31:19 -0600

    smartmontools: patch from alpine, upstream

--- pkgs/tools/system/smartmontools/default.nix
+++ pkgs/tools/system/smartmontools/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, autoreconfHook
+{ stdenv, fetchurl, fetchpatch, autoreconfHook
 , IOKit ? null , ApplicationServices ? null }:
 
 let
@@ -20,7 +20,12 @@ in stdenv.mkDerivation rec {
     sha256 = "0m1hllbb78rr6cxkbalmz1gqkl0psgq8rrmv4gwcmz34n07kvx2i";
   };
 
-  patches = [ ./smartmontools.patch ];
+  patches = [ ./smartmontools.patch ]
+    ++ stdenv.lib.optional stdenv.isMusl (fetchpatch {
+      name = "musl-canonicalize_file_name.patch";
+      url = "https://git.alpinelinux.org/cgit/aports/plain/main/smartmontools/musl-canonicalize_file_name.patch?id=ad1dc3afc60b2a8efb7b9c1f59a02c4fd2c66c6f";
+      sha256 = "101yqmfpfry9fzm4ycj1zirwiwbifqs54q9xsd3xkji14dj7zdai";
+    });
   postPatch = "cp -v ${driverdb} drivedb.h";
 
   nativeBuildInputs = [ autoreconfHook ];
