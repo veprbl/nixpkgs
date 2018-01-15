675b9683c499163543b1b2950833c4e293f17fc2
Will Dietz <w@wdtz.org>
2018-01-14 19:07:06 -0600

    iproute: don't use host BASH for scripts, fix for cross
    Doesn't eliminate glibc dep, somehow
    a dependency on host's "db" leaks,
    but it's a start.
    
    Also don't attempt to build unused 'netem' tools,
    avoiding need to use HOSTCC.

--- pkgs/os-specific/linux/iproute/default.nix
+++ pkgs/os-specific/linux/iproute/default.nix
@@ -1,4 +1,4 @@
-{ fetchurl, fetchgit, stdenv, lib, flex, bison, db, iptables, pkgconfig }:
+{ fetchurl, fetchgit, stdenv, lib, flex, bash, bison, db, iptables, pkgconfig }:
 
 stdenv.mkDerivation rec {
   name = "iproute2-${version}";
@@ -13,6 +13,8 @@ stdenv.mkDerivation rec {
   preConfigure = ''
     patchShebangs ./configure
     sed -e '/ARPDDIR/d' -i Makefile
+    # Don't build netem tools--they're not installed and require HOSTCC
+    substituteInPlace Makefile --replace " netem " " "
   '';
 
   makeFlags = [
@@ -33,10 +35,14 @@ stdenv.mkDerivation rec {
     "CONFDIR=$(out)/etc/iproute2"
   ];
 
-  buildInputs = [ db iptables ];
+  buildInputs = [ bash db iptables ];
   nativeBuildInputs = [ bison flex pkgconfig ];
 
   enableParallelBuilding = true;
+
+  postInstall = ''
+    PATH=${bash}/bin:$PATH patchShebangs $out/sbin
+  '';
 
   meta = with stdenv.lib; {
     homepage = https://wiki.linuxfoundation.org/networking/iproute2;
