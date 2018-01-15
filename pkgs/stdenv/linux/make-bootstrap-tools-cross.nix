776ed8a337268dc58084535892f9fc3f1c8ec5dd
Will Dietz <w@wdtz.org>
2018-01-15 11:20:33 -0600

    make-bootstrap-tools-cross: Add entry for armv6-musl, rename

--- pkgs/stdenv/linux/make-bootstrap-tools-cross.nix
+++ pkgs/stdenv/linux/make-bootstrap-tools-cross.nix
@@ -7,11 +7,12 @@ let
   };
 
 in with (import ../../../lib).systems.examples; {
-  armv5tel  = make sheevaplug;
-  scaleway  = make scaleway-c1;
-  pogoplug4 = make pogoplug4;
-  armv6l    = make raspberryPi;
-  armv7l    = make armv7l-hf-multiplatform;
-  aarch64   = make aarch64-multiplatform;
-  musl      = make musl64;
+  armv5tel   = make sheevaplug;
+  scaleway   = make scaleway-c1;
+  pogoplug4  = make pogoplug4;
+  armv6l     = make raspberryPi;
+  armv7l     = make armv7l-hf-multiplatform;
+  aarch64    = make aarch64-multiplatform;
+  amd64musl  = make musl64;
+  armv6lmusl = make muslpi;
 }
