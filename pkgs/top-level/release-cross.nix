988c7f1044373a5ca3804a13945dc416be800bdf
Will Dietz <w@wdtz.org>
2018-01-15 11:22:42 -0600

    release-cross: rename, add muslpi

--- pkgs/top-level/release-cross.nix
+++ pkgs/top-level/release-cross.nix
@@ -125,7 +125,16 @@ in
   android = mapTestOnCross lib.systems.examples.aarch64-multiplatform (linuxCommon // {
   });
 
-  musl = mapTestOnCross lib.systems.examples.musl64 linuxCommon;
+  musl64 = mapTestOnCross lib.systems.examples.musl64 linuxCommon;
+  muslpi = mapTestOnCross lib.systems.examples.muslpi  (linuxCommon // {
+    vim = nativePlatforms;
+    unzip = nativePlatforms;
+    ddrescue = nativePlatforms;
+    lynx = nativePlatforms;
+    patchelf = nativePlatforms;
+    buildPackages.binutils = nativePlatforms;
+    mpg123 = nativePlatforms;
+  });
 
   /* Cross-built bootstrap tools for every supported platform */
   bootstrapTools = let
