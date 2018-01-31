f201f0624b95c1ae95020767cb375c237baba949
Will Dietz <w@wdtz.org>
2018-01-31 10:17:51 -0600

    bootstrap-files/musl64: grab from wdtz.org, temporarily host there

--- pkgs/stdenv/linux/bootstrap-files/musl64.nix
+++ pkgs/stdenv/linux/bootstrap-files/musl64.nix
@@ -3,8 +3,8 @@
 
 {
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = https://gravity.cs.illinois.edu/build/2177137/download/1/bootstrap-tools.tar.xz;
-    sha256 = "eab723b47e516671c52fef9b11ac909d55a7745ed04dcc5ae294a1d974b144e0";
-    # path: /nix/store/y4w65vk57lrf8z3li3dqzz6bsq6761lb-stdenv-bootstrap-tools/on-server/bootstrap-tools.tar.xz
+    # XXX: Find a permanent location for this
+    url = https://wdtz.org/files/5zfs7s729n4lrlxmhlnc6qmfrlhahy9s-stdenv-bootstrap-tools-x86_64-unknown-linux-musl/on-server/bootstrap-tools.tar.xz;
+    sha256 = "0lwi08c2v7ip2z9li597ixywix976561hr358z2fbd6sqi943axl";
   };
 }
