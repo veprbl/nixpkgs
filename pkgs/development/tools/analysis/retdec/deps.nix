7b954c7cf5835f1d1d234437d3978798b9cd121b
Will Dietz <w@wdtz.org>
2018-02-02 18:20:43 -0600

    retdec: generate dependency info!

---
+++ pkgs/development/tools/analysis/retdec/deps.nix
@@ -0,0 +1,50 @@
+{
+  "capstone" = {
+    url = "https://github.com/avast-tl/capstone/archive/27c713fe4f6eaf9721785932d850b6291a6073fe.zip";
+    sha256 = "4d8d0461d7d5737893253698cd0b6d0d64545c1a74b166e8b1d823156a3109cb";
+  };
+  "elfio" = {
+    url = "https://github.com/avast-tl/elfio/archive/1677a88d902d0fa6a5a7a31dc4da45748bba0bde.zip";
+    sha256 = "a446b2751b177bde6c5bff5a3716a978918daba6f4953aeef3850036cb43fde7";
+  };
+  "googletest" = {
+    url = "https://github.com/google/googletest/archive/83fa0cb17dad47a1d905526dcdddb5b96ed189d2.zip";
+    sha256 = "3b7cf6bfd1fdec3204933b4c0419c010e89b2409dcd8cbc1ac6a78aab058e2b0";
+  };
+  "jsoncpp" = {
+    url = "https://github.com/open-source-parsers/jsoncpp/archive/1.8.4.zip";
+    sha256 = "2979436dbd4c48a3284dca9fa8f212298425ba3920ed6bacdda8905a94b111a8";
+  };
+  "keystone" = {
+    url = "https://github.com/avast-tl/keystone/archive/4722974f58dbe878c805ce2b7c0a41cf6153903c.zip";
+    sha256 = "145c8bb836d2f06f845b5015b3eab350ae59d35a54052f00f5d26445172415fd";
+  };
+  "libdwarf" = {
+    url = "https://github.com/avast-tl/libdwarf/archive/85465d5e235cc2d2f90d04016d6aca1a452d0e73.zip";
+    sha256 = "2864aa7b46529778476190e90359669eb35799ad273233c4df2203bec7db0738";
+  };
+  "llvm" = {
+    url = "https://github.com/avast-tl/llvm/archive/257248365d034351e164aae04060f0d8f4c5b1f1.zip";
+    sha256 = "0088e715fd92d9670398514791563f1efc727d5a81b5cac8830c7e746ab26c06";
+  };
+  "pelib" = {
+    url = "https://github.com/avast-tl/pelib/archive/e93eaa7c150f4608a5d02a67f5edc9e54456fe24.zip";
+    sha256 = "2ffd7e89451c980a1af6d24d4f6dfbb69a660b06ad5de44c481f6431e21de394";
+  };
+  "rapidjson" = {
+    url = "https://github.com/Tencent/rapidjson/archive/v1.1.0.zip";
+    sha256 = "8e00c38829d6785a2dfb951bb87c6974fa07dfe488aa5b25deec4b8bc0f6a3ab";
+  };
+  "tinyxml2" = {
+    url = "https://github.com/leethomason/tinyxml2/archive/cc1745b552dd12bb1297a99f82044f83b06729e0.zip";
+    sha256 = "de255ce39ba1efe20cbffdebd3eea2d08f1269d03440a4fd60e142b0481c3f00";
+  };
+  "yaracpp" = {
+    url = "https://github.com/avast-tl/yaracpp/archive/v1.0.1.zip";
+    sha256 = "2252ba7cfd9b190d3e260a741bbd2abc0fbd7c0422eb13d878278a89016c4fda";
+  };
+  "yaramod" = {
+    url = "https://github.com/avast-tl/yaramod/archive/v1.0.1.zip";
+    sha256 = "20793f99bd1a22e9b743400653d54790a48457ef0ae1cf8f089a1bf3bf38f339";
+  };
+}
