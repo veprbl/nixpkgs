670b27e4eb78f6c18d6e68158f9e9dea0eeb4712
Will Dietz <w@wdtz.org>
2018-02-09 15:01:09 -0600

    appstream-glib: 0.7.2 -> 0.7.6

--- pkgs/development/libraries/appstream-glib/default.nix
+++ pkgs/development/libraries/appstream-glib/default.nix
@@ -4,13 +4,13 @@
 , libuuid, json_glib, meson, gperf, ninja
 }:
 stdenv.mkDerivation rec {
-  name = "appstream-glib-0.7.2";
+  name = "appstream-glib-0.7.6";
 
   src = fetchFromGitHub {
     owner = "hughsie";
     repo = "appstream-glib";
     rev = stdenv.lib.replaceStrings ["." "-"] ["_" "_"] name;
-    sha256 = "1jvwfida12d2snc8p9lpbpqzrixw2naaiwfmsrldwkrxsj3i19pl";
+    sha256 = "1nzm6w9n7fb2m06w88gwszaqf74bnip87ay0ca59wajq6y4mpfgv";
   };
 
   nativeBuildInputs = [ meson pkgconfig ninja ];
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
                   gcab attr acl docbook_xsl libuuid json_glib
                   libarchive gobjectIntrospection gperf ];
   propagatedBuildInputs = [ gtk3 ];
-  mesonFlags = [ "-Denable-rpm=false" "-Denable-stemmer=false" "-Denable-dep11=false" ];
+  mesonFlags = [ "-Drpm=false" "-Dstemmer=false" "-Ddep11=false" ];
 
   meta = with stdenv.lib; {
     description = "Objects and helper methods to read and write AppStream metadata";
