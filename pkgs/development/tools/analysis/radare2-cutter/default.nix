fb663a331cbb58f05865292af88c9a2d856fe8e4
Will Dietz <w@wdtz.org>
2018-03-03 12:30:36 -0600

    radare2-cutter: try latest r2 from git, requires newer capstone too

--- pkgs/development/tools/analysis/radare2-cutter/default.nix
+++ pkgs/development/tools/analysis/radare2-cutter/default.nix
@@ -1,16 +1,31 @@
-{ stdenv, fetchFromGitHub, fetchpatch, qmake, pkgconfig, qtbase, qtsvg, radare2 }:
+{ stdenv, fetchFromGitHub, fetchgit, fetchpatch, qmake, pkgconfig, qtbase, qtsvg, radare2 }:
 
 let
   # Pinned version, submodule
   r2-git = radare2.overrideAttrs(o: rec {
     name = "radare2-for-cutter-${version}";
-    version = "2018-02-26";
+    version = "2018-03-03";
     src = fetchFromGitHub {
       owner = "radare";
       repo = "radare2";
-      rev = "70bd99da259438fbe552b7debfe68c571cdcd2da";
-      sha256 = "1x08xskcz0skhkbgfd98sm7qq6jkniaayavxqjh20gxxcmyhhzfx";
+      rev = "00668df334800762038a77ca666ef46d378650f8";
+      sha256 = "07shnhgjfhzyra4nkfzjy0a2smszk1m1w8wawsgbmdxb6l118yvn";
     };
+
+    # Copied from r2, but with updated capstone
+    postPatch = let
+      cs_tip = "4a1b580d069c82d60070d0869a87000db7cdabe2"; # version from $sourceRoot/shlr/Makefile
+      capstone = fetchgit {
+        url = "https://github.com/aquynh/capstone.git";
+        rev = cs_tip;
+        sha256 = "1b126npshdbwh5y7rafmb9w4dzlvxsf4ca6bx4zs2y7kbk48jyn8";
+        leaveDotGit = true;
+      };
+    in ''
+      if ! grep -F "CS_TIP=${cs_tip}" shlr/Makefile; then echo "CS_TIP mismatch"; grep CS_TIP= shlr/Makefile; exit 1; fi
+      cp -r ${capstone} shlr/capstone
+      chmod -R u+rw shlr/capstone
+    '';
   });
 in
 stdenv.mkDerivation rec {
