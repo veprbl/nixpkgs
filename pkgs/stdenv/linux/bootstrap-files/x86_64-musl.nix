3b5a58eb623ec9a39c6d6a870f966d466c799725
Will Dietz <w@wdtz.org>
2018-03-05 15:08:35 -0600

    use regenerated musl bootstrap? hmm

--- pkgs/stdenv/linux/bootstrap-files/x86_64-musl.nix
+++ pkgs/stdenv/linux/bootstrap-files/x86_64-musl.nix
@@ -4,8 +4,9 @@
     sha256 = "16lzrwwvdk6q3g08gs45pldz0rh6xpln2343xr444960h6wqxl5v";
     executable = true;
   };
-  bootstrapTools = import <nix/fetchurl.nix> {
-    url = https://wdtz.org/files/030q34q7fk6jdfxkgcqp5rzr4yhw3pgx-stdenv-bootstrap-tools-x86_64-unknown-linux-musl/on-server/bootstrap-tools.tar.xz;
-    sha256 = "0ly0wj8wzbikn2j8sn727vikk90bq36drh98qvfx1kkh5k5azm2j";
-  };
+  bootstrapTools = /nix/store/2m15z3pmg495w52jc8dg2nbxxzmzvb04-stdenv-bootstrap-tools/on-server/bootstrap-tools.tar.xz;
+  #bootstrapTools = import <nix/fetchurl.nix> {
+  #  url = https://wdtz.org/files/030q34q7fk6jdfxkgcqp5rzr4yhw3pgx-stdenv-bootstrap-tools-x86_64-unknown-linux-musl/on-server/bootstrap-tools.tar.xz;
+  #  sha256 = "0ly0wj8wzbikn2j8sn727vikk90bq36drh98qvfx1kkh5k5azm2j";
+  #};
 }
