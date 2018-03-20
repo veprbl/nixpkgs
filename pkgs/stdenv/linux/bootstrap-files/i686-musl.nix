4fcc1131816e52a63d49e78f813cfae324c6aab0
Will Dietz <w@wdtz.org>
2018-03-20 12:32:37 -0500

    i686-musl: fix hashes

--- pkgs/stdenv/linux/bootstrap-files/i686-musl.nix
+++ pkgs/stdenv/linux/bootstrap-files/i686-musl.nix
@@ -2,11 +2,11 @@
 {
   busybox = import <nix/fetchurl.nix> {
     url = https://wdtz.org/files/nr8xhx04mx7m2j8gnyif00vbk8jw3zh4-stdenv-bootstrap-tools-i686-unknown-linux-musl/on-server/busybox;
-    sha256 = "16lzrwwvdk6q3g08gs45pldz0rh6xpln2343xr444960h6wqxl6v";
+    sha256 = "1cnayg52s731acmy8phdpvfivjri5qgypsyp57hx97wrymijdzjp";
     executable = true;
   };
   bootstrapTools = import <nix/fetchurl.nix> {
     url = https://wdtz.org/files/nr8xhx04mx7m2j8gnyif00vbk8jw3zh4-stdenv-bootstrap-tools-i686-unknown-linux-musl/on-server/bootstrap-tools.tar.xz;
-    sha256 = "0ly0wj8wzbikn2j8sn727vikk90bq36drh98qvfx1kkh5k5azm3j";
+    sha256 = "00gwpw4b8vryrbmfmxr40mvcc3m2pk2wmycn86vi086pr9gcqx07";
   };
 }
