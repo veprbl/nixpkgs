ea4069a76c43045813083a3e225e3be5d0412b1a
Will Dietz <w@wdtz.org>
2018-03-23 18:09:27 -0500

    add missing file

---
+++ nixos/tests/simple-musl.nix
@@ -0,0 +1,27 @@
+import ./make-test.nix ({ pkgs, ...} : {
+  name = "simple";
+  meta = with pkgs.stdenv.lib.maintainers; {
+    maintainers = [ eelco ];
+  };
+
+  machine = { config, pkgs, ... }: {
+    imports = [ ../modules/profiles/minimal.nix ];
+
+    #nixpkgs.pkgs = import ../.. {
+    #  inherit (config.nixpkgs) config overlays;#  system;
+    #  localSystem.config = "x86_64-unknown-linux-musl";
+    #};
+    nixpkgs.localSystem.config = "x86_64-unknown-linux-musl";
+
+    #i18n.glibcLocales = pkgs.musl;
+    #i18n.glibcLocales = (import (fetchTarball channel:nixos-unstable) {}).glibcLocales;
+    i18n.glibcLocales = pkgs.stdenv.bootstrapTools;
+  };
+
+  testScript =
+    ''
+      startAll;
+      $machine->waitForUnit("multi-user.target");
+      $machine->shutdown;
+    '';
+})
