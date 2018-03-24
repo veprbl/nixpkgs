52eba9753aeba4f02c8ce0de50f10bd98de1ef1e
Will Dietz <w@wdtz.org>
2018-03-24 15:33:47 -0500

    nixos: use pkgs.getent and stdenv.cc.libc

--- nixos/modules/system/activation/activation-script.nix
+++ nixos/modules/system/activation/activation-script.nix
@@ -21,7 +21,8 @@ let
     [ coreutils
       gnugrep
       findutils
-      glibc # needed for getent
+      #glibc # needed for getent
+      pkgs.getent
       shadow
       nettools # needed for hostname
       utillinux # needed for mount and mountpoint
