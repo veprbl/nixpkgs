65d3da93e20418a95c0b9576ff7723da694cd09a
Will Dietz <w@wdtz.org>
2018-03-27 16:59:23 -0500

    stage-1: hmm maybe just always use utillinux's mount/fsck?

--- nixos/modules/config/musl.nix
+++ nixos/modules/config/musl.nix
@@ -6,11 +6,4 @@ with lib;
   # Musl
   nixpkgs.localSystem.config = "x86_64-unknown-linux-musl";
   i18n.glibcLocales = pkgs.musl; # blah
-  boot.initrd = {
-    extraUtilsCommands = ''
-      for BIN in ${pkgs.utillinux}/bin/*; do
-        copy_bin_and_libs $BIN
-      done
-    '';
-  };
 }
