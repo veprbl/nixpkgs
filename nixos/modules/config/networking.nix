9bbe75e1873c3443576addd6a9f5915fd0e7f1c8
Will Dietz <w@wdtz.org>
2018-03-27 16:29:41 -0500

    nixos/networking: use freebsd /etc/rpc if not using glibc

--- nixos/modules/config/networking.nix
+++ nixos/modules/config/networking.nix
@@ -198,7 +198,14 @@ in
         "protocols".source  = pkgs.iana-etc + "/etc/protocols";
 
         # /etc/rpc: RPC program numbers.
-        "rpc".source = pkgs.glibc.out + "/etc/rpc";
+        # TODO: generate and bundle as part of iana-etc
+        "rpc".source =
+          if pkgs.stdenv.hostPlatform.libc == "glibc" then
+            pkgs.glibc.out + "/etc/rpc"
+            else builtins.fetchurl {
+              url = https://raw.githubusercontent.com/freebsd/freebsd/91f34482ca182b474992ae5c488e1c4099b921ed/etc/rpc;
+              sha256 = "0vnyrlazgjy0piw0cyd6qhw8mci3r9nx5ns3fr0bi1n7flqx1m3a";
+            };
 
         # /etc/hosts: Hostname-to-IP mappings.
         "hosts".text =
