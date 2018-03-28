30a8708a57e3b8745efe3bff4b3b114474d3438a
Will Dietz <w@wdtz.org>
2018-03-28 10:25:26 -0500

    lvm2: cleanup

--- pkgs/os-specific/linux/lvm2/default.nix
+++ pkgs/os-specific/linux/lvm2/default.nix
@@ -75,9 +75,6 @@ stdenv.mkDerivation {
       cp scripts/blk_availability_systemd_red_hat.service $out/etc/systemd/system
       cp scripts/lvm2_activation_generator_systemd_red_hat $out/lib/systemd/system-generators
     '';
-    #+ stdenv.lib.optionalString stdenv.hostPlatform.isMusl ''
-    #  substituteInPlace $out/etc/lvm.conf --replace 
-    #'';
 
   meta = {
     homepage = http://sourceware.org/lvm2/;
