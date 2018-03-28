05883178910170ef5529cd25fffaa19ec15a13d6
Will Dietz <w@wdtz.org>
2018-03-28 12:56:08 -0500

    Revert "driver-paths"
    This reverts commit af6560b96c5132cbeca0fadb0fe534f62175bcee.

--- pkgs/servers/x11/xorg/overrides.nix
+++ pkgs/servers/x11/xorg/overrides.nix
@@ -477,8 +477,7 @@ in
         libXfont2
       ];
       # fix_segfault: https://bugs.freedesktop.org/show_bug.cgi?id=91316
-      # driver paths: https://cgit.freedesktop.org/xorg/xserver/patch/?id=319af6f471912160ab3eb6395ef50f9950063d43
-      commonPatches = [ ./xorg-server-1.12-unloadsubmodule.patch ./driver-paths.patch ];
+      commonPatches = [ ./xorg-server-1.12-unloadsubmodule.patch ];
 
       # XQuartz requires two compilations: the first to get X / XQuartz,
       # and the second to get Xvfb, Xnest, etc.
