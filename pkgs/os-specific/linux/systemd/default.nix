8da04ebd260c7bc96d35712e8e0a07541d887225
Will Dietz <w@wdtz.org>
2018-03-29 12:37:04 -0500

    systemd-musl: -4, util-linux fix for update

--- pkgs/os-specific/linux/systemd/default.nix
+++ pkgs/os-specific/linux/systemd/default.nix
@@ -226,8 +226,8 @@ in stdenv.mkDerivation (rec {
     let systemd_rev = "c58ab03f64890e7db88745a843bd4520e307099b"; # v238-stable
   in [
     (fetchpatch {
-      url = "https://github.com/dtzWill/systemd/compare/${systemd_rev}...238-musl-2.patch";
-      sha256 = "13v7yzyifilb41jjjmwh3vlfaw6la5ilhrxbmipif3p69pbm355f";
+      url = "https://github.com/dtzWill/systemd/compare/${systemd_rev}...238-musl-4.patch";
+      sha256 = "1bp6jcbhpwv2ivhqv1xgvyvyag3nrg4v25jaqab05aypld52cs4p";
     })
   ];
   postPatch = ''
