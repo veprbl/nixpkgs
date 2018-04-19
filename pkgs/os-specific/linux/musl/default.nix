b1c88bb45e322e8a08aaccdd9f690de951aeeb2a
Will Dietz <w@wdtz.org>
2018-04-18 21:29:43 -0500

    musl: kludge: use builtins.fetchGit for latest

--- pkgs/os-specific/linux/musl/default.nix
+++ pkgs/os-specific/linux/musl/default.nix
@@ -20,12 +20,13 @@ let
 in
 stdenv.mkDerivation rec {
   name    = "musl-${version}";
-  version = "1.1.19";
+  version = "1.1.19-git";
 
-  src = fetchurl {
-    url    = "http://www.musl-libc.org/releases/musl-${version}.tar.gz";
-    sha256 = "1nf1wh44bhm8gdcfr75ayib29b99vpq62zmjymrq7f96h9bshnfv";
-  };
+  src = builtins.fetchGit https://github.com/dtzWill/musl;
+  #`src = fetchurl {
+  #`  url    = "http://www.musl-libc.org/releases/musl-${version}.tar.gz";
+  #`  sha256 = "1nf1wh44bhm8gdcfr75ayib29b99vpq62zmjymrq7f96h9bshnfv";
+  #`};
 
   enableParallelBuilding = true;
 
