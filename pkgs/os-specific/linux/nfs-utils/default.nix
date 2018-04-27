d5e80dcdb86d903c190bdd5d1eaeac4d4762ca19
Will Dietz <w@wdtz.org>
2018-04-27 14:56:42 -0500

    ensure _GNU_SOURCE is set JIC

--- pkgs/os-specific/linux/nfs-utils/default.nix
+++ pkgs/os-specific/linux/nfs-utils/default.nix
@@ -55,6 +55,8 @@ in stdenv.mkDerivation rec {
     ./nfs-utils-1.2.3-sm-notify-res_init.patch
   ];
 
+  NIX_CFLAGS_COMPILE = [ "-D_GNU_SOURCE" ];
+
   postPatch =
     ''
       patchShebangs tests
