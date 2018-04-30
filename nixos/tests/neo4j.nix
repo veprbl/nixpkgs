c7f314ff0184bc476940095160511a7a0e499d36
Will Dietz <w@wdtz.org>
2018-04-29 19:46:23 -0500

    neo4j: env vars, try various commands

--- nixos/tests/neo4j.nix
+++ nixos/tests/neo4j.nix
@@ -1,6 +1,8 @@
 # Basic test for neo4j
 
-import ./make-test.nix ({ pkgs, ...} : {
+let
+  neo4jpassword = "secret";
+in import ./make-test.nix ({ pkgs, ...} : {
   name = "neo4j";
   meta = with pkgs.stdenv.lib.maintainers; {
     maintainers = [ dtzWill ];
@@ -9,6 +11,12 @@ import ./make-test.nix ({ pkgs, ...} : {
   nodes = {
     server = { config, lib, pkgs, ... }: {
       services.neo4j.enable = true;
+
+      # XXX: These should be taken from service config, not hardcoded
+      environment.variables = {
+        NEO4J_HOME = "${pkgs.neo4j}/share/neo4j";
+        NEO4J_CONF = "/var/lib/neo4j/conf";
+      };
     };
   };
 
@@ -17,6 +25,13 @@ import ./make-test.nix ({ pkgs, ...} : {
     startAll;
 
     $server->waitForUnit("neo4j");
+    $server->waitForOpenPort("7474");
+
+    # $server->succeed("neo4j-admin set-initial-password ${neo4jpassword}");
+
+    # $server->succeed("NEO4J_HOME=/var/lib/neo4j cypher-shell -u neo4j -p ${neo4jpassword} --debug");
+    # $server->succeed("neo4j console");
+    $server->succeed("neo4j-shell");
   '';
 
 })
