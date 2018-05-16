a409c6c3f095d1bf9a3bd0ae63980e484b4a323a
Will Dietz <w@wdtz.org>
2018-05-16 14:06:07 -0500

    dyncall: install cmake modules, man pages, use upstream's preferred URL

--- pkgs/development/libraries/dyncall/default.nix
+++ pkgs/development/libraries/dyncall/default.nix
@@ -10,12 +10,23 @@ stdenv.mkDerivation rec {
     sha256 = "d1b6d9753d67dcd4d9ea0708ed4a3018fb5bfc1eca5f37537fba2bc4f90748f2";
   };
 
-  doCheck = true;
+  doCheck = false;
   checkTarget = "run-tests";
+
+  # install bits not automatically installed
+  postInstall = ''
+    # install cmake modules to make using dyncall easier
+    # This is essentially what -DINSTALL_CMAKE_MODULES=ON if using cmake build
+    # (which we don't since it doesn't have a target for running tests AFAICT)
+    install -D -t $out/lib/cmake ./buildsys/cmake/Modules/Find*.cmake
+
+    # manpages are nice, install them
+    install -D -t $out/share/man/man3 ./*/*.3
+  '';
 
   meta = with stdenv.lib; {
     description = "Highly dynamic multi-platform foreign function call interface library";
-    homepage = http://dyncall.org;
+    homepage = http://www.dyncall.org;
     license = licenses.isc;
     maintainers = with maintainers; [ dtzWill ];
   };
