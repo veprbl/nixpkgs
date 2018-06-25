4a396302b01080a0d8e1e0db0fec7759c907da06
Frederik Rietdijk <fridh@fridh.nl>
2018-06-25 11:28:30 +0200

    python: Sphinx: 1.7.0 -> 1.7.5

--- pkgs/development/python-modules/sphinx/default.nix
+++ pkgs/development/python-modules/sphinx/default.nix
@@ -27,10 +27,10 @@
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "Sphinx";
-  version = "1.7.0";
+  version = "1.7.5";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "278b7923f3f4ed2a1d1359f0ae94d89ac90ddd4189e8362f4b4d3baa2afe6b4a";
+    sha256 = "d45480a229edf70d84ca9fae3784162b1bc75ee47e480ffe04a4b7f21a95d76d";
   };
   LC_ALL = "en_US.UTF-8";
 
