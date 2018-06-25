26efc49f8e0164106bd4619032a9adbb27dc31a4
Frederik Rietdijk <fridh@fridh.nl>
2018-06-25 11:28:25 +0200

    python: zc.buildout: 2.11.0 -> 2.11.5

--- pkgs/development/python-modules/buildout-nix/default.nix
+++ pkgs/development/python-modules/buildout-nix/default.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "zc.buildout";
-  version = "2.11.0";
+  version = "2.11.5";
   name = "${pname}-nix-${version}";
 
   src = fetchurl {
     url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "092b0a147d5fb4e79ee0afde665570f85738e714463854f9e4f7f38d0b27ea82";
+    sha256 = "537a22715d82362cddd811da08d11a31d30d5161ce7994b208bd85ebb348d122";
   };
 
   patches = [ ./nix.patch ];
