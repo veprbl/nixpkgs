33dc2e17299128622a99eb995c24b60de263e605
Frederik Rietdijk <fridh@fridh.nl>
2018-06-25 11:28:24 +0200

    python: astroid: 1.6.1 -> 1.6.5

--- pkgs/development/python-modules/astroid/default.nix
+++ pkgs/development/python-modules/astroid/default.nix
@@ -6,11 +6,11 @@
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "astroid";
-  version = "1.6.1";
+  version = "1.6.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f0a0e386dbca9f93ea9f3ea6f32b37a24720502b7baa9cb17c3976a680d43a06";
+    sha256 = "fc9b582dba0366e63540982c3944a9230cbc6f303641c51483fa547dcc22393a";
   };
 
   propagatedBuildInputs = [ logilab_common six lazy-object-proxy wrapt ]
