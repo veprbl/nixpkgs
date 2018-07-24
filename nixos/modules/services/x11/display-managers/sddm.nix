a0c090d447fe155b5ef9f9d8757b5f77a4e46a3b
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-07-24 16:52:49 +0200

    nixos/displayManager: Create a common environment wrapper for all dms
    This makes it easier to support a wider variety of .desktop session files. In
    particular this makes it possible to use both the «legacy» sessions and upstream
    session files.
    
    We separate `xsession` into two parts, `xsessionWrapper` and `xsession`.
    `xsessionWrapper` sets up the correct environment and then lauches the session's
    Exec command (from the .desktop file), falling back to launching the default
    window/desktopManager through the `xsession` script (required by at least some
    nixos tests).
    
    `xsession` then _only_ handles launching desktop-managers/window-managers defined
    through `services.xserver.desktopManager.session`.

--- nixos/modules/services/x11/display-managers/sddm.nix
+++ nixos/modules/services/x11/display-managers/sddm.nix
@@ -60,7 +60,7 @@ let
     MinimumVT=${toString (if xcfg.tty != null then xcfg.tty else 7)}
     ServerPath=${xserverWrapper}
     XephyrPath=${pkgs.xorg.xorgserver.out}/bin/Xephyr
-    SessionCommand=${dmcfg.session.script}
+    SessionCommand=${dmcfg.session.wrapper}
     SessionDir=${dmcfg.session.desktops}/share/xsessions
     XauthPath=${pkgs.xorg.xauth}/bin/xauth
     DisplayCommand=${Xsetup}
