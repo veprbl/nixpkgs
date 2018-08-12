667e443e99ae7ed1b8c0bc89f5c65b6ee7d7dc04
Jan Tojnar <jtojnar@gmail.com>
2018-08-12 19:38:57 +0200

    libhandy: init at 0.0.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3894,6 +3894,8 @@ with pkgs;
 
   libgaminggear = callPackage ../development/libraries/libgaminggear { };
 
+  libhandy = callPackage ../development/libraries/libhandy { };
+
   libipfix = callPackage ../development/libraries/libipfix { };
 
   libircclient = callPackage ../development/libraries/libircclient { };
