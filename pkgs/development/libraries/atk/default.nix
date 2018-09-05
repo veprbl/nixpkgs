619d5c12b98faa1e389c5996f4ffae9f9bea73a2
Jan Tojnar <jtojnar@gmail.com>
2018-09-05 00:42:34 +0000

    atk: 2.28.1 → 2.29.92

--- pkgs/development/libraries/atk/default.nix
+++ pkgs/development/libraries/atk/default.nix
@@ -4,7 +4,7 @@
 
 let
   pname = "atk";
-  version = "2.28.1";
+  version = "2.29.92";
 in
 
 stdenv.mkDerivation rec {
@@ -12,16 +12,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${gnome3.versionBranch version}/${name}.tar.xz";
-    sha256 = "1z7laf6qwv5zsqcnj222dm5f43c6f3liil0cgx4s4s62xjk1wfnd";
+    sha256 = "0l40yfqi137565ylq9xmg9n6f65nhbjzj5s8z3gs3f5p2405r6y8";
   };
-
-  patches = [
-    # darwin linker arguments https://bugzilla.gnome.org/show_bug.cgi?id=794326
-    (fetchurl {
-      url = https://bugzilla.gnome.org/attachment.cgi?id=369680;
-      sha256 = "11v8fhpsbapa04ifb2268cga398vfk1nq8i628441632zjz1diwg";
-    })
-  ];
 
   outputs = [ "out" "dev" ];
 
