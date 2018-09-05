d3bf8025db7cc4ccab9ccbc60dc5f5fdea6f4e31
Jan Tojnar <jtojnar@gmail.com>
2018-09-05 00:49:11 +0000

    gobjectIntrospection: 1.56.0 → 1.58.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9864,7 +9864,6 @@ with pkgs;
   gobjectIntrospection = callPackage ../development/libraries/gobject-introspection {
     nixStoreDir = config.nix.storeDir or builtins.storeDir;
     inherit (darwin) cctools;
-    python = python2;
   };
 
   goocanvas = callPackage ../development/libraries/goocanvas { };
