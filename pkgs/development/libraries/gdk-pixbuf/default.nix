85f84fd2c0bd7ed99b461f462a668a9951534db3
Jan Tojnar <jtojnar@gmail.com>
2018-09-05 00:44:17 +0000

    gdk_pixbuf: 2.36.12 → 2.38.0

--- pkgs/development/libraries/gdk-pixbuf/default.nix
+++ pkgs/development/libraries/gdk-pixbuf/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, fetchFromGitLab, fetchpatch, fixDarwinDylibNames, meson, ninja, pkgconfig, gettext, python3, libxml2, libxslt, docbook_xsl
+{ stdenv, fetchurl, fetchpatch, fixDarwinDylibNames, meson, ninja, pkgconfig, gettext, python3, libxml2, libxslt, docbook_xsl
 , docbook_xml_dtd_43, gtk-doc, glib, libtiff, libjpeg, libpng, libX11, gnome3
 , jasper, gobjectIntrospection, doCheck ? false, makeWrapper }:
 
@@ -9,17 +9,9 @@ in
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
-  # TODO: Change back once tests/bug753605-atsize.jpg is part of the dist tarball
-  # src = fetchurl {
-  #   url = "mirror://gnome/sources/${pname}/${gnome3.versionBranch version}/${name}.tar.xz";
-  #   sha256 = "0d534ysa6n9prd17wwzisq7mj6qkhwh8wcf8qgin1ar3hbs5ry7z";
-  # };
-  src = fetchFromGitLab {
-    domain = "gitlab.gnome.org";
-    owner = "GNOME";
-    repo = "gdk-pixbuf";
-    rev = version;
-    sha256 = "1iiaxmld57bvxbcv4j7vv7bskd6nhl1nqzsqw4fsh5yvyqmrka0z";
+  src = fetchurl {
+    url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
+    sha256 = "0ixfmnxjylx06mjaw116apymwi1a8rnkmkbbvqaxxg2pfwy9fl6x";
   };
 
   patches = [
@@ -28,14 +20,13 @@ stdenv.mkDerivation rec {
     # For now, we are patching the build script to avoid the dependency.
     ./no-mime-sniffing.patch
 
-    
+    # Move installed tests to a separate output
+    ./installed-tests-path.patch
+
     (fetchpatch {
       url = https://gitlab.gnome.org/GNOME/gdk-pixbuf/commit/a7d582f75a71320554b881e063a65f4ced679c1c.patch;
       sha256 = "0z0w52bh4hcrdllbgrqvh12iqzr7k1pb0wdr9vz2qslg1kjk4j92";
     })
-
-    # Move installed tests to a separate output
-    ./installed-tests-path.patch
   ];
 
   outputs = [ "out" "dev" "man" "devdoc" "installedTests" ];
@@ -61,12 +52,6 @@ stdenv.mkDerivation rec {
   ];
 
   postPatch = ''
-    # disable attempt to generate loaders.cache since it fails and we do it
-    cat > build-aux/post-install.sh <<EOF
-    #!/bin/sh
-    echo "Skipping post-install script"
-    EOF
-
     chmod +x build-aux/* # patchShebangs only applies to executables
     patchShebangs build-aux
 
@@ -96,6 +81,10 @@ stdenv.mkDerivation rec {
     for f in $out/bin/* $dev/bin/*; do
         install_name_tool -change @rpath/libgdk_pixbuf-2.0.0.dylib $out/lib/libgdk_pixbuf-2.0.0.dylib $f
     done
+  '';
+
+  preInstall = ''
+    PATH=$PATH:$out/bin # for install script
   '';
 
   # The tests take an excessive amount of time (> 1.5 hours) and memory (> 6 GB).
