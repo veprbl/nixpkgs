2c9ef9623a4eb6555565d050d895bec64b7023d6
Jörg Thalheim <joerg@thalheim.io>
2018-09-17 09:40:42 +0100

    tldr: shell completion

--- pkgs/tools/misc/tldr/default.nix
+++ pkgs/tools/misc/tldr/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, curl, libzip, pkgconfig }:
+{ stdenv, fetchFromGitHub, curl, libzip, pkgconfig, fetchpatch }:
 
 stdenv.mkDerivation rec {
   name = "tldr-${version}";
@@ -11,12 +11,26 @@ stdenv.mkDerivation rec {
     sha256 = "10ylpiqc06p0qpma72vwksd7hd107s0vlx9c6s9rz4vc3i274lb6";
   };
 
+  patches = [
+    # fish support
+    (fetchpatch {
+      url = "https://github.com/tldr-pages/tldr-cpp-client/commit/7e17a596043675e7d95c4d86ea33b3fbe3b50ebf.patch";
+      sha256 = "044yj55nq0lcmalghfv05akqldbznfl9bb83pyzc2k3vh250fgyp";
+    })
+  ];
+
   buildInputs = [ curl libzip ];
   nativeBuildInputs = [ pkgconfig ];
 
   makeFlags = ["CC=cc" "LD=cc" "CFLAGS="];
 
   installFlags = [ "PREFIX=$(out)" ];
+
+  postInstall = ''
+    install -D autocomplete/complete.zsh "$out/share/zsh/vendor_completions/_tldr"
+    install -D autocomplete/complete.bash "$out/etc/bash_completion.d/tldr.sh"
+    install -D autocomplete/complete.fish "$out/share/fish/vendor_completions.d/tldr.fish"
+  '';
 
   meta = with stdenv.lib; {
     description = "Simplified and community-driven man pages";
