05497dc811b3b18c8a70323d76b10688cb0fb934
Scalavision <scalavision@gmail.com>
2018-09-20 18:26:36 +0200

    wip - need to fix something error with make install

--- pkgs/applications/science/biology/cnvnator/default.nix
+++ pkgs/applications/science/biology/cnvnator/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, stdenv, ocaml, fetchurl,  samtools, zlib, bzip2, lzma, curl, pkgconfig, perl, ncurses, doxygen, ccache, libXpm }:
+{ pkgs, stdenv, ocaml, fetchFromGitHub, fetchurl,  samtools, zlib, bzip2, lzma, curl, pkgconfig, perl, ncurses, doxygen, ccache, libXpm }:
 # The call.package of nix will ensure that the packages above is passed in appropriately
 let 
 
@@ -18,10 +18,18 @@ let
   });
 
   srcs = {
-    
+
+    /*
     cnvnator = fetchurl {
       url = "https://github.com/abyzovlab/CNVnator/archive/v${cnvnatorVersion}.tar.gz";
       sha256 = "1hg57wdlg0xs7vylqp4yf95hhpksj7w9dha6ypmmy7ls3zvariaq";
+    };*/
+
+    cnvnator = fetchFromGitHub {
+      owner = "abyzovlab";
+      repo = "CNVnator";
+      rev = "de012f2bccfd4e11e84cf685b19fc138115f2d0d";
+      sha256 = "0fs6a7krwk0w55fgfbv7m5vk9brllf6pk5dmy3rkpdmdk9mslr2j";
     };
 
     yeppp = fetchurl {
@@ -45,6 +53,8 @@ in stdenv.mkDerivation rec {
   '';
 
   preConfigure = ''
+
+    cat Makefile
 
     echo "content of this directory:"
     ls -ahl
@@ -75,13 +85,27 @@ in stdenv.mkDerivation rec {
     echo "What do we have here?"
     ls -ahl
     pwd
-    ./configure --without-curses
+    ./configure --without-curses --enable-libcurl
     make
 
     cd ../ 
   '';
 
   CCACHE_DIR=".ccache";
+
+  preBuild = '' 
+    echo "going to build cnvnator ..."
+    pwd
+    ls -ahl
+    mv samtools-${samtools.version} samtools
+
+    echo >> MakeFile
+    echo >> MakeFile
+
+    echo "install:  cnvnator" >> MakeFile
+
+    cat MakeFile
+  '';
 
   meta = with stdenv.lib; {
     description = "CNVnator calls CNV from mapped paired-end sequencing reads";
