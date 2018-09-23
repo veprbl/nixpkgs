553cddb1b2c4670d6508f7b9d298b14dac287012
Jan Tojnar <jtojnar@gmail.com>
2018-09-23 03:00:08 +0200

    gnome-podcasts: init at 0.4.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2918,6 +2918,8 @@ with pkgs;
 
   gnome-builder = callPackage ../applications/editors/gnome-builder { };
 
+  gnome-podcasts = callPackage ../applications/audio/gnome-podcasts { };
+
   gnokii = callPackage ../tools/misc/gnokii { };
 
   gnuapl = callPackage ../development/interpreters/gnu-apl { };
