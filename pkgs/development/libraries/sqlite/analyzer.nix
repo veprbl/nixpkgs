f34c6b4123f1ddbf93401d6430e9c1209ad34dd2
Will Dietz <w@wdtz.org>
2018-09-24 12:17:15 -0500

    Revert "Merge remote-tracking branch 'dtz/update/sqlite-3.25' into nixos-dtz"
    This reverts commit 9530bf9257272fac284139515e13b81d607f1935, reversing
    changes made to 5b909e2a849c106e31a09f6403f45fbde9665343.

--- pkgs/development/libraries/sqlite/analyzer.nix
+++ pkgs/development/libraries/sqlite/analyzer.nix
@@ -6,11 +6,11 @@ in
 
 stdenv.mkDerivation rec {
   name = "sqlite-analyzer-${version}";
-  version = "3.25.1";
+  version = "3.24.0";
 
   src = assert version == sqlite.version; fetchurl {
     url = "https://sqlite.org/2018/sqlite-src-${archiveVersion version}.zip";
-    sha256 = "1bkz5n8zshh51brcxvyfysdy6mmx6zy4xhhdnwvsavnynk042bz6";
+    sha256 = "19ck2sg13i6ga5vapxak42jn6050vpfid0zrmah7jh32mksh58vj";
   };
 
   nativeBuildInputs = [ unzip ];
