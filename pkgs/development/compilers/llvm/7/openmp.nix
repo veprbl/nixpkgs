eaacd54c727a4670f2ce77952bcedc05be0ba59e
Will Dietz <w@wdtz.org>
2018-09-24 18:44:56 -0500

    openmp: test deps

--- pkgs/development/compilers/llvm/7/openmp.nix
+++ pkgs/development/compilers/llvm/7/openmp.nix
@@ -3,21 +3,25 @@
 , cmake
 , llvm
 , perl
+, python
 , version
 , pkgconfig
 , libffi
 , libelf
+, lit
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "openmp-${version}";
 
   src = fetch "openmp" "1zrqlaxr954sp8lcr7g8m0z0pr8xyq4i6p11x6gcamjm5xijnrih";
 
-  nativeBuildInputs = [ cmake perl pkgconfig ];
+  nativeBuildInputs = [ cmake pkgconfig ] ++ stdenv.lib.optionals doCheck [ python lit ];
   buildInputs = [ llvm libffi libelf /* cuda */ ];
 
   enableParallelBuilding = true;
+
+  doCheck = true;
 
   meta = {
     description = "Components required to build an executable OpenMP program";
