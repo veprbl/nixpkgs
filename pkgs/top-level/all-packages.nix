606d3d8b4895016d428c9ef4ced71245a2a117ac
Will Dietz <w@wdtz.org>
2018-09-25 17:30:00 -0500

    nheko: bump tweeny, use latest boost

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18065,7 +18065,7 @@ with pkgs;
 
   nedit = callPackage ../applications/editors/nedit { };
 
-  nheko = libsForQt5.callPackage ../applications/networking/instant-messengers/nheko { };
+  nheko = libsForQt5.callPackage ../applications/networking/instant-messengers/nheko { boost = boost168; };
 
   nomacs = libsForQt5.callPackage ../applications/graphics/nomacs { };
 
