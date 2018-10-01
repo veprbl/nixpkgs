47b06216a1b429bbf53a5a0ac14104c28a11927d
Will Dietz <w@wdtz.org>
2018-10-01 09:04:48 -0500

    arc-theme: small bump :)

--- pkgs/misc/themes/arc/default.nix
+++ pkgs/misc/themes/arc/default.nix
@@ -7,13 +7,13 @@ in
 
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
-  version = "20180929";
+  version = "20181001";
 
   src = fetchFromGitHub {
     owner  = "NicoHood";
     repo   = pname;
-    rev    = "86e43a874cf4a2f1e5855f147818ffc6786aeff5";
-    sha256 = "18qna7y0cycp41p2cmmisjgg39ls6ral3ylb7bsq1hd19gifaysk";
+    rev    = "7a719566dbbc46fba388429eb57236d0e9b873d2";
+    sha256 = "1nmh445v936x6a3gqqg18db6yyy5lf97zpc5v9izvdzni2nnj309";
   };
 
   preBuild = ''
