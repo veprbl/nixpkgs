39bd3595f9cfa16a348c5ee7a3f1e0f9e4fef139
Will Dietz <w@wdtz.org>
2018-10-01 05:20:03 -0500

    webkitgtk222x: 2.22.0 -> 2.22.2
    So many 2's! :)

--- pkgs/development/libraries/webkitgtk/2.22.nix
+++ pkgs/development/libraries/webkitgtk/2.22.nix
@@ -15,7 +15,7 @@ assert stdenv.isDarwin -> !enableGtk2Plugins;
 with stdenv.lib;
 stdenv.mkDerivation rec {
   name = "webkitgtk-${version}";
-  version = "2.22.0";
+  version = "2.22.2";
 
   meta = {
     description = "Web content rendering engine, GTK+ port";
@@ -28,7 +28,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://webkitgtk.org/releases/${name}.tar.xz";
-    sha256 = "0akgijgpm7ijbqx8295ivdfbvw63xrqh7x77lg40a543k15bahps";
+    sha256 = "1flrbr8pzbrlwv09b4pmgh6vklw7jghd2lgrhcb72vl9s7a8fm1l";
   };
 
   patches = optionals stdenv.isDarwin [
