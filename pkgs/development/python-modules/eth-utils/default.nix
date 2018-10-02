c04d34a4aa235d59d1d6f3141e10357bac4a0999
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2018-10-02 19:50:15 +0300

    pythonPackages.eth-utils: 1.2.1 -> 1.2.2

--- pkgs/development/python-modules/eth-utils/default.nix
+++ pkgs/development/python-modules/eth-utils/default.nix
@@ -3,7 +3,7 @@
 
 buildPythonPackage rec {
   pname = "eth-utils";
-  version = "1.2.1";
+  version = "1.2.2";
 
   # Tests are missing from the PyPI source tarball so let's use GitHub
   # https://github.com/ethereum/eth-utils/issues/130
@@ -11,7 +11,7 @@ buildPythonPackage rec {
     owner = "ethereum";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0g8f5vdjh7qd8kgsqqd9qkm6m79rx3w9yp0rf9vpdsv3xfzrkh1w";
+    sha256 = "018nn862d5m9r0x3mj141xx59g4hf4diz6137hiqla4d9m8fxcr7";
   };
 
   checkInputs = [ pytest hypothesis ];
