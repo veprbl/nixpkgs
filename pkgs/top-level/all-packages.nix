9289bf8c7177cebd0af2eb7d2cedce69e7576dee
Will Dietz <w@wdtz.org>
2018-10-03 14:49:54 -0500

    alot: latest git, py3, fixup gpgme so that is possible

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15496,7 +15496,7 @@ with pkgs;
 
   alock = callPackage ../misc/screensavers/alock { };
 
-  inherit (python2Packages) alot;
+  inherit (python3Packages) alot;
 
   alpine = callPackage ../applications/networking/mailreaders/alpine {
     tcl = tcl-8_5;
