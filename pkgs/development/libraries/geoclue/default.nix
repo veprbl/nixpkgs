22b02695e4fa1a6559ca792f81ab212f8de8977c
Will Dietz <w@wdtz.org>
2018-10-03 22:31:48 -0500

    geoclue git

--- pkgs/development/libraries/geoclue/default.nix
+++ pkgs/development/libraries/geoclue/default.nix
@@ -1,4 +1,4 @@
-{ fetchurl, stdenv, fetchpatch, intltool, pkgconfig, gtk-doc, docbook_xsl, docbook_xml_dtd_412, glib, json-glib, libsoup, libnotify, gdk_pixbuf
+{ fetchurl, stdenv, fetchgit, fetchpatch, intltool, meson, pkgconfig, gtk-doc, docbook_xsl, docbook_xml_dtd_412, glib, json-glib, libsoup, libnotify, gdk_pixbuf
 , modemmanager, avahi, glib-networking, wrapGAppsHook, gobjectIntrospection
 , withDemoAgent ? false
 }:
@@ -7,16 +7,22 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "geoclue-${version}";
-  version = "2.4.12";
+  version = "2018-09-30";
 
-  src = fetchurl {
-    url = "https://www.freedesktop.org/software/geoclue/releases/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "1jnad1f3rf8h05sz1lc172jnqdhqdpz76ff6m7i5ss3s0znf5l05";
+  src = fetchgit {
+    url = https://gitlab.freedesktop.org/geoclue/geoclue;
+    rev = "acd253dcc61d0b69dc6aa345ad4f148038c26290";
+    sha256 = "0ipqnl08c9s7ylq52mnfgpy9xkwi8360c8qqlai9cwnw1n96c42i";
   };
+  #src = fetchurl {
+  #  url = "https://www.freedesktop.org/software/geoclue/releases/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
+  #  sha256 = "1jnad1f3rf8h05sz1lc172jnqdhqdpz76ff6m7i5ss3s0znf5l05";
+  #};
 
   outputs = [ "out" "dev" "devdoc" ];
 
   nativeBuildInputs = [
+    meson
     pkgconfig intltool wrapGAppsHook gobjectIntrospection
     # devdoc
     gtk-doc docbook_xsl docbook_xml_dtd_412
@@ -30,26 +36,18 @@ stdenv.mkDerivation rec {
 
   propagatedBuildInputs = [ glib glib-networking ];
 
-  # Whitelist elementary's agent
-  patches = [
-    (fetchpatch {
-      url = "https://gitlab.freedesktop.org/geoclue/geoclue/commit/2b0491e408be1ebcdbe8751bb2637c1acb78f71e.patch";
-      sha256 = "0pac94y55iksk340dlx3gkhb9lrci90mxqqy5fnh1zbjw9bqxfn4";
-    })
-  ];
-
-  configureFlags = [
-    "--with-systemdsystemunitdir=$(out)/etc/systemd/system"
-    "--enable-introspection"
-    "--enable-gtk-doc"
-    "--enable-demo-agent=${if withDemoAgent then "yes" else "no"}"
-  ] ++ optionals stdenv.isDarwin [
-    "--disable-silent-rules"
-    "--disable-3g-source"
-    "--disable-cdma-source"
-    "--disable-modem-gps-source"
-    "--disable-nmea-source"
-  ];
+#  configureFlags = [
+#    "--with-systemdsystemunitdir=$(out)/etc/systemd/system"
+#    "--enable-introspection"
+#    "--enable-gtk-doc"
+#    "--enable-demo-agent=${if withDemoAgent then "yes" else "no"}"
+#  ] ++ optionals stdenv.isDarwin [
+#    "--disable-silent-rules"
+#    "--disable-3g-source"
+#    "--disable-cdma-source"
+#    "--disable-modem-gps-source"
+#    "--disable-nmea-source"
+#  ];
 
   meta = with stdenv.lib; {
     description = "Geolocation framework and some data providers";
