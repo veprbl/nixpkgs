fbb7740291c13a2e80386f1e8c77cfadf0812cf2
Will Dietz <w@wdtz.org>
2018-10-05 15:53:11 -0500

    awesome: use lua53 (no compat)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15642,7 +15642,7 @@ with pkgs;
 
   awesome-4-0 = callPackage ../applications/window-managers/awesome {
     cairo = cairo.override { xcbSupport = true; };
-    luaPackages = luaPackages.override { inherit lua; };
+    luaPackages = lua53Packages;
   };
   awesome = awesome-4-0;
 
