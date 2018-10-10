049fb9571573ffe7652f5362b6876f2edba660ff
Will Dietz <w@wdtz.org>
2018-10-09 19:58:01 -0500

    systemd: let systemctl link against all the deps I suppose :P

--- pkgs/os-specific/linux/systemd/default.nix
+++ pkgs/os-specific/linux/systemd/default.nix
@@ -67,7 +67,6 @@ in stdenv.mkDerivation rec {
     "-Dtty-gid=3" # tty in NixOS has gid 3
     # "-Dtests=" # TODO
     "-Dlz4=true"
-    #"-Dxz=false"
     "-Dhostnamed=true"
     "-Dnetworkd=true"
     "-Dsysusers=false"
@@ -81,7 +80,6 @@ in stdenv.mkDerivation rec {
     "-Dlibidn=false"
     "-Dlibidn2=true"
     "-Dlink-udev-shared=false"
-    "-Dlink-systemctl-shared=false"
     "-Dquotacheck=false"
     "-Dldconfig=false"
     "-Dsmack=true"
