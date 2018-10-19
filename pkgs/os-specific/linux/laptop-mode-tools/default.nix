d125d1376747156d1f44602b73dadfb60a4ef19f
Will Dietz <w@wdtz.org>
2018-10-19 11:11:24 -0500

    laptop-mode-tools: fixups

--- pkgs/os-specific/linux/laptop-mode-tools/default.nix
+++ pkgs/os-specific/linux/laptop-mode-tools/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, coreutils }:
 
 stdenv.mkDerivation rec {
   name = "laptop-mode-tools-${version}";
@@ -10,6 +10,11 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "1pl0rh1bh23ji5r60nvra26ns3z5dfbjj1l9lhzf7hsmpydrgznd";
   };
+
+  postPatch = ''
+    substituteInPlace install.sh \
+      --replace DESTDIR/usr DESTDIR
+  '';
 
   installPhase = ''
     MAN_D=/share/man \
@@ -24,5 +29,12 @@ stdenv.mkDerivation rec {
   preFixup = ''
     substituteInPlace $out/lib/udev/rules.d/99-laptop-mode.rules \
       --replace lmt-udev $out/lib/udev/lmt-udev
+
+    substituteInPlace $out/lib/systemd/system/lmt-poll.service \
+      --replace /lib/udev/lmt-udev $out/lib/udev/lmt-udev
+
+    substituteInPlace $out/lib/systemd/system/laptop-mode.service \
+      --replace /bin/rm ${coreutils}/bin/rm \
+      --replace /usr/sbin/laptop-mode-tools $out/bin/laptop-mode-tools
   '';
 }
