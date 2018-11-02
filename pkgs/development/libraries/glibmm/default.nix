b2d7c7493fbe94c97b7e6d31a048dbfd2a0f093f
Will Dietz <w@wdtz.org>
2018-11-02 13:46:38 -0500

    glibmm: 2.56.0 -> 2.59.1

--- pkgs/development/libraries/glibmm/default.nix
+++ pkgs/development/libraries/glibmm/default.nix
@@ -1,15 +1,15 @@
 { stdenv, fetchurl, fetchpatch, pkgconfig, gnum4, glib, libsigcxx }:
 
 let
-  ver_maj = "2.56";
-  ver_min = "0";
+  ver_maj = "2.59";
+  ver_min = "1";
 in
 stdenv.mkDerivation rec {
   name = "glibmm-${ver_maj}.${ver_min}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/glibmm/${ver_maj}/${name}.tar.xz";
-    sha256 = "1abrkqhca5p8n6ly3vp1232rny03s7lrd8f8iz2m2m141nxgqx3f";
+    sha256 = "1f36sr8hj1zqwn1alkyw05p8rwvwh5gc6fwv96z62gwc30wh3ilv";
   };
 
   outputs = [ "out" "dev" ];
