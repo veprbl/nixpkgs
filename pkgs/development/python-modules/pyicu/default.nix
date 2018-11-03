f8e09656a6ee6a58ccfe6c74702ddd43058fbae8
Will Dietz <w@wdtz.org>
2018-11-02 21:58:44 -0500

    pyicu: 2.0.3 -> 2.2

--- pkgs/development/python-modules/pyicu/default.nix
+++ pkgs/development/python-modules/pyicu/default.nix
@@ -9,21 +9,21 @@
 
 buildPythonPackage rec {
   pname = "PyICU";
-  version = "2.0.3";
+  version = "2.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0pzss3l0b0vcsyr7wlqdd6pkcqldspajfgd9k2iijf6r152d2ln4";
+    sha256 = "0wq9y5fi1ighgf5aws9nr87vi1w44p7q1k83rx2y3qj5d2xyhspa";
   };
 
   patches = [
     (fetchpatch {
-      url = https://sources.debian.org/data/main/p/pyicu/2.0.3-1/debian/patches/icu_test.patch;
+      url = https://sources.debian.org/data/main/p/pyicu/2.2-1/debian/patches/icu_test.patch;
       sha256 = "1iavdkyqixm9i753svl17barla93b7jzgkw09dn3hnggamx7zwx9";
     })
   ];
 
-  buildInputs = [ pkgs.icu pytest ];
+  buildInputs = [ pkgs.icu60 pytest ];
   propagatedBuildInputs = [ six ];
 
   meta = with stdenv.lib; {
