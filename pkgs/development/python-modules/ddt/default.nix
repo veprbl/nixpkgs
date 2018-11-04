720a25912d2262e7fd599a582c2fdd724bac2d7a
Will Dietz <w@wdtz.org>
2018-11-04 17:25:54 -0600

    ddt: checkinputs

--- pkgs/development/python-modules/ddt/default.nix
+++ pkgs/development/python-modules/ddt/default.nix
@@ -1,6 +1,10 @@
 { stdenv
 , buildPythonPackage
 , fetchPypi
+, six
+, mock
+, nose
+, pyyaml
 }:
 
 buildPythonPackage rec {
@@ -11,6 +15,8 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "898364fc34b27981b925171a0011c174c94633cb678eb1fac05fe7a234c7912c";
   };
+
+  checkInputs = [ six mock nose pyyaml ];
 
   meta = with stdenv.lib; {
     description = "Data-Driven/Decorated Tests, a library to multiply test cases";
