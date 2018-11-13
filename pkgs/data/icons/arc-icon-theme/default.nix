f41794e1f0021f9b7e756164cbef894fb5a11404
Will Dietz <w@wdtz.org>
2018-11-13 16:09:09 -0500

    arc-icon-theme: use pname

--- pkgs/data/icons/arc-icon-theme/default.nix
+++ pkgs/data/icons/arc-icon-theme/default.nix
@@ -1,13 +1,12 @@
 { stdenv, fetchFromGitHub, autoreconfHook, gtk3, moka-icon-theme }:
 
 stdenv.mkDerivation rec {
-  name = "${package-name}-${version}";
-  package-name = "arc-icon-theme";
+  pname = "arc-icon-theme";
   version = "2016-11-22";
 
   src = fetchFromGitHub {
     owner = "horst3180";
-    repo = package-name;
+    repo = pname;
     rev = "55a575386a412544c3ed2b5617a61f842ee4ec15";
     sha256 = "1ch3hp08qri93510hypzz6m2x4xgg2h15wvnhjwh1x1s1b7jvxjd";
   };
