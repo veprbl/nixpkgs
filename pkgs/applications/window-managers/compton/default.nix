1056d2e087e28b699a3de50a557f1ee5e02e0b87
Will Dietz <w@wdtz.org>
2018-11-14 11:57:14 -0600

    compton: -> 4-rc4

--- pkgs/applications/window-managers/compton/default.nix
+++ pkgs/applications/window-managers/compton/default.nix
@@ -63,7 +63,7 @@ let
 
   gitSource = rec {
     pname = "compton-git";
-    version = "2018-11-04"; # "4-rc1";
+    version = "4-rc4";
 
     COMPTON_VERSION = "v${version}";
 
@@ -72,8 +72,8 @@ let
     src = fetchFromGitHub {
       owner  = "yshui";
       repo   = "compton";
-      rev    = "2b0b7435dfe96cf3ad8bb82c95c743752e70577e"; # COMPTON_VERSION;
-      sha256 = "0mi8may6mp2l9l2gwk9sdviglnbpvmxn4743541xm2h2vffchk40";
+      rev    = COMPTON_VERSION;
+      sha256 = "02d7b86yg15fbjyr80qsajhpr7ykl4iyaa16w9pygz8nh8lh8rbb";
     };
 
     buildInputs = [
