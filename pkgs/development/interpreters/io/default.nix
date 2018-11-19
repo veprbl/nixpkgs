94b37eb5f0ab15fc48f6b79b2fc47ee18abdc750
Will Dietz <w@wdtz.org>
2018-11-19 03:51:15 -0600

    beep

--- pkgs/development/interpreters/io/default.nix
+++ pkgs/development/interpreters/io/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchFromGitHub, cmake, zlib, sqlite, gmp, libffi, cairo,
   ncurses, freetype, libGLU_combined, libpng, libtiff, libjpeg, readline, libsndfile,
   libxml2, freeglut, libsamplerate, pcre, libevent, libedit, yajl,
-  python3, openssl, glfw, pkgconfig, libpthreadstubs, libXdmcp
+  python3, openssl, glfw, pkgconfig, libXdmcp
 }:
 
 stdenv.mkDerivation rec {
@@ -23,18 +23,9 @@ stdenv.mkDerivation rec {
     zlib sqlite gmp libffi cairo ncurses freetype
     libGLU_combined libpng libtiff libjpeg readline libsndfile libxml2
     freeglut libsamplerate pcre libevent libedit yajl
-    pkgconfig glfw openssl libpthreadstubs libXdmcp
+    pkgconfig glfw openssl libXdmcp
     python3
   ];
-
-  preConfigure = ''
-    # The Addon generation (AsyncRequest and a others checked) seems to have
-    # trouble with building on Virtual machines. Disabling them until it
-    # can be fully investigated.
-    sed -ie \
-          "s/add_subdirectory(addons)/#add_subdirectory(addons)/g" \
-          CMakeLists.txt
-  '';
 
   # for gcc5; c11 inline semantics breaks the build
   NIX_CFLAGS_COMPILE = "-fgnu89-inline";
