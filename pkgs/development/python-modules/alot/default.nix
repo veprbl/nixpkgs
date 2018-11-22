b5726b8564bda1c864674e50b3c6113cace8791a
Will Dietz <w@wdtz.org>
2018-11-22 14:59:48 -0600

    ggpme, alot: fix python used

--- pkgs/development/python-modules/alot/default.nix
+++ pkgs/development/python-modules/alot/default.nix
@@ -1,5 +1,5 @@
 { stdenv, lib, buildPythonPackage, fetchFromGitHub, isPy3k
-, notmuch, urwid, urwidtrees, twisted, python_magic, configobj, mock, file, gpgme, setuptools
+, notmuch, urwid, urwidtrees, twisted, python_magic, configobj, mock, file, gpgme
 , service-identity
 , gnupg ? null, sphinx, awk ? null, procps ? null, future ? null
 , withManpage ? false }:
@@ -21,8 +21,6 @@ buildPythonPackage rec {
   };
 
   nativeBuildInputs = lib.optional withManpage sphinx;
-
-  buildInputs = [ setuptools ];
 
   propagatedBuildInputs = [
     notmuch
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2525,7 +2525,10 @@ in {
 
   google_resumable_media = callPackage ../development/python-modules/google_resumable_media { };
 
-  gpgme = toPythonModule (pkgs.gpgme.override { pythonSupport=true; });
+  gpgme = toPythonModule (pkgs.gpgme.override {
+    pythonSupport=true;
+    inherit (self) python;
+  });
 
   gphoto2 = callPackage ../development/python-modules/gphoto2 {
     inherit (pkgs) pkgconfig;
