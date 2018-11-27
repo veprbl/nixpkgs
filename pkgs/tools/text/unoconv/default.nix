0fc7eb0832e96eaf7801883847ff7bb5f1b9b8a1
Will Dietz <w@wdtz.org>
2018-11-27 08:06:17 -0600

    unwrpped

--- pkgs/tools/text/unoconv/default.nix
+++ pkgs/tools/text/unoconv/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, python3, libreoffice, asciidoc, makeWrapper
+{ stdenv, fetchFromGitHub, python3, libreoffice-unwrapped, asciidoc, makeWrapper
 # whether to install odt2pdf/odt2doc/... symlinks to unoconv
 , installSymlinks ? true
 }:
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
 
   postInstall = ''
     sed -i "s|/usr/bin/env python.*|${python3}/bin/${python3.executable}|" "$out/bin/unoconv"
-    wrapProgram "$out/bin/unoconv" --set UNO_PATH "${libreoffice.libreoffice}/lib/libreoffice/program/"
+    wrapProgram "$out/bin/unoconv" --set UNO_PATH "${libreoffice-unwrapped}/lib/libreoffice/program/"
   '' + (if installSymlinks then ''
     make install-links prefix="$out"
   '' else "");
