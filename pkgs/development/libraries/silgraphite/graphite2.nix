85fcac93fd8cfce4150b8f56548f94cfa643573e
Will Dietz <w@wdtz.org>
2018-11-28 10:44:45 -0600

    graphite2: 1.3.6 -> 1.3.12, enable tests
    Many fixes, some security-related?
    
    See notes for the vairous releases:
    
    https://github.com/silnrsi/graphite/blob/1.3.12/ChangeLog

--- pkgs/development/libraries/silgraphite/graphite2.nix
+++ pkgs/development/libraries/silgraphite/graphite2.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, pkgconfig, freetype, cmake, python }:
 
 stdenv.mkDerivation rec {
-  version = "1.3.6";
+  version = "1.3.12";
   name = "graphite2-${version}";
 
   src = fetchurl {
     url = "https://github.com/silnrsi/graphite/releases/download/"
-      + "${version}/graphite-${version}.tgz";
-    sha256 = "0xdg6bc02bl8yz39l5i2skczfg17q4lif0qqan0dhvk0mibpcpj7";
+      + "${version}/${name}.tgz";
+    sha256 = "1l1940d8fz67jm6a0x8cjb5p2dv48cvz3wcskwa83hamd70k15fd";
   };
 
   nativeBuildInputs = [ pkgconfig cmake ];
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   patches = stdenv.lib.optionals stdenv.isDarwin [ ./macosx.patch ];
 
   checkInputs = [ python ];
-  doCheck = false; # fails, probably missing something
+  #doCheck = false; # fails, probably missing something
 
   meta = with stdenv.lib; {
     description = "An advanced font engine";
