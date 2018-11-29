43e98fefc124d17392adba99de0183a6570c60c2
c0bw3b <c0bw3b@users.noreply.github.com>
2018-11-29 22:44:29 +0100

    iptables: 1.6.2 -> 1.8.2
    + enabled BPF compiler by default since it is the future
    
    Changelog 1.8.0:
    https://www.netfilter.org/projects/iptables/files/changes-iptables-1.8.0.txt
    
    Changelog 1.8.1:
    https://www.netfilter.org/projects/iptables/files/changes-iptables-1.8.1.txt
    
    Changelog 1.8.2:
    https://www.netfilter.org/projects/iptables/files/changes-iptables-1.8.2.txt

--- pkgs/os-specific/linux/iptables/default.nix
+++ pkgs/os-specific/linux/iptables/default.nix
@@ -10,6 +10,26 @@ stdenv.mkDerivation rec {
     sha256 = "1bqj9hf3szy9r0w14iy23w00ir8448nfhpcprbwmcchsxm88nxx3";
   };
 
+  patches = [
+    # Adds missing bits to extensions' libipt_icmp.c and libip6t_icmp6.c that were causing build to fail
+    (fetchpatch {
+      url = "https://git.netfilter.org/iptables/patch/?id=907e429d7548157016cd51aba4adc5d0c7d9f816";
+      sha256 = "0vc7ljcglz5152lc3jx4p44vjfi6ipvxdrgkdb5dmkhlb5v93i2h";
+    })
+    # Build with musl libc fails because of conflicting struct ethhdr definitions
+    (fetchpatch {
+      url = "https://git.netfilter.org/iptables/patch/?id=51d374ba41ae4f1bb851228c06b030b83dd2092f";
+      sha256 = "05fwrq03f9sm0v2bfwshbrg7pi2p978w1460arnmpay3135gj266";
+    })
+    # extensions: libip6t_mh: fix bogus translation error
+    (fetchpatch {
+      url = "https://git.netfilter.org/iptables/patch/?id=5839d7fe62ff667af7132fc7d589b386951f27b3";
+      sha256 = "0578jn1ip710z9kijwg9g2vjq2kfrbafl03m1rgi4fasz215gvkf";
+    })
+    # Prevent headers collisions between linux and netfilter (in.h and in6.h)
+    (./netinet-headers-collision.patch)
+  ];
+
   nativeBuildInputs = [ bison flex pkgconfig ];
 
   buildInputs = [ libnetfilter_conntrack libnftnl libmnl libpcap ];
