7df5188a0c331d18c2473a23bfa8383b46161a41
Will Dietz <w@wdtz.org>
2018-12-02 17:03:17 -0600

    igt: meson

--- pkgs/development/tools/misc/intel-gpu-tools/default.nix
+++ pkgs/development/tools/misc/intel-gpu-tools/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchurl, pkgconfig, libdrm, libpciaccess, cairo, udev, xorgproto
 , libX11, libXext, libXv, libXrandr, glib, bison, libunwind, python3, kmod
-, procps, utilmacros, gnome2, openssl, peg }:
+, procps, utilmacros, gnome2, openssl, peg, meson, ninja }:
 
 stdenv.mkDerivation rec {
   name = "intel-gpu-tools-${version}";
@@ -12,14 +12,14 @@ stdenv.mkDerivation rec {
   #  sha256 = "1l4s95m013p2wvddwr4cjqyvsgmc88zxx2887p1fbb1va5n0hjsd";
   #};
 
-  nativeBuildInputs = [ pkgconfig utilmacros ];
+  nativeBuildInputs = [ pkgconfig utilmacros meson ninja ];
   buildInputs = [ libdrm libpciaccess cairo xorgproto udev libX11 kmod
     libXext libXv libXrandr glib bison libunwind python3 procps
     gnome2.gtkdoc openssl peg ];
 
-  preConfigure = ''
-    ./autogen.sh
-  '';
+  #preConfigure = ''
+  #  ./autogen.sh
+  #'';
 
   preBuild = ''
     patchShebangs tests
