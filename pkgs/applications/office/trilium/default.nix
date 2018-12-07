e6252ffe2cdef612a2607c76c34f639773dc2218
Will Dietz <w@wdtz.org>
2018-12-07 16:39:48 -0600

    trilium: bump to 0.25.2, rework a bit... works! \o/
    * fix URL
    * let setup-hook handle unpacking
    * wrap AND autoPatchelfHook, for reasons I guess

--- pkgs/applications/office/trilium/default.nix
+++ pkgs/applications/office/trilium/default.nix
@@ -1,27 +1,37 @@
-{ stdenv, fetchurl, p7zip }:
+{ stdenv, fetchurl, p7zip, autoPatchelfHook, atomEnv, makeWrapper }:
 
 stdenv.mkDerivation rec {
   name = "trilium-${version}";
-  version = "0.24.5";
-
-  phases = [ "unpackPhase" "installPhase" ];
+  version = "0.25.2";
 
   src = fetchurl {
-    url = "https://github.com/zadam/trilium/releases/download/${version}/trilium-linux-x64-${version}.7z";
-    sha256 = "0dpkw875k941wkj14r3x86q15da3kjihb4lg4sjxbmhq2gv4jdjv";
+    url = "https://github.com/zadam/trilium/releases/download/v${version}/trilium-linux-x64-${version}.7z";
+    sha256 = "0rzh40jq61qsai0bhvrflzbv4vkrv8alxw9k95s0vk3nj0wr36z1";
   };
 
-  unpackCmd = ''
-    ${p7zip}/bin/7zr x $curSrc
-  '';
+  nativeBuildInputs = [
+    p7zip /* for unpacking */
+    autoPatchelfHook
+    makeWrapper
+  ];
+
+  buildInputs = atomEnv.packages;
 
   installPhase = ''
     mkdir -p $out/bin
-    mkdir -p $out/usr/share/trilium
+    mkdir -p $out/share/trilium
 
-    cp -r ./* $out/usr/share/trilium
-    ln -s $out/usr/share/trilium/trilium $out/bin/trilium
+    cp -r ./* $out/share/trilium
+    ln -s $out/share/trilium/trilium $out/bin/trilium
   '';
+
+
+  # This "shouldn't" be needed, remove when possible :)
+  preFixup = ''
+    wrapProgram $out/bin/trilium --prefix LD_LIBRARY_PATH : "${atomEnv.libPath}"
+  '';
+
+  dontStrip = true;
 
   meta = with stdenv.lib; {
     description = "Trilium Notes is a hierarchical note taking application with focus on building large personal knowledge bases.";
