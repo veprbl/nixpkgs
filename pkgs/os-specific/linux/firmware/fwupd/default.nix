a46c74143ecb99ce94e523239166b6db81e97de9
Will Dietz <w@wdtz.org>
2018-12-15 23:40:57 -0600

    Revert "fwupd: revert back to 1.1 series while debugging..."
    This reverts commit 3adef71df6564b0c4cf40d4a99a569c8ae3fbbb9.

--- pkgs/os-specific/linux/firmware/fwupd/default.nix
+++ pkgs/os-specific/linux/firmware/fwupd/default.nix
@@ -1,14 +1,16 @@
-{ stdenv, fetchurl, gtk-doc, pkgconfig, gobject-introspection, intltool
+{ stdenv, fetchurl, fetchFromGitHub, fetchpatch, gtk-doc, pkgconfig, gobjectIntrospection, intltool
 , libgudev, polkit, libxmlb, gusb, sqlite, libarchive, glib-networking
 , libsoup, help2man, gpgme, libxslt, elfutils, libsmbios, efivar, glibcLocales
 , gnu-efi, libyaml, valgrind, meson, libuuid, colord, docbook_xml_dtd_43, docbook_xsl
 , ninja, gcab, gnutls, python3, wrapGAppsHook, json-glib, bash-completion
 , shared-mime-info, umockdev, vala, makeFontsConf, freefont_ttf
 , cairo, freetype, fontconfig, pango
+
 }:
 let
   # Updating? Keep $out/etc synchronized with passthru.filesInstalledToEtc
-  version = "1.2.1";
+  #version = "1.2.1";
+  version = "2018-11-27";
   python = python3.withPackages (p: with p; [ pygobject3 pycairo pillow ]);
   installedTestsPython = python3.withPackages (p: with p; [ pygobject3 requests ]);
 
@@ -17,10 +19,17 @@ let
   };
 in stdenv.mkDerivation {
   name = "fwupd-${version}";
-  src = fetchurl {
-    url = "https://people.freedesktop.org/~hughsient/releases/fwupd-${version}.tar.xz";
-    sha256 = "126b3lsh4gkyajsqm2c8l6wqr4dd7m26krz2527khmlps0lxdhg1";
+  src = fetchFromGitHub {
+    owner = "hughsie";
+    repo = "fwupd";
+    rev = "b4fd12a4c6d877820e87598edf5ef5e4867735ba";
+    sha256 = "1wlcn78bxamp5dy469fkc4nimcwk0gqaz3a8kjb0dbdbq9g38ka8";
   };
+
+  #src = fetchurl {
+  #  url = "https://people.freedesktop.org/~hughsient/releases/fwupd-${version}.tar.xz";
+  #  sha256 = "126b3lsh4gkyajsqm2c8l6wqr4dd7m26krz2527khmlps0lxdhg1";
+  #};
 
   outputs = [ "out" "lib" "dev" "devdoc" "man" "installedTests" ];
 
@@ -53,6 +62,8 @@ in stdenv.mkDerivation {
     substituteInPlace meson.build --replace \
       "conf.set_quoted('SYSCONFDIR', sysconfdir)" \
       "conf.set_quoted('SYSCONFDIR', '/etc')"
+
+    substituteInPlace data/installed-tests/meson.build --replace sysconfdir sysconfdir_install
   '';
 
   # /etc/os-release not available in sandbox
