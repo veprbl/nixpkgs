376ae7210586e6421bb56db909ac25496019c992
Will Dietz <w@wdtz.org>
2018-12-17 12:19:51 -0600

    hyperfine: 1.4.0 -> 1.5.0
    https://github.com/sharkdp/hyperfine/releases/tag/v1.5.0

--- pkgs/tools/misc/hyperfine/default.nix
+++ pkgs/tools/misc/hyperfine/default.nix
@@ -4,16 +4,16 @@
 
 rustPlatform.buildRustPackage rec {
   name = "hyperfine-${version}";
-  version = "1.4.0";
+  version = "1.5.0";
 
   src = fetchFromGitHub {
     owner  = "sharkdp";
     repo   = "hyperfine";
     rev    = "refs/tags/v${version}";
-    sha256 = "1mn5nv3zljj2wz40imf62gknv84f7igslsf59gg1qvhgvgsd98sp";
+    sha256 = "16xxznz8xkflw7dvlzayx4g0ia0cqdv28yxbr0r2dx7v8w9vx2cn";
   };
 
-  cargoSha256 = "1kyx1fhz8l5m8dhwd7j3hic86xx71216775m9bslmm2z4csl7r1s";
+  cargoSha256 = "1a5x3bwphr3s9w45cpxp99c4qpvcxhb6ghg3npkqnb8iv3d6w8by";
 
   buildInputs = stdenv.lib.optional stdenv.isDarwin Security;
 
