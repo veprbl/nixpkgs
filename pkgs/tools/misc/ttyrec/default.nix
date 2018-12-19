22f0dd07105a5f8961e22965a9eac0b9c354e057
Will Dietz <w@wdtz.org>
2018-12-19 07:26:34 -0600

    ttyrec: deb patches edition

--- pkgs/tools/misc/ttyrec/default.nix
+++ pkgs/tools/misc/ttyrec/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, fetchzip }:
 
 stdenv.mkDerivation rec {
   name = "ttyrec-${version}";
@@ -9,11 +9,17 @@ stdenv.mkDerivation rec {
     sha256 = "ef5e9bf276b65bb831f9c2554cd8784bd5b4ee65353808f82b7e2aef851587ec";
   };
 
-  patches = [ ./clang-fixes.patch ./musl.patch ./tty.diff ];
+  postPatch = let debPatch = fetchzip {
+    url = http://deb.debian.org/debian/pool/main/t/ttyrec/ttyrec_1.0.8-5.debian.tar.gz;
+    sha256 = "09wm4abj8bf2pirs882ri4kddn8ba6hz8hz5q2ffa3k4ifn98s2d";
+  }; in ''
+    for patch in $(cat ${debPatch}/patches/series); do
+      patch -p1 < "${debPatch}/patches/$patch"
+    done
+  '';
 
-  NIX_CFLAGS_COMPILE = [ "-DHAVE_openpty" ];
-  NIX_LDFLAGS = [ "-lutil" ];
-  makeFlags = stdenv.lib.optional stdenv.cc.isClang "CC=clang";
+  # TODO: check if cc still needs to be set on darwin
+  #makeFlags = stdenv.lib.optional stdenv.cc.isClang "CC=clang";
 
   installPhase = ''
     mkdir -p $out/{bin,man}
