45593b9c1bcfaf6565f71befa2d2e5adee2fc659
Will Dietz <w@wdtz.org>
2018-12-19 21:37:32 -0600

    Revert "libsigcxx: 2.10.1 -> 2.99.12 (unstable)"
    This reverts commit c44b0432f2b246a36b54f8008163d4fbed1fd509.

--- pkgs/development/libraries/libsigcxx/default.nix
+++ pkgs/development/libraries/libsigcxx/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, pkgconfig, gnum4 }:
 let
-  ver_maj = "2.99"; # odd major numbers are unstable (esp 99 :P)
-  ver_min = "12";
+  ver_maj = "2.10"; # odd major numbers are unstable
+  ver_min = "1";
 in
 stdenv.mkDerivation rec {
   name = "libsigc++-${ver_maj}.${ver_min}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/libsigc++/${ver_maj}/${name}.tar.xz";
-    sha256 = "1a4hcry9y129gz3269rq5r3miwf27hay51jm09b2vbsvgwksw0nr";
+    sha256 = "00v08km4wwzbh6vjxb21388wb9dm6g2xh14rgwabnv4c2wk5z8n9";
   };
 
   nativeBuildInputs = [ pkgconfig gnum4 ];
