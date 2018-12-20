334dd82903a16b1866921a6de754a2960f7546c0
Will Dietz <w@wdtz.org>
2018-12-20 10:13:49 -0600

    Revert "Revert "Revert "networkmanager: 1.12.4 -> 1.14.4"""
    The release numbering is confusing and doesn't appear
    to be documented in any place I tried looking.
    
    Anyway 1.14.4 is from Oct, let's use 1.12.x which
    after bumping to 1.12.6 will be from this month,
    so hopefully has the important bits...
    
    This reverts commit 7899c56e47bf8dcd25cc0c139dd690de54c8dcc5.

--- pkgs/tools/networking/network-manager/default.nix
+++ pkgs/tools/networking/network-manager/default.nix
@@ -9,11 +9,11 @@ let
   pname = "NetworkManager";
 in stdenv.mkDerivation rec {
   name = "network-manager-${version}";
-  version = "1.14.4";
+  version = "1.12.4";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "064cgj9za0kzarks0lrv0qw2ysdphb5l97iw0c964bfiqzjfv8rm";
+    sha256 = "0gfc2k72vr47x628056w65mq9avshsvzr8yahya5fbsivz9hvpkf";
   };
 
   outputs = [ "out" "dev" ];
