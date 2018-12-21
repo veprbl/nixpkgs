aa71e64ac948a74f598ab5e7220826f2b266c1e6
Will Dietz <w@wdtz.org>
2018-12-21 16:35:02 -0600

    intel-media-driver: 2018-12-15 -> 18.4.pre4

--- pkgs/development/libraries/intel-media-driver/default.nix
+++ pkgs/development/libraries/intel-media-driver/default.nix
@@ -5,15 +5,15 @@
 
 stdenv.mkDerivation rec {
   name = "intel-media-driver-${version}";
-  #version = "18.3.0";
-  version = "2018-12-15";
+  version = "18.4.pre4";
+  #version = "2018-12-15";
 
   src = fetchFromGitHub {
     owner  = "intel";
     repo   = "media-driver";
-#    rev    = "intel-media-${version}";
-    rev = "60229409b85b2fad42558f42d2b7648cd1dd2703";
-    sha256 = "1ad3g9v4f8kz05yck6q76vaz24cm2xfn9g8yf82zlqmc6af2nqy4";
+    rev    = "intel-media-${version}";
+#    rev = "60229409b85b2fad42558f42d2b7648cd1dd2703";
+    sha256 = "1890dmd46jf83y8k12y511apkdq8hxd8wk65ik83jf209b1c3g9h";
   };
 
   cmakeFlags = [
