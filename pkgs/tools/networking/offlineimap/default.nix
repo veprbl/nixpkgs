83cf3ea4bcc43529c1a2d6ebad5849c391deb9f8
Will Dietz <w@wdtz.org>
2018-12-22 19:40:16 -0600

    offlineimap: 7.2.1 -> 7.2.2
    https://github.com/OfflineIMAP/offlineimap/blob/v7.2.2/Changelog.md
    
    Also touchup doc building by providing needed files.

--- pkgs/tools/networking/offlineimap/default.nix
+++ pkgs/tools/networking/offlineimap/default.nix
@@ -1,25 +1,20 @@
 { stdenv, fetchFromGitHub, python2Packages,
-  asciidoc, libxml2, libxslt, docbook_xsl }:
+  asciidoc, libxml2, libxslt, docbook_xsl, docbook_xml_dtd_45 }:
 
 python2Packages.buildPythonApplication rec {
-  version = "7.2.1";
+  version = "7.2.2";
   pname = "offlineimap";
 
   src = fetchFromGitHub {
     owner = "OfflineIMAP";
     repo = "offlineimap";
     rev = "v${version}";
-    sha256 = "1m5i74baazwazqp98ssma968rnwzfl1nywb7icf0swc8447ps97q";
+    sha256 = "11nj7y9fa7v6vcxk3wr8smfgm3mxxnmq3l8q69rrjxlfzcv7dl8m";
   };
-
-  postPatch = ''
-    # Skip xmllint to stop failures due to no network access
-    sed -i docs/Makefile -e "s|a2x -v -d |a2x -L -v -d |"
-  '';
 
   doCheck = false;
 
-  nativeBuildInputs = [ asciidoc libxml2 libxslt docbook_xsl ];
+  nativeBuildInputs = [ asciidoc libxml2 libxslt docbook_xsl docbook_xml_dtd_45 ];
   propagatedBuildInputs = with python2Packages; [ six kerberos ];
 
   postInstall = ''
