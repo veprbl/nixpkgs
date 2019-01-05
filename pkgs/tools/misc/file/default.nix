d221d4ba2110f06a04322a03d75c26e54190bbf7
Will Dietz <w@wdtz.org>
2019-01-04 19:36:39 -0600

    file: latest git

--- pkgs/tools/misc/file/default.nix
+++ pkgs/tools/misc/file/default.nix
@@ -1,18 +1,24 @@
-{ stdenv, fetchurl, file, zlib, libgnurx }:
+{ stdenv, fetchurl, fetchFromGitHub, autoreconfHook, file, zlib, libgnurx }:
 
 stdenv.mkDerivation rec {
-  name = "file-${version}";
-  version = "5.35";
+  pname = "file";
+  version = "5.35.0.1"; # not really!
 
-  src = fetchurl {
-    urls = [
-      "ftp://ftp.astron.com/pub/file/${name}.tar.gz"
-      "https://distfiles.macports.org/file/${name}.tar.gz"
-    ];
-    sha256 = "0ijm1fabm68ykr1zbx0bxnka5jr3n42sj8y5mbkrnxs0fj0mxi1h";
+  src = fetchFromGitHub {
+    owner = pname;
+    repo = pname;
+    rev = "e0c3763d7988b16b8169882b664a1839fb967e4e";
+    sha256 = "0mcn4605i2dgs485f4x4ay8vvd2vq963k236wgcdji471h07pigz";
   };
+  #src = fetchurl {
+  #  urls = [
+  #    "ftp://ftp.astron.com/pub/file/${name}.tar.gz"
+  #    "https://distfiles.macports.org/file/${name}.tar.gz"
+  #  ];
+  #  sha256 = "0ijm1fabm68ykr1zbx0bxnka5jr3n42sj8y5mbkrnxs0fj0mxi1h";
+  #};
 
-  nativeBuildInputs = stdenv.lib.optional (stdenv.hostPlatform != stdenv.buildPlatform) file;
+  nativeBuildInputs = [ autoreconfHook ] ++ stdenv.lib.optional (stdenv.hostPlatform != stdenv.buildPlatform) file;
   buildInputs = [ zlib ]
               ++ stdenv.lib.optional stdenv.hostPlatform.isWindows libgnurx;
 
