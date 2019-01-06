5b61a7bf4980dc5a4968641e52713048dd26c516
Will Dietz <w@wdtz.org>
2019-01-06 02:14:32 -0600

    openconnect: -> 8.01

--- pkgs/tools/networking/openconnect/default.nix
+++ pkgs/tools/networking/openconnect/default.nix
@@ -1,30 +1,19 @@
-{ stdenv, fetchgit, fetchurl, pkgconfig, autoreconfHook, vpnc, openssl ? null, gnutls ? null, gmp, libxml2, stoken, zlib, lz4, libtasn1
-, pcsclite }:
+{ stdenv, fetchgit, fetchurl, pkgconfig, vpnc, openssl ? null, gnutls ? null, gmp, libxml2, stoken, zlib, lz4, libtasn1, pcsclite }:
 
 assert (openssl != null) == (gnutls == null);
 
 stdenv.mkDerivation rec {
-  name = "openconnect-7.08.99git";
+  pname = "openconnect";
+  version = "8.01";
 
-  src = fetchgit {
-    url = git://git.infradead.org/users/dwmw2/openconnect.git;
-    rev = "a25e844477b4662f0ede159a3c633c91597c8813";
-    sha256 = "1pc9bxlb76p2a790df725dv4666y8q41gl870z3dzdhvsmgx75bi";
+  src = fetchurl {
+    urls = [
+      "ftp://ftp.infradead.org/pub/openconnect/${pname}-${version}.tar.gz"
+    ];
+    sha256 = "1z96ssf2v9xflwiry73pahmx79zap10vpfkzv1s786n8k57qm1j8";
   };
-  #src = fetchurl {
-  #  urls = [
-  #    "ftp://ftp.infradead.org/pub/openconnect/${name}.tar.gz"
-  #  ];
-  #  sha256 = "00wacb79l2c45f94gxs63b9z25wlciarasvjrb8jb8566wgyqi0w";
-  #};
 
   outputs = [ "out" "dev" ];
-
-  #preConfigure = ''
-  #    export PKG_CONFIG=${pkgconfig}/bin/pkg-config
-  #    export LIBXML2_CFLAGS="-I ${libxml2.dev}/include/libxml2"
-  #    export LIBXML2_LIBS="-L${libxml2.out}/lib -lxml2"
-  #  '';
 
   configureFlags = [
     "--with-vpnc-script=${vpnc}/etc/vpnc/vpnc-script"
@@ -32,7 +21,7 @@ stdenv.mkDerivation rec {
     "--without-openssl-version-check"
   ];
 
-  nativeBuildInputs = [ pkgconfig autoreconfHook ];
+  nativeBuildInputs = [ pkgconfig ];
   propagatedBuildInputs = [ vpnc openssl gnutls gmp libxml2 stoken zlib lz4 libtasn1 pcsclite ];
 
   meta = {
