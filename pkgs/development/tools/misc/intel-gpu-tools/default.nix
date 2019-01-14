b33b26a6d30699cb2f5ef7257da5e3b9e9b998dd
Will Dietz <w@wdtz.org>
2019-01-13 18:43:02 -0600

    intel-gpu-tools: pin and bump

--- pkgs/development/tools/misc/intel-gpu-tools/default.nix
+++ pkgs/development/tools/misc/intel-gpu-tools/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, libdrm, libpciaccess, cairo, pixman, udev, xorgproto
+{ stdenv, fetchurl, fetchgit, pkgconfig, libdrm, libpciaccess, cairo, pixman, udev, xorgproto
 , libX11, libXext, libXv, libXrandr, glib, bison, libunwind, python3, kmod
 , procps, utilmacros, gnome2, openssl, peg, meson, ninja, elfutils, flex }:
 
@@ -6,7 +6,11 @@ stdenv.mkDerivation rec {
   name = "intel-gpu-tools-${version}";
   version = "1.23-git";
 
-  src = fetchGit https://gitlab.freedesktop.org/drm/igt-gpu-tools;
+  src = fetchgit {
+    url = https://gitlab.freedesktop.org/drm/igt-gpu-tools;
+    rev = "fde4dce431bf324939a982017169214e0fa00d4f";
+    sha256 = "10x2pl171hsf798s725z7c6jwkqvcrnj360bw6i08bhi2313v622";
+  };
   #src = fetchurl {
   #  url = "https://xorg.freedesktop.org/archive/individual/app/igt-gpu-tools-${version}.tar.xz";
   #  sha256 = "1l4s95m013p2wvddwr4cjqyvsgmc88zxx2887p1fbb1va5n0hjsd";
