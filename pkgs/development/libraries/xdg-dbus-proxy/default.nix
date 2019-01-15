00457cae80271f9e1951bf85f644425b404557f7
Will Dietz <w@wdtz.org>
2019-01-15 12:53:00 -0600

    xdg-dbus-proxy: 0.1.0 -> 0.1.1

--- pkgs/development/libraries/xdg-dbus-proxy/default.nix
+++ pkgs/development/libraries/xdg-dbus-proxy/default.nix
@@ -1,18 +1,20 @@
 { stdenv, fetchurl, pkgconfig, glib }:
 
 let
-  version = "0.1.0";
+  version = "0.1.1";
 in stdenv.mkDerivation rec {
   name = "xdg-dbus-proxy-${version}";
 
   src = fetchurl {
     url = "https://github.com/flatpak/xdg-dbus-proxy/releases/download/${version}/${name}.tar.xz";
-    sha256 = "055wli36lvdannp6qqwbvd78353n61wn9kp8y3dchh39wq7x7vwy";
+    sha256 = "1w8yg5j51zsr9d97d4jjp9dvd7iq893p2xk54i6lf3lx01ribdqh";
   };
 
   nativeBuildInputs = [ pkgconfig ];
 
   buildInputs = [ glib ];
+
+  doCheck = false; # needs dbus-daemon instance
 
   meta = with stdenv.lib; {
     description = "DBus proxy for Flatpak and others";
