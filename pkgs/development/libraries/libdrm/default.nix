1cce5f4ccbe99d3fb9e6d6ee377f1f72fc3d81e3
Will Dietz <w@wdtz.org>
2019-01-22 11:05:11 -0600

    libdrm: 2.4.96 -> 2.4.97
    https://lists.x.org/archives/xorg-announce/2019-January/002937.html

--- pkgs/development/libraries/libdrm/default.nix
+++ pkgs/development/libraries/libdrm/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, pkgconfig, meson, ninja, libpthreadstubs, libpciaccess, valgrind-light }:
 
 stdenv.mkDerivation rec {
-  name = "libdrm-2.4.96";
+  name = "libdrm-2.4.97";
 
   src = fetchurl {
     url = "https://dri.freedesktop.org/libdrm/${name}.tar.bz2";
-    sha256 = "14xkip83qgljjaahzq40qgl60j54q7k00la1hbf5kk5lgg7ilmhd";
+    sha256 = "14xkip83qgljjaahzq41qgl60j54q7k00la1hbf5kk5lgg7ilmhd";
   };
 
   outputs = [ "out" "dev" "bin" ];
