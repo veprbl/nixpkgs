9655e7ae5033326e8bbc68c25963fbffca53dd99
Will Dietz <w@wdtz.org>
2019-01-29 16:36:13 -0600

    fontmatrix: 0.6.0 -> 2017-12-28 (git)

--- pkgs/applications/graphics/fontmatrix/default.nix
+++ pkgs/applications/graphics/fontmatrix/default.nix
@@ -2,20 +2,21 @@
 
 stdenv.mkDerivation rec {
   name = "fontmatrix-${version}";
-  version = "0.6.0";
+  version = "2017-12-28";
 
   src = fetchFromGitHub {
     owner = "fontmatrix";
     repo = "fontmatrix";
-    rev = "v${version}";
-    sha256 = "0aqndj1jhm6hjpwmj1qm92z2ljh7w78a5ff5ag47qywqha1ngn05";
+#    rev = "v${version}";
+    rev = "8108e6ea8b5944a92d7f27c40509b8e890ddaff1";
+    sha256 = "05c7q7vrsi0ska84dirr650awking59917q08hysn3plpkr0r8ai";
   };
 
   buildInputs = [ qt4 ];
 
   nativeBuildInputs = [ cmake ];
 
-  hardeningDisable = [ "format" ];
+  #hardeningDisable = [ "format" ];
 
   meta = with stdenv.lib; {
     description = "Fontmatrix is a free/libre font explorer for Linux, Windows and Mac";
