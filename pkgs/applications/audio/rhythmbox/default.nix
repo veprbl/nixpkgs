617723b2cd41726c886f91055fef1693956d14dd
Will Dietz <w@wdtz.org>
2019-02-05 15:02:55 -0600

    rhythmbox: 3.4.2 -> 3.4.3

--- pkgs/applications/audio/rhythmbox/default.nix
+++ pkgs/applications/audio/rhythmbox/default.nix
@@ -16,13 +16,13 @@
 }:
 let
   pname = "rhythmbox";
-  version = "3.4.2";
+  version = "3.4.3";
 in stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "0hzcns8gf5yb0rm4ss8jd8qzarcaplp5cylk6plwilsqfvxj4xn2";
+    sha256 = "1yx3n7p9vmv23jsv98fxwq95n78awdxqm8idhyhxx2d6vk4w1hgx";
   };
 
   patches = [
