ef8c4a817e4154e7006905be2954a09e141a12ba
Will Dietz <w@wdtz.org>
2019-02-11 16:08:36 -0600

    qownnotes: 19.2.0 -> 19.2.3

--- pkgs/applications/office/qownnotes/default.nix
+++ pkgs/applications/office/qownnotes/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "qownnotes";
-  version = "19.2.0";
+  version = "19.2.3";
 
   src = fetchurl {
     url = "https://download.tuxfamily.org/${pname}/src/${pname}-${version}.tar.xz";
     # Can grab official version like so:
     # $ curl https://download.tuxfamily.org/qownnotes/src/qownnotes-19.1.8.tar.xz.sha256
-    sha256 = "0n60cnzdfvwn126k8mh5m3wms9avjrnzfrpsvyfhg6l7vm6sbhdi";
+    sha256 = "3efb57dac22c75759112e7807944d8efdb2dc2b3862f85d8b854786e95775bb9";
   };
 
   nativeBuildInputs = [ qmake qttools ];
