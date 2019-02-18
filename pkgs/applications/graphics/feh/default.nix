5bb215d96f3c76e22ba55784c178ed25efd899d2
Will Dietz <w@wdtz.org>
2019-02-17 20:15:54 -0600

    feh: 3.1.2 -> 3.1.3

--- pkgs/applications/graphics/feh/default.nix
+++ pkgs/applications/graphics/feh/default.nix
@@ -6,11 +6,11 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "feh-${version}";
-  version = "3.1.2";
+  version = "3.1.3";
 
   src = fetchurl {
     url = "https://feh.finalrewind.org/${name}.tar.bz2";
-    sha256 = "0qjhlrgr606gc9h96w9piyd13mx63jqfbxxnan41nrh76m8d0dka";
+    sha256 = "builder for '/nix/store/iswrhaddk3fdjhfqr2p0v37vjy2ls2v2-feh-3.1.3.tar.bz2.drv' failed with exit code 1";
   };
 
   outputs = [ "out" "man" "doc" ];
