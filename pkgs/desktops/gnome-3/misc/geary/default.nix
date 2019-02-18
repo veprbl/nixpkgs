399061d4f85bff17d616973c1cd65f7d5f519caf
Will Dietz <w@wdtz.org>
2019-02-17 20:18:48 -0600

    geary: 0.12.4 -> 0.13.0

--- pkgs/desktops/gnome-3/misc/geary/default.nix
+++ pkgs/desktops/gnome-3/misc/geary/default.nix
@@ -6,43 +6,15 @@
 
 let
   pname = "geary";
-  version = "0.12.4";
+  version = "0.13.0";
 in
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
-  #src = fetchurl {
-  #  url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-  #  sha256 = "01ykhkjfkprvh9kp4rzrl6xs2pqibiw44ckvqsn5cs3xy2rlq8mm";
-  #};
-  src = fetchFromGitHub {
-    owner = "GNOME";
-    repo = pname;
-    rev = "292498b00fb6fb2de2576cb95c5682b4f8602dd5";
-    sha256 = "0gwwvv39d268kgp5l6bkpnixidbfpgbd70ia6gq0xibzypca9kcc";
+  src = fetchurl {
+    url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
+    sha256 = "builder for '/nix/store/6czc5xqmhqhah2331r826yka5zvhdr2v-geary-0.13.0.tar.xz.drv' failed with exit code 1";
   };
-
-  ## patches = [
-  ##   # Fix build with webkitgtk-2.22
-  ##   (fetchpatch {
-  ##     url = https://gitlab.gnome.org/GNOME/geary/commit/5d0f711426d76f878cf9b71f7e8f785199c7cde1.patch;
-  ##     sha256 = "1yifng5lfsc6wp7irmi8gjdcfig1cr0chf7rdv3asrk567nmwrsi";
-  ##   })
-  ##   (fetchpatch {
-  ##     url = https://gitlab.gnome.org/GNOME/geary/commit/0d966950a2cba888873cd3a7f4f42bb7a017dc6d.patch;
-  ##     sha256 = "1y6v4fnik4w3paj9nl0yqs54998sx1zr9w3940d579p6dsa8f3fg";
-  ##   })
-  ##   (fetchpatch {
-  ##     url = https://gitlab.gnome.org/GNOME/geary/commit/e091f24b00ec421e1aadd5e360d1550e658ad5ef.patch;
-  ##     sha256 = "0d5hc4h9c1hnn2sk18nkpmzdvwm3h746n2zj8n22ax9rj6lxl38l";
-  ##   })
-  ##   # Fix build with vala 0.40.12
-  ##   # See: https://gitlab.gnome.org/GNOME/vala/blob/0.40.12/NEWS#L22
-  ##   (fetchpatch {
-  ##     url = "https://gitlab.gnome.org/GNOME/geary/commit/088cb2c0aa35ad4b54ea5a0a2edaf0ff96c64b45.patch";
-  ##     sha256 = "0cnjmbd3snm8ggmprqa32f7i3w86gs3ylab9p5ffj921dcpvvlb2";
-  ##   })
-  ## ];
 
   nativeBuildInputs = [ vala_0_40 intltool itstool pkgconfig wrapGAppsHook meson ninja desktop-file-utils gnome-doc-utils gobject-introspection python3 ];
   buildInputs = [
