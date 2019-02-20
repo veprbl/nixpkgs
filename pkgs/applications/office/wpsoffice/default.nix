7043d59b87125ad0ba971253d7f721b2232fc7bc
Will Dietz <w@wdtz.org>
2019-02-20 00:59:50 -0600

    wpsoffice: 10.1.0.5672 -> 10.1.0.6757

--- pkgs/applications/office/wpsoffice/default.nix
+++ pkgs/applications/office/wpsoffice/default.nix
@@ -6,16 +6,16 @@ let
   bits = if stdenv.hostPlatform.system == "x86_64-linux" then "x86_64"
          else "x86";
 
-  version = "10.1.0.5672";
+  version = "10.1.0.6757";
 in stdenv.mkDerivation rec{
   name = "wpsoffice-${version}";
 
   src = fetchurl {
     name = "${name}.tar.xz";
-    url = "http://kdl.cc.ksosoft.com/wps-community/download/a21/wps-office_${version}~a21_${bits}.tar.xz";
+    url = "http://kdl.cc.ksosoft.com/wps-community/download/6757/wps-office_${version}_${bits}.tar.xz";
     sha256 = if bits == "x86_64" then
-      "0mi3n9kplf82gd0g2m0np957agy53p4g1qh81pbban49r4n0ajcz" else
-      "1dk400ap5qwdhjvn8lnk602f5akayr391fkljxdkrpn5xac01m97";
+      "0zk2shi3ciyjnmwvixkmj2qnivkg9a86mnvn83sj9idhlgjx7002" else
+      "11b9p367zg3ly43c807h2sqxm3c8x5capcwxavacaslywcxad4fj";
   };
 
   meta = {
