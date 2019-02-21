6c4d49b14791608e4af953441d5b434bc9910108
Will Dietz <w@wdtz.org>
2019-02-21 14:58:54 -0600

    bah

--- nixos/modules/system/etc/setup-etc.pl
+++ nixos/modules/system/etc/setup-etc.pl
@@ -44,8 +44,10 @@ sub isStatic {
     my $path = shift;
 
     if (-l $path) {
-        my $target = File::Spec->canonpath(File::Spec->rel2abs(readlink $path, dirname $path));
-        return substr($target, 0, length "/etc/static/") eq "/etc/static/";
+        # incomplete!
+        my $target = File::Spec->rel2abs(readlink $path, dirname $path);
+        #my $targetdir = File::Spec->canonpath(dirname $target);
+        return substr($target, 0, length $static) eq $static;
     }
 
     if (-d $path) {
