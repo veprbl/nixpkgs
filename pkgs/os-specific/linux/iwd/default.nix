cb55f24666c00daa439599c7ccfaf53aa1b50d63
Will Dietz <w@wdtz.org>
2019-02-25 15:23:14 -0600

    iwd: use VHT rates in scan, test

--- pkgs/os-specific/linux/iwd/default.nix
+++ pkgs/os-specific/linux/iwd/default.nix
@@ -15,9 +15,9 @@ in stdenv.mkDerivation rec {
 
   src = fetchgit {
     url = https://git.kernel.org/pub/scm/network/wireless/iwd.git;
-    rev = "ad2bf340a4f1800617a05b5697cb3418d1d5a434";
+    rev = "d47e31954b807f402e53d85f9c821f381a13b519";
     #rev = version;
-    sha256 = "0c6gjc5s8nzql8gl1mr8yb678i5w9igcvmkbgkblpkngrqk0qlq5";
+    sha256 = "0ffmv4j8km4nvvja60c8mcg7viw5jxsxxnk1rwrvikc2lg4sdv6f";
   };
 
   nativeBuildInputs = [
