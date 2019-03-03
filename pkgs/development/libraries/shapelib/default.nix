8cda0ba7a53c37196438f7da06163b28b2daf5db
Will Dietz <w@wdtz.org>
2019-03-02 18:04:16 -0600

    shapelib: as in 1.5.0 notes, no longer need 'proj' dep

--- pkgs/development/libraries/shapelib/default.nix
+++ pkgs/development/libraries/shapelib/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, proj }:
+{ stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
   name = "shapelib-1.5.0";
@@ -7,8 +7,6 @@ stdenv.mkDerivation rec {
     url = "https://download.osgeo.org/shapelib/${name}.tar.gz";
     sha256 = "1qfsgb8b3yiqwvr6h9m81g6k9fjhfys70c22p7kzkbick20a9h0z";
   };
-
-  buildInputs =  [ proj ];
 
   meta = with stdenv.lib; {
     description = "C Library for reading, writing and updating ESRI Shapefiles";
