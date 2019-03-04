6182c1b88303d9cbb94161e0bdfa8d421c181955
Will Dietz <w@wdtz.org>
2019-03-04 10:23:57 -0600

    motif: 2.3.6 -> 2.3.8

--- pkgs/development/libraries/motif/default.nix
+++ pkgs/development/libraries/motif/default.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation rec {
   name = "motif-${version}";
-  version = "2.3.6";
+  version = "2.3.8";
 
   src = fetchurl {
     url = "mirror://sourceforge/motif/${name}.tar.gz";
-    sha256 = "1ksqbp0bzdw6wcrx8s4hj4ivvxmw54hz85l2xfigb87cxmmhx0gs";
+    sha256 = "1rxwkrhmj8sfg7dwmkhq885valwqbh26d79033q7vb7fcqv756w5";
   };
 
   buildInputs = [
@@ -30,14 +30,11 @@ stdenv.mkDerivation rec {
 
   makeFlags = [ "CFLAGS=-fno-strict-aliasing" ];
 
-  prePatch = ''
-    rm lib/Xm/Xm.h
-  '' + stdenv.lib.optionalString (!demoSupport) ''
+  prePatch = stdenv.lib.optionalString (!demoSupport) ''
     sed '/^SUBDIRS =,^$/s/\<demos\>//' -i Makefile.{am,in}
   '';
 
   patches = [ ./Remove-unsupported-weak-refs-on-darwin.patch
-              ./Use-correct-header-for-malloc.patch
               ./Add-X.Org-to-bindings-file.patch
             ];
 
