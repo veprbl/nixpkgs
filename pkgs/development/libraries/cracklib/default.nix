440e66bca8b445eb7c4374612a699dc1da449122
Will Dietz <w@wdtz.org>
2019-03-03 23:23:03 -0600

    Revert "cracklib: build default dictionary, run test"
    This reverts commit 1a4f4c2662e4ece7483dd968e7cc6ee80f27b07c.

--- pkgs/development/libraries/cracklib/default.nix
+++ pkgs/development/libraries/cracklib/default.nix
@@ -1,6 +1,5 @@
 { stdenv, fetchurl, zlib, gettext }:
 
-# TODO: wordlist? https://github.com/cracklib/cracklib/releases/download/v2.9.7/cracklib-words-2.9.7.gz is a start!
 stdenv.mkDerivation rec {
   pname = "cracklib";
   version = "2.9.7";
@@ -11,17 +10,6 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [ zlib gettext ];
-
-  postPatch = ''
-    chmod +x util/cracklib-format
-    patchShebangs util
-  '';
-
-  postInstall = ''
-    make dict
-  '';
-  doInstallCheck = true;
-  installCheckTarget = "test";
 
   meta = with stdenv.lib; {
     homepage    = https://github.com/cracklib/cracklib;
