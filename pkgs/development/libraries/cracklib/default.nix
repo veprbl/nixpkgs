71be21eab3622604893183cbce22d81b7aa228fa
Will Dietz <w@wdtz.org>
2019-03-03 23:23:05 -0600

    Revert "cracklib: 2.9.6 -> 2.9.7"
    This reverts commit d3dd0e3f73e89f7eb09beff383f6d915eef1462b.

--- pkgs/development/libraries/cracklib/default.nix
+++ pkgs/development/libraries/cracklib/default.nix
@@ -1,12 +1,11 @@
 { stdenv, fetchurl, zlib, gettext }:
 
 stdenv.mkDerivation rec {
-  pname = "cracklib";
-  version = "2.9.7";
+  name = "cracklib-2.9.6";
 
   src = fetchurl {
-    url = "https://github.com/${pname}/${pname}/releases/download/v${version}/${pname}-${version}.tar.bz2";
-    sha256 = "1rimpjsdnmw8f5b7k558cic41p2qy2n2yrlqp5vh7mp4162hk0py";
+    url = "https://github.com/cracklib/cracklib/releases/download/${name}/${name}.tar.gz";
+    sha256 = "0hrkb0prf7n92w6rxgq0ilzkk6rkhpys2cfqkrbzswp27na7dkqp";
   };
 
   buildInputs = [ zlib gettext ];
