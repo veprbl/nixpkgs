ebb57beb574c6a0ca1742f4ce5758afd41683b8b
Will Dietz <w@wdtz.org>
2019-03-07 14:09:38 -0600

    mkchromecast: add notification support deps

--- pkgs/applications/networking/mkchromecast/default.nix
+++ pkgs/applications/networking/mkchromecast/default.nix
@@ -31,6 +31,9 @@ python3Packages.buildPythonApplication rec {
     pkgs.lame
     pkgs.ffmpeg
     pkgs.nodejs
+    # notify
+    pkgs.libnotify
+    pygobject3
   ] ++ stdenv.lib.optional alsaSupport [ pkgs.alsaLib pkgs.alsaUtils pkgs.ffmpeg ]
     ++ stdenv.lib.optional (!alsaSupport) pkgs.pulseaudio
     ++ stdenv.lib.optional systemTray python3Packages.pyqt5
