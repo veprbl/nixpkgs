f2281ca070ace9250654f844b706f45be0dd2745
Will Dietz <w@wdtz.org>
2019-03-07 22:38:32 -0600

    xteddy: fixup scripts, at least xtoys O:)

--- pkgs/applications/misc/xteddy/default.nix
+++ pkgs/applications/misc/xteddy/default.nix
@@ -8,16 +8,13 @@ stdenv.mkDerivation rec {
     sha256 = "1qli69im6pani8wzmryavndspbcwc4298yl5d6s7qy085qg5m26q";
   };
   postPatch = ''
-    sed -i -e 's,/usr/\(local\)\?share,${placeholder "out"}/share,' \
-      configure xteddy.c Makefile.in images/Makefile.in
+    sed -i -e 's,/usr/\(local\)\?share,${placeholder "out"}/share,g' \
+      configure xteddy.c Makefile.in images/Makefile.in xteddy_test xtoys
+    substituteInPlace xtoys --replace /usr/games/xteddy $out/bin/xteddy
   '';
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ imlib2 xorg.libX11 xorg.libXext ];
   makeFlags = [ "LIBS=-lXext" ];
-  postInstall = ''
-    # remove broken scripts
-    rm $out/bin/{xtoys,xteddy_test}
-  '';
 
   meta = with stdenv.lib; {
     description = "cuddly teddy bear for your X desktop";
