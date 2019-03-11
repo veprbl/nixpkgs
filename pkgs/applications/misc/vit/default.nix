0ce8563bfbdea2401ecc6696ced686edf82a273c
Will Dietz <w@wdtz.org>
2019-03-10 22:17:42 -0500

    vit: 1.3.beta1 -> 1.3, cleanup + maintain
    1.3 is essentially same as long-time beta 1.3.beta1

--- pkgs/applications/misc/vit/default.nix
+++ pkgs/applications/misc/vit/default.nix
@@ -1,24 +1,23 @@
-{ pkgs, fetchFromGitHub, stdenv, makeWrapper, taskwarrior, ncurses,
-perl, perlPackages, which }:
+{ stdenv, fetchFromGitHub
+, makeWrapper, which
+, taskwarrior, ncurses, perlPackages }:
 
-let
-  version = "1.3.beta1";
-in
-stdenv.mkDerivation {
-  name = "vit-${version}";
+stdenv.mkDerivation rec {
+  pname = "vit";
+  version = "1.3";
 
   src = fetchFromGitHub {
     owner = "scottkosty";
-    repo = "vit";
+    repo = pname;
     rev = "v${version}";
-    sha256 = "1hxki4m8ahgkzh3d389xrxhq6ac73ihavsw3cn1b4mn1kygcl3j2";
+    sha256 = "0a34rh5w8393wf7jwwr0f74rp1zv2vz606z5j8sr7w19k352ijip";
   };
 
   preConfigure = ''
     substituteInPlace Makefile.in \
       --replace sudo ""
     substituteInPlace configure \
-      --replace /usr/bin/perl ${perl}/bin/perl
+      --replace /usr/bin/perl ${perlPackages.perl}/bin/perl
     substituteInPlace cmdline.pl \
       --replace "view " "vim -R "
   '';
@@ -28,14 +27,14 @@ stdenv.mkDerivation {
   '';
 
   nativeBuildInputs = [ makeWrapper which ];
-  buildInputs = [ taskwarrior ncurses perl ]
-    ++ (with perlPackages; [ Curses TryTiny TextCharWidth ]);
+  buildInputs = [ taskwarrior ncurses ]
+    ++ (with perlPackages; [ perl Curses TryTiny TextCharWidth ]);
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "Visual Interactive Taskwarrior";
-    maintainers = with pkgs.lib.maintainers; [ ];
-    platforms = pkgs.lib.platforms.all;
-    license = pkgs.lib.licenses.gpl3;
+    maintainers = with maintainers; [ dtzWill ];
+    platforms = platforms.all;
+    license = licenses.gpl3;
   };
 }
 
