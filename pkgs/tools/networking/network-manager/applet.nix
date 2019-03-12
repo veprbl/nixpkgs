becd3b22dbbe08d346872fe0b2bdae3dd150e2d0
Will Dietz <w@wdtz.org>
2019-03-12 14:35:27 -0500

    networkmanagerapplet: -> git, as well

--- pkgs/tools/networking/network-manager/applet.nix
+++ pkgs/tools/networking/network-manager/applet.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, meson, ninja, intltool, gtk-doc, pkgconfig, networkmanager, gnome3
+{ stdenv, fetchurl, fetchFromGitLab, meson, ninja, intltool, gtk-doc, pkgconfig, networkmanager, gnome3
 , libnotify, libsecret, polkit, isocodes, modemmanager, libxml2, docbook_xsl, docbook_xml_dtd_43
 , mobile-broadband-provider-info, glib-networking, gsettings-desktop-schemas
 , libgudev, jansson, wrapGAppsHook, gobject-introspection, python3, gtk3
@@ -6,13 +6,20 @@
 
 let
   pname = "network-manager-applet";
-  version = "1.8.20";
+  version = "1.8.20.99"; # not really, git
 in stdenv.mkDerivation rec {
   name = "${pname}-${version}";
 
-  src = fetchurl {
-    url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "1v1lvw9ak37gxha11rv49sai1vdyv128hdy0kliibiv6alavn385";
+  #src = fetchurl {
+  #  url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
+  #  sha256 = "1v1lvw9ak37gxha11rv49sai1vdyv128hdy0kliibiv6alavn385";
+  #};
+  src = fetchFromGitLab {
+    domain = "gitlab.gnome.org";
+    owner = "GNOME";
+    repo = pname;
+    rev = "6474c16bc4982d974a7ee5767be6a7cab323c1e0";
+    sha256 = "1xh7qcvwvllj687c9bggkdhd30v28b1w4mf48lmphr468dgi9c4c";
   };
 
   mesonFlags = [
