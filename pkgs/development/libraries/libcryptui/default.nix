1c22cd17dade1881dff08f378cab204e2bb8b9ff
Will Dietz <w@wdtz.org>
2019-03-13 18:04:30 -0500

    libcryptui: latest git, years later ;), work w/the usual bits

--- pkgs/development/libraries/libcryptui/default.nix
+++ pkgs/development/libraries/libcryptui/default.nix
@@ -1,16 +1,25 @@
-{ stdenv, fetchurl, pkgconfig, intltool, glib, gnome3, gtk3, gnupg20, gpgme, dbus-glib, libgnome-keyring }:
+{ stdenv, fetchurl, fetchFromGitLab, autoreconfHook, gtk-doc, which, pkgconfig, intltool, glib, gnome3, gtk3, gnupg, gpgme, dbus-glib, gcr }:
 
 stdenv.mkDerivation rec {
   pname = "libcryptui";
   version = "3.12.2";
 
-  src = fetchurl {
-    url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0rh8wa5k2iwbwppyvij2jdxmnlfjbna7kbh2a5n7zw4nnjkx3ski";
+  src = fetchFromGitLab {
+    domain = "gitlab.gnome.org";
+    owner = "GNOME";
+    repo = pname;
+    rev = "1c664b3d6f8d7985cec38c93d2b6a6334c2fbe49";
+    sha256 = "0nsh64wj3mdgl4i4z7ma56n97ns6pibszkfgpwfn8c0a2i1gpibq";
   };
+  #src = fetchurl {
+  #  url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
+  #  sha256 = "0rh8wa5k2iwbwppyvij2jdxmnlfjbna7kbh2a5n7zw4nnjkx3ski";
+  #};
 
-  nativeBuildInputs = [ pkgconfig intltool ];
-  buildInputs = [ glib gtk3 gnupg20 gpgme dbus-glib libgnome-keyring ];
+  autoreconfPhase = "./autogen.sh";
+
+  nativeBuildInputs = [ autoreconfHook pkgconfig intltool gtk-doc which gnome3.gnome-common ];
+  buildInputs = [ glib gtk3 gnupg gpgme dbus-glib gcr ];
   propagatedBuildInputs = [ dbus-glib ];
 
   passthru = {
