e6dcf6342ee218bc761ecb421eadcbb794e1e3ba
Will Dietz <w@wdtz.org>
2019-03-14 16:38:55 -0500

    libtermkey: 0.20 -> 0.22

--- pkgs/development/libraries/libtermkey/default.nix
+++ pkgs/development/libraries/libtermkey/default.nix
@@ -1,20 +1,19 @@
 { stdenv, lib, fetchzip, libtool, pkgconfig, ncurses }:
 
 stdenv.mkDerivation rec {
-  name = "libtermkey-${version}";
-
-  version = "0.20";
+  pname = "libtermkey";
+  version = "0.22";
 
   src = fetchzip {
     url = "http://www.leonerd.org.uk/code/libtermkey/libtermkey-${version}.tar.gz";
-    sha256 = "1i5a2zangq61ba1vdkag34ig5g4gzccldccdbcmqmk93saa6lkbx";
+    sha256 = "02dks6bj7n23lj005yq41azf95wh3hapmgc2lzyh12vigkjh67rg";
   };
 
-  makeFlags = [ "PREFIX=$(out)" ]
+  makeFlags = [ "PREFIX=${placeholder "out"}" ]
     ++ stdenv.lib.optional stdenv.isDarwin "LIBTOOL=${libtool}/bin/libtool";
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ libtool ncurses ];
+  nativeBuildInputs = [ pkgconfig libtool ];
+  buildInputs = [ ncurses ];
 
   meta = with lib; {
     description = "Terminal keypress reading library";
