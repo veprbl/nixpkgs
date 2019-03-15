52c91f339be85a3e525b653b35fedf60f9e87fec
Will Dietz <w@wdtz.org>
2019-03-14 19:52:35 -0500

    gitAndTools: hashfix \o/

--- pkgs/applications/version-management/git-and-tools/lab/default.nix
+++ pkgs/applications/version-management/git-and-tools/lab/default.nix
@@ -11,7 +11,7 @@ buildGoModule rec {
     sha256 = "1210cf6ss4ivm2jxq3k3c34vpra02pl91fpmvqbvw5sm53j7xfaf";
   };
 
-  modSha256 = "1111111111111111111111111111111111111111111111111111";
+  modSha256 = "1g72fnkibmdd9i20wgp5fjf556xp2xwan06dmn3f5rkmnicy3ccl";
 
   meta = with stdenv.lib; {
     description = "Lab wraps Git or Hub, making it simple to clone, fork, and interact with repositories on GitLab";
