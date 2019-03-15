6787e1207ac07e4452afbfd87dd9716e2d9a365e
Will Dietz <w@wdtz.org>
2019-03-14 19:50:02 -0500

    gitAndTools.lab: wip buildGoModule

--- pkgs/applications/version-management/git-and-tools/lab/default.nix
+++ pkgs/applications/version-management/git-and-tools/lab/default.nix
@@ -1,12 +1,8 @@
-{ stdenv, buildGoPackage, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   name = "lab-${version}";
   version = "0.15.2";
-
-  goPackagePath = "github.com/zaquestion/lab";
-
-  subPackages = [ "." ];
 
   src = fetchFromGitHub {
     owner = "zaquestion";
@@ -15,7 +11,7 @@ buildGoPackage rec {
     sha256 = "1210cf6ss4ivm2jxq3k3c34vpra02pl91fpmvqbvw5sm53j7xfaf";
   };
 
-  goDeps = ./deps.nix;
+  modSha256 = "1111111111111111111111111111111111111111111111111111";
 
   meta = with stdenv.lib; {
     description = "Lab wraps Git or Hub, making it simple to clone, fork, and interact with repositories on GitLab";
