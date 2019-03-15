bb0853eca02b723ec97f85552493c9ecb7841e55
Will Dietz <w@wdtz.org>
2019-03-15 10:08:11 -0500

    Revert "freetype2: build and install ft2demos as well"
    Not worth rebuilds, keep for later as they're useful :)
    
    This reverts commit 1117055dcb02fb8b4a0fc630e3003c5d6eabb771.

--- pkgs/development/libraries/freetype/default.nix
+++ pkgs/development/libraries/freetype/default.nix
@@ -7,22 +7,14 @@
   # LCD filtering is also known as ClearType and covered by several Microsoft patents.
   # This option allows it to be disabled. See http://www.freetype.org/patents.html.
   useEncumberedCode ? true
-, xorg
 }:
 
 let
   inherit (stdenv.lib) optional optionalString;
 
-  version = "2.10.0";
-
-  demos_src = fetchurl {
-    url = "mirror://savannah/freetype/ft2demos-${version}.tar.bz2";
-    sha256 = "1fh6dmk6xn2jalcsg4ml5vvm5z5zn62pn6qzpnq4k4v0rp1gjrh4";
-  };
-
 in stdenv.mkDerivation rec {
   pname = "freetype";
-  inherit version;
+  version = "2.10.0";
 
   meta = with stdenv.lib; {
     description = "A font rendering engine";
@@ -44,11 +36,6 @@ in stdenv.mkDerivation rec {
     sha256 = "01mybx78n3n9dhzylbrdy42wxdwfn8rp514qdkzjy6b5ij965k7w";
   };
 
-  postUnpack = ''
-    unpackFile ${demos_src}
-    mv ft2demos-* ft2demos
-  '';
-
   propagatedBuildInputs = [ zlib bzip2 libpng ]; # needed when linking against freetype
   # dependence on harfbuzz is looser than the reverse dependence
   nativeBuildInputs = [ pkgconfig which makeWrapper ]
@@ -60,19 +47,9 @@ in stdenv.mkDerivation rec {
     ] ++
     optional useEncumberedCode ./enable-subpixel-rendering.patch;
 
-  outputs = [ "out" "dev" "demos" ];
+  outputs = [ "out" "dev" ];
 
   configureFlags = [ "--disable-static" "--bindir=$(dev)/bin" "--enable-freetype-config" ];
-
-  postBuild = ''
-    make -C ../ft2demos \
-      TOP_DIR=$PWD \
-      TOP_DIR_2=$PWD/../ft2demos \
-      X11_PATH=${xorg.libX11}/include \
-      exes
-  '';
-
-  buildInputs = [ xorg.libX11 ];
 
   # native compiler to generate building tool
   CC_BUILD = "${buildPackages.stdenv.cc}/bin/cc";
@@ -90,7 +67,6 @@ in stdenv.mkDerivation rec {
 
     wrapProgram "$dev/bin/freetype-config" \
       --set PKG_CONFIG_PATH "$PKG_CONFIG_PATH:$dev/lib/pkgconfig"
-  '' + ''
-    install -Dm755 -t $demos/bin ../ft2demos/bin/.libs/*
   '';
+
 }
