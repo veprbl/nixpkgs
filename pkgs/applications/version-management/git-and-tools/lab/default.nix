f29a385312b0270a5cb996a90f40e537f6cfe79c
Will Dietz <w@wdtz.org>
2019-03-17 16:10:35 -0500

    lab: generate and install zsh completion
    Env variables are set to dummy values
    so `lab` doesn't get stuck trying to helpfully
    interact with user for initial configuration.

--- pkgs/applications/version-management/git-and-tools/lab/default.nix
+++ pkgs/applications/version-management/git-and-tools/lab/default.nix
@@ -15,6 +15,12 @@ buildGoModule rec {
 
   modSha256 = "0bw47dd1b46ywsian2b957a4ipm77ncidipzri9ra39paqlv7abb";
 
+  postInstall = ''
+    mkdir -p $out/share/zsh/site-functions
+    LAB_CORE_HOST=a LAB_CORE_USER=b LAB_CORE_TOKEN=c \
+    $out/bin/lab completion zsh > $out/share/zsh/site-functions/_lab
+  '';
+
   meta = with stdenv.lib; {
     description = "Lab wraps Git or Hub, making it simple to clone, fork, and interact with repositories on GitLab";
     homepage = https://zaquestion.github.io/lab;
