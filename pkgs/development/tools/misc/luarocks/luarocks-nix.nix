4d70c94da4172e2ba6464a13d3d219d1926e5c61
Will Dietz <w@wdtz.org>
2019-03-25 03:20:21 -0500

    luarocks-nix: local source for testing

--- pkgs/development/tools/misc/luarocks/luarocks-nix.nix
+++ pkgs/development/tools/misc/luarocks/luarocks-nix.nix
@@ -1,10 +1,11 @@
 { luarocks, fetchFromGitHub }:
 luarocks.overrideAttrs(old: {
   pname = "luarocks-nix";
-  src = fetchFromGitHub {
-    owner = "teto";
-    repo = "luarocks";
-    rev = "8fb03a9bc8f4fa079d26c0f02804139bb2578848";
-    sha256 = "09iwjvs9sbk6vwhrh7sijmfpji6wvg5bbdraw7l5lpnr9jj5wy91";
-  };
+  src = fetchGit /home/will/src/all/luarocks;
+  #src = fetchFromGitHub {
+  #  owner = "teto";
+  #  repo = "luarocks";
+  #  rev = "8fb03a9bc8f4fa079d26c0f02804139bb2578848";
+  #  sha256 = "09iwjvs9sbk6vwhrh7sijmfpji6wvg5bbdraw7l5lpnr9jj5wy91";
+  #};
 })
