86f7d4d805d6e79d15972870c0ab9d02c29e0c66
Alex Rice <alexrice999@hotmail.co.uk>
2019-03-25 19:01:42 +0000

    brillo: use fetchFromGitLab

--- pkgs/os-specific/linux/brillo/default.nix
+++ pkgs/os-specific/linux/brillo/default.nix
@@ -1,11 +1,13 @@
-{stdenv, fetchurl, which, go-md2man, coreutils}:
+{stdenv, fetchFromGitLab , which, go-md2man, coreutils}:
 
 stdenv.mkDerivation rec {
   version = "1.4.3";
   name = "brillo-${version}";
-  src = fetchurl {
-  url = "https://gitlab.com/cameronnemo/brillo/-/archive/v${version}/brillo-v${version}.tar.bz2";
-    sha256 = "0wjpw9vn521rwpnlhvczzfzdsdq812vsyl151627qw51zadynvz1";
+  src = fetchFromGitLab {
+    owner= "cameronnemo";
+    repo= "brillo";
+    rev= "v${version}";
+    sha256 = "1syv3iav7bwr84x9frz1qd6qmgp8ldbjs4gl3r94nhllkai9spaq";
   };
   makeFlags = [ "PREFIX=" "DESTDIR=$(out)"];
   nativeBuildInputs = [go-md2man which];
