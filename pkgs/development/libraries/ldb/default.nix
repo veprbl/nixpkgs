90fbc98dea8500545870b75169e3f5e0b16bc723
Will Dietz <w@wdtz.org>
2019-03-25 14:42:26 -0500

    ldb: 1.3.3 -> 1.6.3

--- pkgs/development/libraries/ldb/default.nix
+++ pkgs/development/libraries/ldb/default.nix
@@ -1,21 +1,22 @@
-{ stdenv, fetchurl, python, pkgconfig, readline, tdb, talloc, tevent
+{ stdenv, fetchurl, python3, pkgconfig, which, readline, tdb, talloc, tevent
 , popt, libxslt, docbook_xsl, docbook_xml_dtd_42, cmocka
 }:
 
 stdenv.mkDerivation rec {
-  name = "ldb-1.3.3";
+  pname = "ldb";
+  version = "1.6.3";
 
   src = fetchurl {
-    url = "mirror://samba/ldb/${name}.tar.gz";
-    sha256 = "14gsrm7dvyjpbpnc60z75j6fz2p187abm2h353lq95kx2bv70c1b"                                                                                                                                                             ;
+    url = "mirror://samba/${pname}/${pname}-${version}.tar.gz";
+    sha256 = "01livdy3g073bm6xnc8zqnqrxg53sw8q66d1903z62hd6g87whsa"                                                                                                                                                             ;
   };
 
   outputs = [ "out" "dev" ];
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ pkgconfig python3 docbook_xsl docbook_xml_dtd_42 ];
   buildInputs = [
-    python readline tdb talloc tevent popt
-    libxslt docbook_xsl docbook_xml_dtd_42
+    readline tdb talloc tevent popt
+    libxslt
     cmocka
   ];
 
@@ -31,12 +32,13 @@ stdenv.mkDerivation rec {
   ];
 
   preConfigure = ''
-    sed -i 's,#!/usr/bin/env python,#!${python}/bin/python,g' buildtools/bin/waf
+    patchShebangs buildtools/bin/waf
   '';
 
   configureFlags = [
     "--bundled-libraries=NONE"
     "--builtin-libraries=replace"
+    "--disable-python"
   ];
 
   stripDebugList = "bin lib modules";
