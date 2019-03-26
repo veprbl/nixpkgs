38efd5be14d5134b2eb33fcce5c3fdf3d6afc6b9
Will Dietz <w@wdtz.org>
2019-03-26 16:24:34 -0500

    sync

--- pkgs/tools/misc/fontforge/default.nix
+++ pkgs/tools/misc/fontforge/default.nix
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
   patches = [ ./fontforge-20140813-use-system-uthash.patch ];
 
   # do not use x87's 80-bit arithmetic, rouding errors result in very different font binaries
-#  NIX_CFLAGS_COMPILE = lib.optionals stdenv.isi686 [ "-msse2" "-mfpmath=sse" ];
+  NIX_CFLAGS_COMPILE = lib.optionals stdenv.isi686 [ "-msse2" "-mfpmath=sse" ];
 
   nativeBuildInputs = [ pkgconfig autoconf automake gnum4 libtool perl gettext ];
   buildInputs = [
@@ -35,8 +35,11 @@ stdenv.mkDerivation rec {
     ++ lib.optional withGTK "--enable-gtk2-use"
     ++ lib.optional (!withGTK) "--without-x";
 
+  # work-around: git isn't really used, but configuration fails without it
   preConfigure = ''
+    # The way $version propagates to $version of .pe-scripts (https://github.com/dejavu-fonts/dejavu-fonts/blob/358190f/scripts/generate.pe#L19)
     export SOURCE_DATE_EPOCH=$(date -d ${version} +%s)
+
     export GIT="$(type -P true)"
     cp -r "${gnulib}" ./gnulib
     chmod +w -R ./gnulib
