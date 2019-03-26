d1cf3b42041af3dd662b15da8cee3be3d235f115
Will Dietz <w@wdtz.org>
2019-03-26 15:19:50 -0500

    fontforge: fix uthash patch, clean a bit

--- pkgs/tools/misc/fontforge/default.nix
+++ pkgs/tools/misc/fontforge/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, lib
+{ stdenv, fetchurl, lib
 , autoconf, automake, gnum4, libtool, perl, gnulib, uthash, pkgconfig, gettext
 , python, freetype, zlib, glib, libungif, libpng, libjpeg, libtiff, libxml2, cairo, pango
 , withSpiro ? false, libspiro
@@ -8,28 +8,15 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "fontforge-${version}";
-#  version = "20170730";
-  version = "20190226";
+  pname = "fontforge";
+  version = "20190317";
 
-  src = fetchFromGitHub {
-    owner = "fontforge";
-    repo = "fontforge";
-    #rev = version;
-    rev = "cec4a984abb41419bf92fc58e5de0170404f0303";
-    sha256 = "0kkpjzbi30q3npm8d0dhqd9fmzwyfx77cnzyc4d65hfrdf89hcsw";
+  src = fetchurl {
+    url = "https://github.com/${pname}/${pname}/releases/download/${version}/${pname}-${version}.tar.gz";
+    sha256 = "1ddqbpc32cgbccdnv0lfw0qhj59hcqzb7616ph5lkvm91pnas4dp";
   };
 
   patches = [ ./fontforge-20140813-use-system-uthash.patch ];
-
-  # use $SOURCE_DATE_EPOCH instead of non-determenistic timestamps
-  #postPatch = ''
-  #  find . -type f -name '*.c' -exec sed -r -i 's#\btime\(&(.+)\)#if (getenv("SOURCE_DATE_EPOCH")) \1=atol(getenv("SOURCE_DATE_EPOCH")); else &#g' {} \;
-  #  sed -r -i 's#author\s*!=\s*NULL#& \&\& !getenv("SOURCE_DATE_EPOCH")#g'                            fontforge/cvexport.c fontforge/dumppfa.c fontforge/print.c fontforge/svg.c fontforge/splineutil2.c
-  #  sed -r -i 's#\bb.st_mtime#getenv("SOURCE_DATE_EPOCH") ? atol(getenv("SOURCE_DATE_EPOCH")) : &#g'  fontforge/parsepfa.c fontforge/sfd.c fontforge/svg.c
-  #  sed -r -i 's#^\s*ttf_fftm_dump#if (!getenv("SOURCE_DATE_EPOCH")) ttf_fftm_dump#g'                 fontforge/tottf.c
-  #  sed -r -i 's#sprintf\(.+ author \);#if (!getenv("SOURCE_DATE_EPOCH")) &#g'                        fontforgeexe/fontinfo.c
-  #'';
 
   # do not use x87's 80-bit arithmetic, rouding errors result in very different font binaries
 #  NIX_CFLAGS_COMPILE = lib.optionals stdenv.isi686 [ "-msse2" "-mfpmath=sse" ];
@@ -47,17 +34,6 @@ stdenv.mkDerivation rec {
     lib.optionals (!withPython) [ "--disable-python-scripting" "--disable-python-extension" ]
     ++ lib.optional withGTK "--enable-gtk2-use"
     ++ lib.optional (!withGTK) "--without-x";
-
-  # work-around: git isn't really used, but configuration fails without it
-  preConfigure = ''
-    # The way $version propagates to $version of .pe-scripts (https://github.com/dejavu-fonts/dejavu-fonts/blob/358190f/scripts/generate.pe#L19)
-    export SOURCE_DATE_EPOCH=$(date -d ${version} +%s)
-
-    export GIT="$(type -P true)"
-    cp -r "${gnulib}" ./gnulib
-    chmod +w -R ./gnulib
-    ./bootstrap --skip-git --gnulib-srcdir=./gnulib
-  '';
 
   doCheck = false; # tries to wget some fonts
   doInstallCheck = doCheck;
