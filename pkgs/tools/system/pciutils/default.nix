008433ef0ec2fabf02da1b61bb60ede553f12d65
Will Dietz <w@wdtz.org>
2019-03-27 07:38:18 -0500

    Revert "pciutils: git, modernize" (rebuilds)
    This reverts commit aa9bd57c28139cecf6f82d455384c1ece07e727a.

--- pkgs/tools/system/pciutils/default.nix
+++ pkgs/tools/system/pciutils/default.nix
@@ -1,34 +1,26 @@
-{ stdenv, fetchurl, fetchFromGitHub, pkgconfig, zlib, kmod, which }:
+{ stdenv, fetchurl, pkgconfig, zlib, kmod, which }:
 
 stdenv.mkDerivation rec {
-  pname = "pciutils";
-  #version = "3.6.2"; # with release-date database
-  version = "2019-02-21";
+  name = "pciutils-3.6.2"; # with release-date database
 
-  #src = fetchurl {
-  #  url = "mirror://kernel/software/utils/pciutils/${pname}-${version}.tar.xz";
-  #  sha256 = "1wwkpglvvr1sdj2gxz9khq507y02c4px48njy25divzdhv4jwifv";
-  #};
-  src = fetchFromGitHub {
-    owner = "pciutils";
-    repo = "pciutils";
-    rev = "33226851677b92fffd186002d74a1a515fb49413";
-    sha256 = "0z4f5y7mfkar2adblr32csd19vcg0wlbhap4bcsrbg342n7866ky";
+  src = fetchurl {
+    url = "mirror://kernel/software/utils/pciutils/${name}.tar.xz";
+    sha256 = "1wwkpglvvr1sdj2gxz9khq507y02c4px48njy25divzdhv4jwifv";
   };
 
-  nativeBuildInputs = [ pkgconfig which ];
-  buildInputs = [ zlib kmod ];
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ zlib kmod which ];
 
   makeFlags = [
     "SHARED=yes"
-    "PREFIX=${placeholder "out"}"
+    "PREFIX=\${out}"
     "STRIP="
     "HOST=${stdenv.hostPlatform.system}"
     "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
     "DNS=yes"
   ];
 
-  installTargets = [ "install" "install-lib" ];
+  installTargets = "install install-lib";
 
   # Get rid of update-pciids as it won't work.
   postInstall = "rm $out/sbin/update-pciids $out/man/man8/update-pciids.8";
