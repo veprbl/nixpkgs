e05da97c1b008964f37a61917f6fe55d5c01c5f2
Will Dietz <w@wdtz.org>
2019-03-27 07:36:59 -0500

    x86info: 1.30 -> 1.31pre (git)

--- pkgs/os-specific/linux/x86info/default.nix
+++ pkgs/os-specific/linux/x86info/default.nix
@@ -1,26 +1,35 @@
-{stdenv, fetchurl, pciutils, python}:
+{stdenv, fetchurl, fetchFromGitHub, pkgconfig, pciutils, python}:
 
 stdenv.mkDerivation rec {
-  version = "1.30";
-  name = "x86info-${version}";
+  version = "1.31pre";
+  pname = "x86info";
 
-  src = fetchurl {
-    url = "http://codemonkey.org.uk/projects/x86info/${name}.tgz";
-    sha256 = "0a4lzka46nabpsrg3n7akwr46q38f96zfszd73xcback1s2hjc7y";
+  src = fetchFromGitHub {
+    owner = "kernelslacker";
+    repo = pname;
+    rev = "1b41e8b338da6a18092e62e61295028ad59a3c2b";
+    sha256 = "0phnhrh8xjq1lxmnhb9b9lc8c8x4mz0a95xjrd7i6ks61lfvrrnd";
   };
+  #src = fetchurl {
+  #  url = "http://codemonkey.org.uk/projects/x86info/${name}.tgz";
+  #  sha256 = "0a4lzka46nabpsrg3n7akwr46q38f96zfszd73xcback1s2hjc7y";
+  #};
 
   preConfigure = ''
     patchShebangs .
 
-    # ignore warnings
-    sed -i 's/-Werror -Wall//' Makefile
+    # don't treat warnings as errors
+    substituteInPlace Makefile --replace -Werror ""
+    substituteInPlace lsmsr/Makefile --replace -Werror ""
   '';
 
-  buildInputs = [ pciutils python ];
+  nativeBuildInputs = [ python pkgconfig ];
+  buildInputs = [ pciutils ];
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp x86info lsmsr $out/bin
+    install -Dm755 -t $out/bin x86info lsmsr/lsmsr
+    install -Dm655 -T lsmsr/lsmsr.8 $out/share/man/man8/lsmsr.8
+    install -Dm655 -T x86info.1 $out/share/man/man1/x86info.1
   '';
 
   meta = {
