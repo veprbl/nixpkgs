c4ca6bc3e9a4823302491b0c68587094841fc2e9
Will Dietz <w@wdtz.org>
2019-03-28 20:18:06 -0500

    compton: debug

--- pkgs/applications/window-managers/compton/default.nix
+++ pkgs/applications/window-managers/compton/default.nix
@@ -97,16 +97,16 @@ let
       substituteInPlace meson.build --replace "version: '6'" "version: '6-git-${version}'"
     '';
 
-    #patches = [ ./above_sibling.patch ];
+    patches = [ ./logging.patch ]; # ./above_sibling.patch ];
 
-    NIX_CFLAGS_COMPILE = [ "-fno-strict-aliasing" /* "-DDEBUG_RESTACK=1" "-DDEBUG_EVENTS=1" */ ];
+    NIX_CFLAGS_COMPILE = [ "-fno-strict-aliasing" "-DDEBUG_RESTACK=1" "-DDEBUG_EVENTS=1" ];
 
     doCheck = true;
 
     mesonFlags = [
       "-Dbuild_docs=true"
       "-Dunittest=true"
-      #"-Dsanitize=true"
+      "-Dsanitize=true"
     ];
 
     meta = {
