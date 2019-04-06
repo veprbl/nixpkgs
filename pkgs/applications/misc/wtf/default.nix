cfeeadbeddedc99d322c6a34b8a142bf9d664e74
Will Dietz <w@wdtz.org>
2019-04-05 23:09:27 -0500

    wtf: populate build info a bit better

--- pkgs/applications/misc/wtf/default.nix
+++ pkgs/applications/misc/wtf/default.nix
@@ -3,7 +3,9 @@
 , lib
 }:
 
-buildGoPackage rec {
+let
+  release-datetime="2019-01-30" + "T00:00:00+0000";
+in buildGoPackage rec {
   pname = "wtf";
   version = "0.5.0";
 
@@ -16,7 +18,11 @@ buildGoPackage rec {
     sha256 = "1f59ck6rqicswjp6l5x35n0aqdicjc7jkwlpsyy477gisdlbw058";
   };
 
-  buildFlagsArray = [ "-ldflags=" "-X main.version=${version}" ];
+  buildFlagsArray = ''
+    -ldflags=
+    -X main.version=${version}
+    -X main.date=${release-datetime}
+  '';
 
   goDeps = ./deps.nix;
 
