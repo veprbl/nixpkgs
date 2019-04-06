d024ba15f290ee9a4c6a5f3352ff3ce245c28e91
Will Dietz <w@wdtz.org>
2019-04-06 15:56:39 -0500

    Revert "joplin-desktop: wip using appimageTools, can't neatly use gapps hook"
    This reverts commit 0d6fe575e96a08146df6e8122075ba9d51013663.

--- pkgs/applications/misc/joplin-desktop/default.nix
+++ pkgs/applications/misc/joplin-desktop/default.nix
@@ -1,24 +1,30 @@
-{ appimageTools, fetchurl, lib, pkgs }:
+{ stdenv, appimage-run, fetchurl }:
 
 let
   version = "1.0.142";
   sha256 = "0k7lnv3qqz17a2a2d431sic3ggi3373r5k0kwxm4017ama7d72m1";
-  pname = "joplin";
-in appimageTools.wrapType2 rec {
-  name = "${pname}-${version}";
+in
+  stdenv.mkDerivation rec {
+  name = "joplin-${version}";
+
   src = fetchurl {
     url = "https://github.com/laurent22/joplin/releases/download/v${version}/Joplin-${version}-x86_64.AppImage";
     inherit sha256;
   };
 
-  extraPkgs = p: with p; [ gnome3.dconf gtk3 librsvg gsettings-desktop-schemas ];
+  buildInputs = [ appimage-run ];
 
-  profile = ''
-   export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:$XDG_DATA_DIRS"
-   env
+  unpackPhase = ":";
+
+  installPhase = ''
+    mkdir -p $out/{bin,share}
+    cp $src $out/share/joplin.AppImage
+    echo "#!/bin/sh" > $out/bin/joplin-desktop
+    echo "${appimage-run}/bin/appimage-run $out/share/joplin.AppImage" >> $out/bin/joplin-desktop
+    chmod +x $out/bin/joplin-desktop $out/share/joplin.AppImage
   '';
 
-  meta = with lib; {
+  meta = with stdenv.lib; {
     description = "An open source note taking and to-do application with synchronisation capabilities";
     longDescription = ''
       Joplin is a free, open source note taking and to-do application, which can
