4eb53ed1d1e1677ccd9b99f83a0e74697e1f0636
Will Dietz <w@wdtz.org>
2019-04-14 04:10:40 -0500

    zanshin: 0.5.0 -> git

--- pkgs/applications/office/zanshin/default.nix
+++ pkgs/applications/office/zanshin/default.nix
@@ -1,6 +1,6 @@
 {
   mkDerivation, lib,
-  fetchurl,
+  fetchurl, fetchFromGitHub,
   extra-cmake-modules,
   qtbase, boost,
   akonadi-calendar, akonadi-notes, akonadi-search, kidentitymanagement, kontactinterface, kldap,
@@ -11,9 +11,15 @@ mkDerivation rec {
   pname = "zanshin";
   version = "0.5.0";
 
-  src = fetchurl {
-    url = "https://download.kde.org/stable/${pname}/${pname}-${version}.tar.xz";
-    sha256 = "0kyv7bmj9zv69jybdbp3n1ckmlq326bdq9rhzzcz71hifj4c1087";
+  #src = fetchurl {
+  #  url = "https://download.kde.org/stable/${pname}/${pname}-${version}.tar.xz";
+  #  sha256 = "0kyv7bmj9zv69jybdbp3n1ckmlq326bdq9rhzzcz71hifj4c1087";
+  #};
+  src = fetchFromGitHub {
+    owner = "KDE";
+    repo = pname;
+    rev = "43302c2eea1cc234596ac6d52c452cbba2eee377";
+    sha256 = "09ibbx747mfnk5pgzjl6i8yi2li0xppd9vfqzkrml8vpr6jqwbvy";
   };
 
   nativeBuildInputs = [
