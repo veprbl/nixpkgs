4db7415972ab34e0fc9a29ea91e1d4c9443ef5bb
Will Dietz <w@wdtz.org>
2019-04-14 13:46:11 -0500

    super-productivity: ensure libuuid is avail/on rpath

--- pkgs/applications/networking/super-productivity/default.nix
+++ pkgs/applications/networking/super-productivity/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, dpkg, makeWrapper
 , alsaLib, atk, cairo, cups, curl, dbus, expat, fontconfig, freetype, glib
 , gnome2, gtk3, gdk_pixbuf, libnotify, libxcb, nspr, nss, pango
-, systemd, xorg, xprintidle-ng }:
+, utillinux, systemd, xorg, xprintidle-ng }:
 
 let
 
@@ -28,6 +28,7 @@ let
     nss
     stdenv.cc.cc
     systemd
+    utillinux
 
     xorg.libxkbfile
     xorg.libX11
