b9122c5ba934cd9a3eadbdce92ac66e10bfdd4bc
Will Dietz <w@wdtz.org>
2019-04-15 09:20:19 -0500

    mtxclient: new home, git, shared

--- pkgs/development/libraries/mtxclient/default.nix
+++ pkgs/development/libraries/mtxclient/default.nix
@@ -3,31 +3,23 @@
 
 stdenv.mkDerivation rec {
   name = "mtxclient-${version}";
-  version = "0.2.0";
+  version = "0.2.0-git";
 
   src = fetchFromGitHub {
-    owner = "mujx";
+    owner = "Nheko-Reborn";
     repo = "mtxclient";
-    rev = "v${version}";
-    sha256 = "19v1qa6mzvc65m7wy7x0g4i24bcg9xk31y1grwvd3zr0l4v6xcgs";
+    #rev = "v${version}";
+    rev = "6f7b35aa1aea1c1cf96e7aa8168c1c4f53ededec";
+    sha256 = "0cvk5vjvlapvjfyrvlhslvmvgk83hqwvndx4yw63niw6jxh62q5j";
   };
 
-  patches = [
-    # remove on the next mtxclient update
-    (fetchpatch {
-      url = "https://github.com/Nheko-Reborn/mtxclient/commit/41314809da7eb17ec00cff1795af6a528c5e904a.diff";
-      sha256 = "17pzrkdhd4jr8xwd7hhyzak880k8yb9nkg3vcbyjfp5si89dha5j";
-    })
+  cmakeFlags = [
+    "-DBUILD_LIB_TESTS=OFF" "-DBUILD_LIB_EXAMPLES=OFF"
+    "-DBUILD_SHARED_LIBS=ON"
   ];
 
-  postPatch = ''
-    ln -s ${nlohmann_json}/include/nlohmann/json.hpp include/json.hpp
-  '';
-
-  cmakeFlags = [ "-DBUILD_LIB_TESTS=OFF" "-DBUILD_LIB_EXAMPLES=OFF" ];
-
   nativeBuildInputs = [ cmake pkgconfig ];
-  buildInputs = [ boost openssl zlib libsodium olm ];
+  buildInputs = [ boost openssl nlohmann_json zlib libsodium olm ];
 
   meta = with stdenv.lib; {
     description = "Client API library for Matrix, built on top of Boost.Asio";
