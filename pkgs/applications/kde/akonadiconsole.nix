a098f046a2d6fe39736274b2dbc1b8c208fa18f2
Will Dietz <w@wdtz.org>
2019-04-15 22:07:41 -0500

    akonadiconsole: git

--- pkgs/applications/kde/akonadiconsole.nix
+++ pkgs/applications/kde/akonadiconsole.nix
@@ -1,5 +1,5 @@
 {
-  mkDerivation, lib, kdepimTeam,
+  mkDerivation, lib, kdepimTeam, fetchFromGitHub,
   extra-cmake-modules, kdoctools,
   akonadi, akonadi-contacts, calendarsupport, kcalcore, kcompletion,
   kconfigwidgets, kcontacts, kdbusaddons, kitemmodels, kpimtextedit, libkdepim,
@@ -18,4 +18,11 @@ mkDerivation {
     kcontacts kdbusaddons kitemmodels kpimtextedit ktextwidgets kxmlgui
     messagelib qtbase libkdepim akonadi-search xapian
   ];
+
+  src = fetchFromGitHub {
+    owner = "KDE";
+    repo = "akonadiconsole";
+    rev = "2f0f78c9d86e4f6c1cf71e9742d078e51c03a43f";
+    sha256 = "1x7nv3bxghcpjv5rrw5mjbrpg24g3k0w3g1lal6ksxj549p0lh2h";
+  };
 }
