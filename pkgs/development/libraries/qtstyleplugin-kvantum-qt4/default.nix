7bb8775a726dfe0207d9b0bbf57a9e21fa43be4e
Will Dietz <w@wdtz.org>
2019-04-17 13:20:32 -0500

    kvantum: bump (git)

--- pkgs/development/libraries/qtstyleplugin-kvantum-qt4/default.nix
+++ pkgs/development/libraries/qtstyleplugin-kvantum-qt4/default.nix
@@ -8,14 +8,14 @@ stdenv.mkDerivation rec {
     owner = "tsujan";
     repo = "Kvantum";
     #rev = "V${version}";
-    rev = "3d452d5ca671ee64a9a054397966ac4fa5daea7b";
-    sha256 = "1pycbk5dmj6vhnwvm87csmir4crqk4g4z05402xw5ia9k1clpksk";
+    rev = "f1674c455fa718dc8a9cf00fffb3269aa3f20d7a";
+    sha256 = "0gz72qr92dsg6rc0hjsrx83nbf0gcngcn3x9wrwmi0pzqs68vjkr";
   };
 
   nativeBuildInputs = [ qmake4Hook ];
   buildInputs = [ qt4 libX11 libXext ];
 
-  postUnpack = "sourceRoot=\${sourceRoot}/Kvantum";
+  sourceRoot = "source/Kvantum";
 
   buildPhase = ''
     qmake kvantum.pro
