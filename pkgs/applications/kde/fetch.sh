848ca4ef66d32707ee25804fee727841160e734b
Will Dietz <w@wdtz.org>
2019-04-18 23:58:05 -0500

    pick mirror to avoid stalling on the jp host, dunno

--- pkgs/applications/kde/fetch.sh
+++ pkgs/applications/kde/fetch.sh
@@ -1,1 +1,4 @@
-WGET_ARGS=( https://download.kde.org/stable/applications/19.04.0/ -A '*.tar.xz' )
+#WGET_ARGS=( https://download.kde.org/stable/applications/19.04.0/ -A '*.tar.xz' )
+WGET_ARGS=( http://ftp.ussg.iu.edu/kde/stable/applications/19.04.0/ -A '*.tar.xz' )
+
+
