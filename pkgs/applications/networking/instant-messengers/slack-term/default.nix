db985d36c64bcc6a206a3ed344d954decaa761cc
Will Dietz <w@wdtz.org>
2019-04-19 11:46:26 -0500

    slack-term: git

--- pkgs/applications/networking/instant-messengers/slack-term/default.nix
+++ pkgs/applications/networking/instant-messengers/slack-term/default.nix
@@ -3,15 +3,16 @@
 buildGoPackage rec {
   # https://github.com/erroneousboat/slack-term
   name = "slack-term-${version}";
-  version = "0.4.1";
+  version = "0.4.1-git";
 
   goPackagePath = "github.com/erroneousboat/slack-term";
 
   src = fetchFromGitHub {
     owner = "erroneousboat";
     repo = "slack-term";
-    rev = "v${version}";
-    sha256 = "1340bq7h31fxykxbxpn6hv7n2hmjf20f8vg5gan9pjf5jaa6kfza";
+    #rev = "v${version}";
+    rev = "112524daad6315323d6097222d3b2759ed353aaf";
+    sha256 = "18nk3dxm7laknncwkdgc6y7aiya2vwhs8c2xvchp6ln01nnf99z2";
   };
 
   meta = with stdenv.lib; {
