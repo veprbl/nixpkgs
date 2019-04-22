5b26adcf9cc7ccc85e23d7324ec1c4f4540b836c
Will Dietz <w@wdtz.org>
2019-04-22 16:41:24 -0500

    dedup: bump to latest git

--- pkgs/tools/backup/dedup/default.nix
+++ pkgs/tools/backup/dedup/default.nix
@@ -1,12 +1,18 @@
-{ stdenv, fetchurl, lz4, snappy, openmp ? null }:
+{ stdenv, fetchurl, fetchgit, lz4, snappy, openmp ? null }:
 
 stdenv.mkDerivation rec {
   pname = "dedup";
-  version = "1.0";
+  #version = "1.0";
+  version = "2019-04-18";
 
-  src = fetchurl {
-    url = "https://dl.2f30.org/releases/${pname}-${version}.tar.gz";
-    sha256 = "0wd4cnzhqk8l7byp1y16slma6r3i1qglwicwmxirhwdy1m7j5ijy";
+  #src = fetchurl {
+  #  url = "https://dl.2f30.org/releases/${pname}-${version}.tar.gz";
+  #  sha256 = "0wd4cnzhqk8l7byp1y16slma6r3i1qglwicwmxirhwdy1m7j5ijy";
+  #};
+  src = fetchgit {
+    url =  git://git.2f30.org/dedup.git;
+    rev = "3d79ba2671de814d94474dce3765be64a982cc5a";
+    sha256 = "0fhlxcgc05pjkdmd0ibpzfglsxi2gr1lcrn8qz9ibmrwddznzqck";
   };
 
   makeFlags = [
