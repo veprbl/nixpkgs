570a4b9d55f700216915835937fddc3a1ed9e496
Will Dietz <w@wdtz.org>
2019-04-23 14:48:21 -0500

    joplin-desktop: friendlier entry point name
    Don't include version, that's just a pain.

--- pkgs/applications/misc/joplin-desktop/default.nix
+++ pkgs/applications/misc/joplin-desktop/default.nix
@@ -1,7 +1,7 @@
 { appimageTools, fetchurl, lib, gsettings-desktop-schemas, gtk3 }:
 
 let
-  pname = "joplin";
+  pname = "joplin-desktop";
   version = "1.0.143";
 in appimageTools.wrapType2 rec {
   name = "${pname}-${version}";
@@ -15,6 +15,10 @@ in appimageTools.wrapType2 rec {
   profile = ''
     export LC_ALL=C.UTF-8
     export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
+  '';
+
+  extraInstallCommands = ''
+    mv $out/bin/${name} $out/bin/${pname}
   '';
 
   meta = with lib; {
