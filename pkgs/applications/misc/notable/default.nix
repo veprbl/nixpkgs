1e6038b7a707d9b04c2e03974100688d454b8a1e
Will Dietz <w@wdtz.org>
2019-04-23 15:04:52 -0500

    notable: same treatment

--- pkgs/applications/misc/notable/default.nix
+++ pkgs/applications/misc/notable/default.nix
@@ -18,6 +18,8 @@ appimageTools.wrapType2 rec {
     export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
   '';
 
+  multiPkgs = null; # no 32bit needed
+  extraPkgs = appimageTools.defaultFhsEnvArgs.multiPkgs;
   extraInstallCommands = "mv $out/bin/{${name},${pname}}";
 
   meta = with lib; {
