45421afb3ab362a609561c65ed1032d7a21f9a64
Will Dietz <w@wdtz.org>
2019-04-24 10:08:18 -0500

    mono: 5.16.0.220 -> 5.20.1.19
    https://www.mono-project.com/docs/about-mono/releases/5.18.0/
    https://www.mono-project.com/docs/about-mono/releases/5.20.0/
    
    re-enable parallel building optimistically.

--- pkgs/development/compilers/mono/5.nix
+++ pkgs/development/compilers/mono/5.nix
@@ -2,7 +2,6 @@
 
 callPackage ./generic.nix (rec {
   inherit Foundation libobjc;
-  version = "5.16.0.220";
-  sha256 = "1qwdmxssplfdb5rq86f1j8lskvr9dfk5c8hqz9ic09ml69r8c87l";
-  enableParallelBuilding = false;
+  version = "5.20.1.19";
+  sha256 = "0s6l8pqkx5cd9p6l4qld7sqcf7f929xdh0qx45j37z4vzcgbcx05";
 })
