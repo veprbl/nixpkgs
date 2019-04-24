9d9126483a93e42250ee264c158188863541cf92
Will Dietz <w@wdtz.org>
2019-04-24 09:39:35 -0500

    outputcheck: init at 0.4.2

---
+++ pkgs/development/tools/misc/outputcheck/default.nix
@@ -0,0 +1,21 @@
+{ lib, pythonPackages, fetchFromGitHub }:
+
+pythonPackages.buildPythonApplication rec {
+  pname = "outputcheck";
+  version = "0.4.2";
+
+  src = fetchFromGitHub {
+    owner = "stp";
+    repo = pname;
+    rev = version;
+    sha256 = "1y27vz6jq6sywas07kz3v01sqjd0sga9yv9w2cksqac3v7wmf2a0";
+  };
+
+  meta = {
+    description = "Tool for checking tool output inspired by LLVM's FileCheck";
+    homepage = https://github.com/stp/OutputCheck;
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ dtzWill ];
+  };
+}
+
