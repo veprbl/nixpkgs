34a4323e0bcf1d48dd52a696803414a3bd726006
Will Dietz <w@wdtz.org>
2019-04-29 01:52:39 -0500

    libcef: bump

--- pkgs/development/libraries/libcef/default.nix
+++ pkgs/development/libraries/libcef/default.nix
@@ -14,10 +14,15 @@ let
 in
 stdenv.mkDerivation rec {
   name = "cef-binary-${version}";
-  version = "3.3683.1920.g9f41a27";
+  #version = "3.3683.1920.g9f41a27";
+  version = "74.1.13+g98f22d3+chromium-74.0.3729.108";
   src = fetchurl {
-    url = "http://opensource.spotify.com/cefbuilds/cef_binary_${version}_linux64.tar.bz2";
-    sha256 = "12iv798p6g17jqxx4fid4jgwkrpvlfkx4250lk8byhync53zbw0d";
+    #url = "http://opensource.spotify.com/cefbuilds/cef_binary_${version}_linux64.tar.bz2";
+    # TODO: 's,+,%2B,'
+    name = "${name}.tar.bz2";
+    url = http://opensource.spotify.com/cefbuilds/cef_binary_74.1.13%2Bg98f22d3%2Bchromium-74.0.3729.108_linux64.tar.bz2;
+    #sha256 = "12iv798p6g17jqxx4fid4jgwkrpvlfkx4250lk8byhync53zbw0d";
+    sha256 = "0p0pwdk0iavc70m1wcs6g99skx4ynlndhpppbsw41ndjynw1id57";
   };
   nativeBuildInputs = [ cmake ];
   makeFlags = "libcef_dll_wrapper";
