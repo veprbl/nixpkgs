1ff8c5d552ae9a4e2da95c1858172fe751d0c1b7
Will Dietz <w@wdtz.org>
2019-04-29 18:09:34 -0500

    getopt: 1.1.4 -> 1.1.6, don't rely on tarballs.nixos.org
    http://frodo.looijaard.name/project/getopt/release/1-1-6
    http://frodo.looijaard.name/project/getopt/release/1-1-5

--- pkgs/tools/misc/getopt/builder.sh
+++ pkgs/tools/misc/getopt/builder.sh
@@ -1,4 +1,4 @@
 source $stdenv/setup
 installFlags="prefix=$out"
-makeFlags="CFLAGS=-DWITHOUT_GETTEXT LIBCGETOPT=0"
+makeFlags="WITHOUT_GETTEXT=1 LIBCGETOPT=0"
 genericBuild
--- pkgs/tools/misc/getopt/default.nix
+++ pkgs/tools/misc/getopt/default.nix
@@ -1,11 +1,12 @@
 {stdenv, fetchurl}:
 
-stdenv.mkDerivation {
-  name = "getopt-1.1.4";
+stdenv.mkDerivation rec {
+  pname = "getopt";
+  version = "1.1.6";
   builder = ./builder.sh;
   src = fetchurl {
-    url = http://tarballs.nixos.org/getopt-1.1.4.tar.gz;
-    sha256 = "1arvjfzw6p310zbgv629w5hkyslrj44imf3r3s2r4ry2jfcks221";
+    url = "http://frodo.looijaard.name/system/files/software/${pname}/${pname}-${version}.tar.gz";
+    sha256 = "1zn5kp8ar853rin0ay2j3p17blxy16agpp8wi8wfg4x98b31vgyh";
   };
   preBuild = ''
     export buildFlags=CC="$CC" # for darwin
