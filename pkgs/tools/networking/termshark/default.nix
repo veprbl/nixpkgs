f623620afa3ae451e7d1c07453dad7f39a45430e
Will Dietz <w@wdtz.org>
2019-04-29 14:13:25 -0500

    termshark: install bin

--- pkgs/tools/networking/termshark/default.nix
+++ pkgs/tools/networking/termshark/default.nix
@@ -13,8 +13,6 @@ buildGoModule rec {
 
   modSha256 = "09mbjbk5wa18z4xis5b2v2v0b04mf4d896yp88vcj8d8hsmbmc6g";
 
-  subPackages = [ "." ];
-
   meta = with stdenv.lib; {
     homepage = https://termshark.io/;
     description = "A terminal UI for tshark, inspired by Wireshark";
