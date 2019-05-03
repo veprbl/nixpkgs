51a1f33d06f5c67601651b328f01f3b0dd26ecf4
Will Dietz <w@wdtz.org>
2019-05-02 21:15:03 -0500

    sleuthkit: 4.6.5 -> 4.6.6, pname
    https://github.com/sleuthkit/sleuthkit/blob/sleuthkit-4.6.6/NEWS.txt

--- pkgs/tools/system/sleuthkit/default.nix
+++ pkgs/tools/system/sleuthkit/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchFromGitHub, autoreconfHook, libewf, afflib, openssl, zlib }:
 
 stdenv.mkDerivation rec {
-  version = "4.6.5";
-  name = "sleuthkit-${version}";
+  version = "4.6.6";
+  pname = "sleuthkit";
 
   src = fetchFromGitHub {
-    owner = "sleuthkit";
-    repo = "sleuthkit";
+    owner = pname;
+    repo = pname;
     rev = name;
-    sha256 = "1q1cdixnfv9v4qlzza8xwdsyvq1vdw6gjgkd41yc1d57ldp1qm0c";
+    sha256 = "07lpka1sps95d0gfx1l09sp8m74h1v8ibjjd8877dbpq2pi6yay6";
   };
 
   postPatch = ''
