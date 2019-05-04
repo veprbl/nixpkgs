7b50ca595bd8464c523dcbd5f8e93375332170d3
Milan Pässler <me@pbb.lc>
2019-05-04 02:35:55 +0200

    viu: init at 0.1

---
+++ pkgs/tools/graphics/viu/default.nix
@@ -0,0 +1,22 @@
+{ stdenv, rustPlatform, fetchFromGitHub, cmake, pkgconfig, zlib }:
+
+rustPlatform.buildRustPackage rec {
+  name = "viu-${version}";
+  version = "0.1";
+
+  src = fetchFromGitHub {
+    owner = "atanunq";
+    repo = "viu";
+    rev = "v${version}";
+    sha256 = "1j2sr8mhnbyzm168spzr4mk8gkjlfqh993b80sf2zv2sy83p8gfv";
+  };
+
+  cargoSha256 = "14pf2xvkk9qqq9qj5agxmfl3npgy6my961yfzv7p977712kdakh3";
+
+  meta = with stdenv.lib; {
+    description = "A command-line application to view images from the terminal written in Rust";
+    homepage = https://github.com/atanunq/viu;
+    license = licenses.mit;
+    maintainers = with maintainers; [ petabyteboy ];
+  };
+}
