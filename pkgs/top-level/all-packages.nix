af593d6c0cd496f1068c95dd82740a4c4f859212
Will Dietz <w@wdtz.org>
2019-05-05 22:12:10 -0500

    creduce: 2.8.0 -> 2.9.0, llvm7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8935,7 +8935,7 @@ in
   credstash = with python3Packages; toPythonApplication credstash;
 
   creduce = callPackage ../development/tools/misc/creduce {
-    inherit (llvmPackages_6) llvm clang-unwrapped;
+    inherit (llvmPackages_7) llvm clang-unwrapped;
   };
 
   cscope = callPackage ../development/tools/misc/cscope { };
