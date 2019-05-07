a754124e5799dbe0d8e923a5c09f0e962396e753
Will Dietz <w@wdtz.org>
2019-05-06 21:27:33 -0500

    viu: bump, read from stdin

--- pkgs/tools/graphics/viu/default.nix
+++ pkgs/tools/graphics/viu/default.nix
@@ -8,8 +8,8 @@ rustPlatform.buildRustPackage rec {
     owner = "atanunq";
     repo = "viu";
     #rev = "v${version}";
-    rev = "1dcc30e1aad4402936a814fd3aaf68d1afb307f4";
-    sha256 = "1g8fc6chaw1rjhqp2li6m9xri47sha2fcfkq7qq04cqs847sx33j";
+    rev = "712241291081d5dae6aca5a5f1d80d219997f074";
+    sha256 = "16g0ip5i1iw3mxxsr0n70n79rvk0vac2cmxmzpl3ylw45sa3qvnl";
   };
 
   cargoSha256 = "1h9dm2hhld2079dnx4x5nzkn3ivk6g5ijhv49jxnc200mx4mr1s5";
