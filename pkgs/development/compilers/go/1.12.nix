3f30036f4d016e6995675d4890e39aa3df1232fa
Will Dietz <w@wdtz.org>
2019-05-08 02:01:37 -0500

    go_1_12: 1.12.2 -> 1.12.5

--- pkgs/development/compilers/go/1.12.nix
+++ pkgs/development/compilers/go/1.12.nix
@@ -29,12 +29,12 @@ let
 in
 
 stdenv.mkDerivation rec {
-  name = "go-${version}";
-  version = "1.12.2";
+  pname = "go";
+  version = "1.12.5";
 
   src = fetchurl {
     url = "https://dl.google.com/go/go${version}.src.tar.gz";
-    sha256 = "0n3v7gydzqhpn3chkrk32ab9ac7yfi1zsijdfff302d6lj02b6dg";
+    sha256 = "1s034pr60h20lsw171vbzqxqvcqn0s04dxfyqczyfcmkrf4g199a";
   };
 
   # perl is used for testing go vet
