d236cf96a05b42b806259ccc93145974d2a69867
Will Dietz <w@wdtz.org>
2019-05-07 23:43:08 -0500

    hunter: touchup

--- pkgs/tools/misc/hunter/default.nix
+++ pkgs/tools/misc/hunter/default.nix
@@ -17,6 +17,8 @@ buildRustPackage rec {
 
   buildInputs = [ file /* libmagic */];
 
+  HOME = ".";
+
 #  postInstall = ''
 #    mkdir -p $out/share/man/man1
 #    cp contrib/man/exa.1 $out/share/man/man1/
@@ -34,18 +36,18 @@ buildRustPackage rec {
   # Some tests fail, but Travis ensures a proper build
   #doCheck = false;
 
-  meta = with stdenv.lib; {
-    description = "Replacement for 'ls' written in Rust";
-    longDescription = ''
-      exa is a modern replacement for ls. It uses colours for information by
-      default, helping you distinguish between many types of files, such as
-      whether you are the owner, or in the owning group. It also has extra
-      features not present in the original ls, such as viewing the Git status
-      for a directory, or recursing into directories with a tree view. exa is
-      written in Rust, so it’s small, fast, and portable.
-    '';
-    homepage = https://the.exa.website;
-    license = licenses.mit;
-    maintainers = [ maintainers.ehegnes ];
-  };
+  ## TODO: ## meta = with stdenv.lib; {
+  ## TODO: ##   description = "Replacement for 'ls' written in Rust";
+  ## TODO: ##   longDescription = ''
+  ## TODO: ##     exa is a modern replacement for ls. It uses colours for information by
+  ## TODO: ##     default, helping you distinguish between many types of files, such as
+  ## TODO: ##     whether you are the owner, or in the owning group. It also has extra
+  ## TODO: ##     features not present in the original ls, such as viewing the Git status
+  ## TODO: ##     for a directory, or recursing into directories with a tree view. exa is
+  ## TODO: ##     written in Rust, so it’s small, fast, and portable.
+  ## TODO: ##   '';
+  ## TODO: ##   homepage = https://the.exa.website;
+  ## TODO: ##   license = licenses.mit;
+  ## TODO: ##   maintainers = [ maintainers.ehegnes ];
+  ## TODO: ## };
 }
