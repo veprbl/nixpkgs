3d8ac22180c54b5fb1765c92ec2f22bde22a6994
Will Dietz <w@wdtz.org>
2019-05-10 00:11:39 -0500

    qogir-theme: bump, git

--- pkgs/data/themes/qogir/default.nix
+++ pkgs/data/themes/qogir/default.nix
@@ -2,13 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "qogir-theme";
-  version = "2019-05-03";
+  version = "2019-05-07";
 
   src = fetchFromGitHub {
     owner = "vinceliuice";
     repo = pname;
-    rev = version;
-    sha256 = "031nqr47b3x8ahcym7cfc75y8sy53dcmrrrlywi7m1a10ckfp0pd";
+    #rev = version;
+    rev = "a0188de8cc703ae72df4b29dead5f646a131aaa0";
+    sha256 = "1sac1k6r3ax18dwn2d5j55mpv1f8s9gnw5rr0jh1waax8swjaq1p";
   };
 
   buildInputs = [ gdk_pixbuf librsvg ];
