4f29b2bc11dbe7cc342dbf8bb375811830122f1d
Will Dietz <w@wdtz.org>
2019-05-10 03:16:19 -0500

    discount: 2.2.5 -> 2.2.6
    https://github.com/Orc/discount/releases/tag/v2.2.6

--- pkgs/tools/text/discount/default.nix
+++ pkgs/tools/text/discount/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
-  version = "2.2.5";
+  version = "2.2.6";
   pname = "discount";
 
   src = fetchFromGitHub {
     owner = "Orc";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1b2nqnf3cd8bxgnx5ld5mnjn5ln4srvjfa5q9ivfwigg94w6l098";
+    sha256 = "1y066jkxfas0vdixbqq66j9p00a102sbfgq5gbrblfczqjrmc38w";
   };
 
   patches = ./fix-configure-path.patch;
