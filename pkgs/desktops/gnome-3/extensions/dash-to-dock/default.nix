bee4514024a8ab8e37afc19b4b044f1970a7a3bb
Tor Hedin Brønner <torhedinbronner@gmail.com>
2020-03-15 15:17:24 +0100

    gnomeExtensions.dash-to-dock: 67 -> 20200313
    Get something working until there's an official release.

--- pkgs/desktops/gnome-3/extensions/dash-to-dock/default.nix
+++ pkgs/desktops/gnome-3/extensions/dash-to-dock/default.nix
@@ -1,14 +1,15 @@
 { stdenv, fetchFromGitHub, glib, gettext }:
 
 stdenv.mkDerivation rec {
-  pname = "gnome-shell-dash-to-dock";
-  version = "67";
+  pname = "gnome-shell-dash-to-dock-unstable";
+  version = "20200313";
 
   src = fetchFromGitHub {
     owner = "micheleg";
     repo = "dash-to-dock";
-    rev = "extensions.gnome.org-v" + version;
-    sha256 = "1746xm0iyvyzj6m3pvjx11smh9w1s7naz426ki0dlr5l7jh3mpy5";
+    # rev = "extensions.gnome.org-v" + version;
+    rev = "800dc0abd65323f158b33a6c883329c036bb7bcb";
+    sha256 = "1a92q8yg4pdsn2f6qvn00lcxxm7l8sd67ii44kbbzbhkfa351l62";
   };
 
   nativeBuildInputs = [
